facf088f8534b699fc654ee16347c00fe7d48774
Tomasz Balon <balontomasz@gmail.com>
2019-08-13 20:10:56 +0200

    Add helper method to sign_in

--- spec/features/dashboard_page_spec.rb
+++ spec/features/dashboard_page_spec.rb
@@ -2,13 +2,7 @@ require 'rails_helper'
 
 describe 'Opening home page', type: :feature do
   it 'signs me in' do
-    user = User.create(
-      email: 'test@test.com',
-      password: 'password',
-      password_confirmation: 'password',
-      confirmed_at: Time.now
-    )
-    sign_in user
+    sign_in_user
 
     visit '/'
     expect(page).to have_current_path('/dashboard')
