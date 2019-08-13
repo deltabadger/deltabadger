b28f83446d565b37e6431f87f896c370a56762b5
Tomasz Balon <balontomasz@gmail.com>
2019-08-13 21:17:39 +0200

    Update tests

--- spec/features/home_page_spec.rb
+++ spec/features/home_page_spec.rb
@@ -1,9 +1,20 @@
 require 'rails_helper'
 
 describe 'Opening home page', type: :feature do
-  it 'signs me in' do
+  it 'opens page' do
     visit '/'
     expect(page).to have_content 'Home page'
     expect(page).to have_http_status(200)
+  end
+
+  context 'with signed in user' do
+    it 'opens page' do
+      sign_in_user
+
+      visit '/'
+      expect(page).to have_current_path('/dashboard')
+      expect(page).to have_content 'Dashboard'
+      expect(page).to have_http_status(200)
+    end
   end
 end
