8fb225fc9495bec2c745ad926367aaaf79cd8e23
Tomasz Balon <7355928+balontomasz@users.noreply.github.com>
2019-08-26 00:27:03 +0200

    Add api_keys

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_25_214346) do
+ActiveRecord::Schema.define(version: 2019_08_25_215208) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "api_keys", force: :cascade do |t|
+    t.bigint "exchange_id", null: false
+    t.bigint "user_id", null: false
+    t.string "encrypted_key"
+    t.string "encrypted_key_iv"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["exchange_id"], name: "index_api_keys_on_exchange_id"
+    t.index ["user_id"], name: "index_api_keys_on_user_id"
+  end
 
   create_table "exchanges", force: :cascade do |t|
     t.string "name"
@@ -38,4 +49,6 @@ ActiveRecord::Schema.define(version: 2019_08_25_214346) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "api_keys", "exchanges"
+  add_foreign_key "api_keys", "users"
 end
