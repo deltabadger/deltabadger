972325ae93429ac534f80cd5f6229204e36f7135
Tomasz Balon <balontomasz@gmail.com>
2019-09-10 01:13:33 +0200

    Add BitBay exchange

--- app/services/exchange_api/get.rb
+++ app/services/exchange_api/get.rb
@@ -14,8 +14,8 @@ module ExchangeApi
           api_secret: api_key.secret
         )
 
-      when 'Deribit'
-        ExchangeApi::Clients::Deribit.new
+      when 'Deribit'  then ExchangeApi::Clients::Deribit.new
+      when 'BitBay'   then ExchangeApi::Clients::Bitbay.new
       end
     end
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,10 @@
 Exchange.find_or_create_by!(name: 'Kraken')
 Exchange.find_or_create_by!(name: 'Deribit')
+Exchange.find_or_create_by!(name: 'BitBay')
+
+User.find_or_create_by(
+  email: "test@test.com"
+) do |user|
+  user.password = "polopolo"
+  user.confirmed_at = user.confirmed_at || Time.now
+end
