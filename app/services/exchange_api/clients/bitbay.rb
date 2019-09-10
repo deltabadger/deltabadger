4e3c5530505f5a63e226388970654e5035b451ec
Tomasz Balon <7355928+balontomasz@users.noreply.github.com>
2019-09-10 13:28:32 +0200

    Call Bitbay when validating credentials

--- app/services/exchange_api/clients/bitbay.rb
+++ app/services/exchange_api/clients/bitbay.rb
@@ -1,12 +1,36 @@
+# rubocop#disable Style/StringLiterals
 module ExchangeApi
   module Clients
     class Bitbay < ExchangeApi::Clients::Base
+      def initialize(api_key:, api_secret:)
+        @api_key = api_key
+        @api_secret = api_secret
+      end
+
       def validate_credentials
-        false
+        url = 'https://api.bitbay.net/rest/trading/history/transactions'
+        response = Faraday.get(url, {}, headers(''))
+        response.status == 200
       end
 
       def buy
         puts 'Buying on bitbay'
+      end
+
+      private
+
+      def headers(body)
+        timestamp = Time.now.to_i.to_s
+        post = @api_key + timestamp.to_s + body.to_s
+        signature = OpenSSL::HMAC.hexdigest('sha512', @api_secret, post)
+
+        {
+          'API-Key' => @api_key,
+          'API-Hash' => signature,
+          'operation-id' => SecureRandom.uuid.to_s,
+          'Request-Timestamp' => timestamp,
+          'Content-Type' => 'application/json'
+        }
       end
     end
   end
