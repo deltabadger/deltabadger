70005d81690b54de1ee19e2b62a096f2657a53a8
Tomasz Balon <balontomasz@gmail.com>
2019-09-13 00:09:26 +0200

    Add styling to Start/Stop

--- app/javascript/deltabadger/components/Bot.jsx
+++ app/javascript/deltabadger/components/Bot.jsx
@@ -1,22 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-export const Bot = ({ id, settings, status, exchangeName, handleStart, handleStop }) => {
+export const Bot = ({ id, settings, status, exchangeName, handleStart, handleStop, open }) => {
   const description = `${settings.type} for ${settings.price}${settings.currency}/${settings.interval} on ${exchangeName}`
 
-  const StartButton = () => (<button onClick={() => handleStart(id)}>Start</button>)
-  const StopButton = () => (<button onClick={() => handleStop(id)}>Stop</button>)
-
-  // return (
-  //   <div>
-  //     { description }
-  //     { status == 'working' ? <StopButton /> : <StartButton/> }
-  //   </div>
-  // )
+  const StartButton = () => (
+    <div onClick={() => handleStart(id)} className="btn btn-success"><span>Start</span> <i className="fas fa-play"></i></div>
+  )
+  const StopButton = () => (
+    <div onClick={() => handleStop(id)} className="btn btn-outline-primary"><span>Pause</span> <i className="fas fa-pause"></i></div>
+  )
 
   return (
     <div className="db-bots__item db-bot db-bot--dca db-bot--pick-exchange db-bot--running db-bot--collapsed">
       <div className="db-bot__header">
-        <div className="btn btn-outline-primary"><span>Pause</span> <i className="fas fa-pause"></i></div>
+        { status == 'working' ? <StopButton /> : <StartButton/> }
         <div className="db-bot__infotext text-danger">
           <div className="db-bot__infotext__left">
             Kraken:BTCEUR
