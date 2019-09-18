befbb2afa7a1f45ded7cc47e7ceaa1898b1ec366
Tomasz Balon <balontomasz@gmail.com>
2019-09-18 14:00:49 +0200

    Unschedule transactions when bot is stopped

--- app/services/stop_bot.rb
+++ app/services/stop_bot.rb
@@ -1,13 +1,16 @@
 class StopBot < BaseService
   def initialize(
-    bots_repository: BotsRepository.new
+    bots_repository: BotsRepository.new,
+    unschedule_transactions: UnscheduleTransactions.new
   )
 
     @bots_repository = bots_repository
+    @unschedule_transactions = unschedule_transactions
   end
 
   def call(bot_id)
     bot = @bots_repository.find(bot_id)
+    @unschedule_transactions.call(bot)
     @bots_repository.update(bot.id, status: 'stopped')
 
     Result::Success.new
