7041ea1509b83ef16c8231c33869092bd32aef39
Tomasz Balon <balontomasz@gmail.com>
2019-09-18 14:00:49 +0200

    Unschedule transactions when bot is stopped

---
+++ app/services/unschedule_transactions.rb
@@ -0,0 +1,15 @@
+class UnscheduleTransactions < BaseService
+  def call(bot)
+    queue = Sidekiq::ScheduledSet.new
+    queue.each do |job|
+      job.delete if delete?(job, bot)
+    end
+  end
+
+  private
+
+  def delete?(job, bot)
+    job.args == [bot.id] &&
+      job.klass == 'MakeTransactionWorker'
+  end
+end
