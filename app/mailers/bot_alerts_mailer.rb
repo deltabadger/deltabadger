c6a82c437b4d275f68fbd6c371b8b37c073cf76e
Tomasz Balon <balontomasz@gmail.com>
2019-09-23 02:15:56 +0200

    Add sending email when error occured

--- app/mailers/bot_alerts_mailer.rb
+++ app/mailers/bot_alerts_mailer.rb
@@ -1,2 +1,9 @@
 class BotAlertsMailer < ApplicationMailer
+  def notify_about_error
+    @user = params[:user]
+    @errors = params[:errors]
+    @bot = params[:bot]
+
+    mail(to: @user.email, subject: 'Something went wrong!')
+  end
 end
