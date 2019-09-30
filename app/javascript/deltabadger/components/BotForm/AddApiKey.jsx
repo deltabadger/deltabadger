cb163815a548cf1b71bac484617aca0828e92316
Jan <jan@janklosowski.com>
2019-09-30 21:13:26 +0200

    add API key submit styles

--- app/javascript/deltabadger/components/BotForm/AddApiKey.jsx
+++ app/javascript/deltabadger/components/BotForm/AddApiKey.jsx
@@ -19,27 +19,48 @@ export const AddApiKey = ({ handleReset, handleSubmit, errors }) => {
   }
 
   return (
-    <div>
-      { errors }
-      <form onSubmit={_handleSubmit}>
-        <label>
-          API Key:
-          <input
-            type="text"
-            value={key}
-            onChange={e => setKey(e.target.value)}
-          />
-        </label>
-        <label>
-          Secret API Key:
-          <input
-            type="text"
-            value={secret}
-            onChange={e => setSecret(e.target.value)}
-          />
-        </label>
-        <input type="submit" value="Submit" />
-      </form>
+    <div className="db-bots__item db-bot db-bot--get-apikey">
+      <div className="db-bot__header">
+        <div onClick={_handleSubmit} className="btn btn-primary"><span>Submit</span> <i className="fas fa-arrow-right"></i></div>
+        <div className="db-bot__infotext db-bot__infotext--setup">Get API Key (2 of 3)
+          <div className="progress progress--thin progress--bot-setup">
+            <div className="progress-bar" role="progressbar" style={{width: "40%", ariaValuenow: "40", ariaValuemin: "0", ariaValuemax: "100"}}></div>
+          </div>
+        </div>
+
+      </div>
+      <div className="row db-bot__exchanges">
+        { errors }
+        <form onSubmit={_handleSubmit} class="form-row w-100 mx-4 mt-4">
+          <div class="col form-group db-form-group--fg-2">
+            <label>API Key:</label>
+            <input
+              type="text"
+              value={key}
+              onChange={e => setKey(e.target.value)}
+              class="form-control"
+            />
+          </div>
+          <div class="col form-group db-form-group--fg-2">
+            <label>Secret API Key:</label>
+            <input
+              type="text"
+              value={secret}
+              onChange={e => setSecret(e.target.value)}
+              class="form-control col"
+            />
+          </div>
+        </form>
+      </div>
+      <div class="alert alert-warning mx-4 mt-2 mb-4" role="alert">
+        <b class="alert-heading mb-2">How to get API keys:</b>
+        <hr/>
+        <ol>
+          <li>Login to Kraken</li>
+          <li>Go to Settings</li>
+          <li>Get keys</li>
+        </ol>
+      </div>
       <ResetButton />
     </div>
   )
