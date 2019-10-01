999ad9892fcbe1cc76e5866213c6839efaf38735
Jan <jan@janklosowski.com>
2019-10-01 10:05:04 +0200

    add bot styles

--- app/javascript/deltabadger/components/BotForm/AddApiKey.jsx
+++ app/javascript/deltabadger/components/BotForm/AddApiKey.jsx
@@ -24,14 +24,14 @@ export const AddApiKey = ({ handleReset, handleSubmit, errors }) => {
         <div onClick={_handleSubmit} className="btn btn-primary"><span>Submit</span> <i className="fas fa-arrow-right"></i></div>
         <div className="db-bot__infotext db-bot__infotext--setup">Get API Key (2 of 3)
           <div className="progress progress--thin progress--bot-setup">
-            <div className="progress-bar" role="progressbar" style={{width: "40%", ariaValuenow: "40", ariaValuemin: "0", ariaValuemax: "100"}}></div>
+            <div className="progress-bar" role="progressbar" style={{width: "33%", ariaValuenow: "33", ariaValuemin: "0", ariaValuemax: "100"}}></div>
           </div>
         </div>
 
       </div>
       <div className="row db-bot__exchanges">
         { errors }
-        <form onSubmit={_handleSubmit} class="form-row w-100 mx-4 mt-4">
+        <form onSubmit={_handleSubmit} class="form-row w-100 mx-0 mt-4">
           <div class="col form-group db-form-group--fg-2">
             <label>API Key:</label>
             <input
@@ -52,14 +52,16 @@ export const AddApiKey = ({ handleReset, handleSubmit, errors }) => {
           </div>
         </form>
       </div>
-      <div class="alert alert-warning mx-4 mt-2 mb-4" role="alert">
-        <b class="alert-heading mb-2">How to get API keys:</b>
-        <hr/>
-        <ol>
-          <li>Login to Kraken</li>
-          <li>Go to Settings</li>
-          <li>Get keys</li>
-        </ol>
+      <div class="row">
+        <div class="alert alert-warning mx-0 mb-3 col" role="alert">
+          <b class="alert-heading mb-2">How to get API keys:</b>
+          <hr/>
+          <ol>
+            <li>Login to Kraken</li>
+            <li>Go to Settings</li>
+            <li>Get keys</li>
+          </ol>
+        </div>
       </div>
       <ResetButton />
     </div>
