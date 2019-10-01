66fffcae9ff77732cc439878208b81ab4bee0470
Tomasz Balon <balontomasz@gmail.com>
2019-10-02 00:03:44 +0200

    Validate subscription before creating bot

--- app/services/bots/create.rb
+++ app/services/bots/create.rb
@@ -1,12 +1,20 @@
 module Bots
   class Create < BaseService
-    # def initialize(subscription_validator: Subscriptions::Validator.new)
-    #   @subscription_validator = subscription_validator
-    # end
+    def initialize(
+      subscription_validator: lambda do |user|
+        if user.bots.count.positive? && user.subscription == 'free'
+          Result::Failure.new
+        else
+          Result::Success.new
+        end
+      end
+    )
+      @subscription_validator = subscription_validator
+    end
 
     def call(user, params)
-      # subscription_validation_result = @subscription_validator.call(user)
-      # return result if subscription_validation_result.failure?
+      subscription_validation_result = @subscription_validator.call(user)
+      return result if subscription_validation_result.failure?
 
       bot_params = params.merge(user: user)
       type = params.fetch(:bot_type)
