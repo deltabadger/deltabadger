8790214862ac8d9837764d5dc663cff4565cdaf5
Tomasz Balon <balontomasz@gmail.com>
2019-10-04 15:59:35 +0200

    Move newsletter to react

--- app/javascript/packs/guest2.jsx
+++ app/javascript/packs/guest2.jsx
@@ -1,8 +1,9 @@
+import Rails from 'rails-ujs';
 import React from 'react'
 import ReactDOM from 'react-dom'
-import Rails from 'rails-ujs';
 import style from '../deltabadger/styles/main.scss'
 import { CookieBanner } from '../deltabadger/components/CookieBanner';
+import { NewsletterForm } from '../deltabadger/components/NewsletterForm';
 
 require.context('../images', true)
 
@@ -13,6 +14,15 @@ if (document.getElementById('cookie_consent')) {
     ReactDOM.render(
       <CookieBanner />,
       document.getElementById('cookie_consent')
+    )
+  })
+}
+
+if (document.getElementById('newsletter')) {
+  document.addEventListener('DOMContentLoaded', () => {
+    ReactDOM.render(
+      <NewsletterForm />,
+      document.getElementById('newsletter')
     )
   })
 }
