5df01f726d45e01b7bfc3db5bd5054f6f18a0a07
Tomasz Balon <balontomasz@gmail.com>
2019-10-04 15:59:35 +0200

    Move newsletter to react

---
+++ app/javascript/deltabadger/components/NewsletterForm.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export const NewsletterForm = () => {
+  return (
+    <div>
+      <div className="form-group">
+        <input className="form-control" />
+      </div>
+      <div className="form-group">
+        <div className="btn btn-success">"Keep me in the loop!</div>
+      </div>
+    </div>
+  )
+}
