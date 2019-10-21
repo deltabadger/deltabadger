66f4fd2fc05b941397e8ac749dd587c954b0fda5
Jan Klosowski <jan@deltabadger.com>
2019-10-21 15:34:05 +0200

    add info paddings

--- app/javascript/deltabadger/components/BotDetails/Info.jsx
+++ app/javascript/deltabadger/components/BotDetails/Info.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 export const Info = ({ bot }) => (
-  <div className="tab-pane" id="info" role="tabpanel" aria-labelledby="info-tab">
-    Info
+  <div className="tab-pane pl-3 pr-3" id="info" role="tabpanel" aria-labelledby="info-tab">
+    <p>Info</p>
   </div>
 )
