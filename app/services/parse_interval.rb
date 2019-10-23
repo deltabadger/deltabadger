6a5d2ad36709db44a106a47f759feea44a489a7b
Tomasz Balon <balontomasz@gmail.com>
2019-10-23 13:16:57 +0200

    Improve intervals

--- app/services/parse_interval.rb
+++ app/services/parse_interval.rb
@@ -1,17 +1,12 @@
 class ParseInterval < BaseService
   Error = StandardError
 
-  INTERVALS = %w[month week day minutes].freeze
+  INTERVALS = %w[month week day hour minute].freeze
 
   def call(settings)
     interval = settings.fetch('interval')
     raise Error, 'Invalid interval' if !INTERVALS.include?(interval)
 
-    case interval
-    when 'minutes'
-      1.minutes
-    else
-      1.public_send(interval)
-    end
+    1.public_send(interval)
   end
 end
