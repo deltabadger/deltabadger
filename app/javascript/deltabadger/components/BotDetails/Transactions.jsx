c049803c094a3c20690f0c1e3f2a626d354f161c
Jan <jan@janklosowski.com>
2019-10-25 20:04:43 +0200

    statistics and date formats

--- app/javascript/deltabadger/components/BotDetails/Transactions.jsx
+++ app/javascript/deltabadger/components/BotDetails/Transactions.jsx
@@ -3,11 +3,37 @@ import React from 'react'
 export const Transactions = ({ bot }) => (
   <div className="tab-pane show active" id="stats" role="tabpanel" aria-labelledby="stats-tab">
 
-    <table className="table table-borderless table-sm db-table db-table--tx">
+    <table className="table table-borderless db-table">
+      <tbody>
+        <tr>
+          <td scope="col">Bought:</td>
+          <th scope="col">0.2123 BTC</th>
+        </tr>
+        <tr>
+          <td scope="col">Average price:</td>
+          <th scope="col">$7,584.22</th>
+        </tr>
+        <tr>
+          <td scope="col">Spent:</td>
+          <th scope="col">$1,610.00</th>
+        </tr>
+        <tr>
+          <td scope="col">Current value:</td>
+          <th scope="col">$1,883.70</th>
+        </tr>
+        <tr>
+          <td scope="col">Profit/Loss:</td>
+          <th scope="col" className="text-success">+$273.70 (+17%)</th>
+        </tr>
+      </tbody>
+    </table>
+
+    <div className="simple-chart-placeholder"></div>
+
+    <table className="table table-borderless table-striped db-table db-table--tx">
       <thead>
         <tr>
           <th scope="col">Date</th>
-          <th scope="col">Exchange</th>
           <th scope="col">Action</th>
           <th scope="col">Amount(BTC)</th>
           <th scope="col">Price({bot.settings.currency})</th>
@@ -16,8 +42,7 @@ export const Transactions = ({ bot }) => (
       <tbody>
         { bot.transactions.map(t => (
           <tr key={t.id} >
-            <th scope="row">{t.created_at}</th>
-            <td>{bot.exchangeName}</td>
+            <td scope="row">{t.created_at}</td>
             <td>{bot.settings.type}</td>
             <td>{t.amount || "N/A"}</td>
             <td>{t.price || "N/A"}</td>
