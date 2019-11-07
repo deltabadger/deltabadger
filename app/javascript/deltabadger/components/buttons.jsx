3fd18e6ea5094386756e1ac99e7a01e49d40d48f
Tomasz Balon <balontomasz@gmail.com>
2019-11-07 04:24:49 +0100

    Add modal to remove button

--- app/javascript/deltabadger/components/buttons.jsx
+++ app/javascript/deltabadger/components/buttons.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export const StartButton = ({onClick}) => (
   <div onClick={onClick} className="btn btn-success"><span>Start</span> <i className="material-icons">play_arrow</i></div>
@@ -7,15 +7,67 @@ export const StopButton = ({onClick}) => (
   <div onClick={onClick} className="btn btn-outline-primary"><span>Pause</span> <i className="material-icons">pause</i></div>
 )
 
-export const RemoveButton = ({onClick}) => (
-  <div
-    onClick={onClick}
-    className="btn btn-link btn--reset"
-  >
-    <i className="material-icons">sync</i>
-    <span>Reset</span>
-  </div>
-)
+// export const RemoveButton = ({onClick}) => (
+//   <div
+//     onClick={onClick}
+//     className="btn btn-link btn--reset"
+//   >
+//     <i className="material-icons">sync</i>
+//     <span>Reset</span>
+//   </div>
+// )
+
+export const RemoveButton = ({onClick}) => {
+  const [isOpen, setOpen] = useState(false)
+
+  return(
+    <div>
+    <div
+      onClick={() => setOpen(true) }
+      className="btn btn-link btn--reset"
+    >
+      <i className="material-icons">sync</i>
+      <span>Reset</span>
+    </div>
+
+    { isOpen &&
+      <div> MODAL
+      <div onClick={() => {onClick() && setOpen(false)}} className="btn btn-primary">Remove!!!!!</div>
+      <div onClick={() => {setOpen(false)}} className="btn btn-primary">CloseModal</div>
+      </div>
+    }
+    </div>
+  )
+}
+
+// export const RemoveButton = ({onClick}) => (
+//   <div>
+//     <div className="btn btn-link btn--reset" data-toggle="modal" data-target="#exampleModal" >
+//       <i className="material-icons">sync</i>
+//       <span>Reset</span>
+//     </div>
+
+//     <div className="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+//       <div className="modal-dialog" role="document">
+//         <div className="modal-content">
+//           <div className="modal-header">
+//             <h5 className="modal-title" id="exampleModalLabel">Modal title</h5>
+//             <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+//               <span aria-hidden="true">&times;</span>
+//             </button>
+//           </div>
+//           <div className="modal-body">
+//             ...
+//           </div>
+//           <div className="modal-footer">
+//             <button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
+//             <button onClick={onClick} type="button" className="btn btn-primary" data-dismiss="modal">Save changes</button>
+//           </div>
+//         </div>
+//       </div>
+//     </div>
+//   </div>
+// )
 
 export const CloseButton = ({onClick}) => (
   <div
