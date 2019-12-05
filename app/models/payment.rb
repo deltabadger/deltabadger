5b246829b92020e7347b7fdc8696a5d0f3d220eb
Tomasz Balon <balontomasz@gmail.com>
2019-12-05 18:49:34 +0100

    Add Payment model

---
+++ app/models/payment.rb
@@ -0,0 +1,6 @@
+class Payment < ApplicationRecord
+  belongs_to :user
+
+  enum currency: %i[USD EUR PLN]
+  enum status: %i[unpaid pending success failure]
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_02_014142) do
+ActiveRecord::Schema.define(version: 2019_12_05_041341) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,6 +44,17 @@ ActiveRecord::Schema.define(version: 2019_12_02_014142) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "payments", force: :cascade do |t|
+    t.string "payment_id"
+    t.integer "status"
+    t.decimal "total"
+    t.integer "currency"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_payments_on_user_id"
   end
 
   create_table "subscribers", force: :cascade do |t|
