54452c370cce0428eca49355fd38fe4ba12eb0bf
Tomasz Balon <7355928+balontomasz@users.noreply.github.com>
2019-12-11 16:00:15 +0100

    Send only 1 email when limit is almost reached

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_05_041341) do
+ActiveRecord::Schema.define(version: 2019_12_11_144804) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -77,6 +77,7 @@ ActiveRecord::Schema.define(version: 2019_12_05_041341) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "credits"
+    t.boolean "limit_almost_reached_sent", default: false
     t.index ["subscription_plan_id"], name: "index_subscriptions_on_subscription_plan_id"
     t.index ["user_id"], name: "index_subscriptions_on_user_id"
   end
