71a4b653b1c9b10d0fe38273f5ed71d99bee633f
Tomasz Balon <7355928+balontomasz@users.noreply.github.com>
2019-12-12 15:19:56 +0100

    Improve payment form

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_11_172138) do
+ActiveRecord::Schema.define(version: 2019_12_12_133134) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -54,6 +54,10 @@ ActiveRecord::Schema.define(version: 2019_12_11_172138) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "first_name"
+    t.string "last_name"
+    t.date "birth_date"
+    t.boolean "eu"
     t.index ["user_id"], name: "index_payments_on_user_id"
   end
 
