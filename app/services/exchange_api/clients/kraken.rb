c5dd495e36602550ae027e0fcf1e1ebe32e84997
Tomasz Balon <7355928+balontomasz@users.noreply.github.com>
2019-12-12 18:36:45 +0100

    Refactor errors mapping + kraken wip

--- app/services/exchange_api/clients/kraken.rb
+++ app/services/exchange_api/clients/kraken.rb
@@ -2,10 +2,11 @@ require 'kraken_ruby_client'
 
 module ExchangeApi
   module Clients
-    class Kraken
-      def initialize(api_key:, api_secret:)
+    class Kraken < ExchangeApi::Clients::Base
+      def initialize(api_key:, api_secret:, map_errors: ExchangeApi::MapErrors::Kraken.new)
         @client =
           ::Kraken::Client.new(api_key: api_key, api_secret: api_secret)
+        @map_errors = map_errors
       end
 
       def validate_credentials
@@ -16,10 +17,30 @@ module ExchangeApi
         true
       end
 
-      def buy(_)
-        puts 'Buying things on Kraken!'
+      def current_price(settings)
+        currency = settings.fetch('currency')
+        result = @client.ticker("xbt#{currency}")['result']["XXBTZ#{currency}"]
 
-        true
+        bid = result.fetch('b').first.to_f
+        ask = result.fetch('a').first.to_f
+
+        (bid + ask) / 2
+      end
+
+      def buy(settings)
+        currency = settings.fetch('currency')
+        price = settings.fetch('price')
+        pair = "XBT#{currency}"
+
+        response = @client.add_order(pair: pair, type: 'buy', ordertype: 'market', volume: 1)
+
+        if response.fetch("error").any?
+          Result::Failure.new(
+            *@map_errors.call(response.fetch('error'))
+          )
+        else
+          true
+        end
       end
 
       def sell(_)
