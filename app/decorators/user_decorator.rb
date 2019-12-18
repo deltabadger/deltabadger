9b43732ed068088ebe0fc245a6e450d95357f360
Tomasz Balon <balontomasz@gmail.com>
2019-12-18 03:16:18 +0100

    Hide limit banner on upgrade page

--- app/decorators/user_decorator.rb
+++ app/decorators/user_decorator.rb
@@ -1,5 +1,23 @@
 class UserDecorator < ActiveRecordDecorator
+  def initialize(user:, context:)
+    @context = context
+    super(user)
+  end
+
   def plan_days_left
     (subscription.end_time.to_date - Date.today).to_i
   end
+
+  def show_limit_reached_navbar?
+    limit_reached? && !action?('upgrade', 'index')
+  end
+
+  private
+
+  def action?(controller, action)
+    context.params[:controller] == controller &&
+      context.params[:action] == action
+  end
+
+  attr_reader :context
 end
