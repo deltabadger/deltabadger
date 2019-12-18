a2fa115ea387b986ec94ade2e3b3eccaadf0bb73
Tomasz Balon <balontomasz@gmail.com>
2019-12-18 12:35:37 +0100

    Changes to stats

--- app/services/presenters/api/stats.rb
+++ app/services/presenters/api/stats.rb
@@ -18,21 +18,22 @@ module Presenters
         current_price = api.current_price(bot.settings)*10
 
         transactions_amount_sum = transactions.sum(&:amount)
-        transactions_price_sum  = transactions.sum(&:price)
+        transactions_price_sum  = transactions.sum(&:price).ceil(5)
+
         average_price = transactions_price_sum / transactions.length
-        total_invested = transactions_amount_sum * average_price
+        total_invested = transactions_price_sum
         current_value  = transactions_amount_sum * current_price
         profit_loss = current_value - total_invested
         profit_loss_percentage = (1 - current_value/total_invested)*100
 
         {
           bought: "#{transactions_amount_sum.floor(2)} BTC",
-          totalInvested: "#{total_invested.floor(2)} USD",
-          averagePrice: "#{average_price.floor(2)} USD",
-          currentValue: "#{current_value.floor(2)} USD",
+          totalInvested: "#{total_invested.floor(2)} #{bot.currency}",
+          averagePrice: "#{average_price.floor(2)} #{bot.currency}",
+          currentValue: "#{current_value.floor(2)} #{bot.currency}",
           profitLoss: {
             positive: profit_loss.positive?,
-            value: "#{profit_loss.positive? ? '+' : '-'}#{profit_loss.floor(2).abs} USD (#{profit_loss.positive? ? '+' : '-'}#{profit_loss_percentage.floor(2).abs}%)"
+            value: "#{profit_loss.positive? ? '+' : '-'}#{profit_loss.floor(2).abs} #{bot.currency} (#{profit_loss.positive? ? '+' : '-'}#{profit_loss_percentage.floor(2).abs}%)"
           }
         }
       end
