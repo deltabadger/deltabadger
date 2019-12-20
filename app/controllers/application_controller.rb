69d8cf7601e7448e548ae877e575b4afe100fc4f
Jan <jan@janklosowski.com>
2019-12-20 21:50:08 +0100

    add terms and conditions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,6 +10,6 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer
-      .permit(:sign_up, keys: %i[terms_of_service updates_agreement])
+      .permit(:sign_up, keys: %i[terms_and_conditions updates_agreement])
   end
 end
