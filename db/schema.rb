13889aa2fd74fdd5d368bad1dd2b4a760c13b742
Tomasz Balon <7355928+balontomasz@users.noreply.github.com>
2020-01-01 19:40:48 +0100

    Fix and rename ToS field

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_27_141621) do
+ActiveRecord::Schema.define(version: 2020_01_01_183538) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -114,7 +114,7 @@ ActiveRecord::Schema.define(version: 2019_12_27_141621) do
     t.datetime "updated_at", null: false
     t.string "unconfirmed_email"
     t.boolean "admin", default: false, null: false
-    t.boolean "terms_of_service"
+    t.boolean "terms_and_conditions"
     t.boolean "updates_agreement"
     t.boolean "welcome_banner_showed", default: false
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
