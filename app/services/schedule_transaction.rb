3a2b7c1b38d51daec90caaee6cbd3efe06b8098e
Tomasz Balon <balontomasz@gmail.com>
2020-01-03 11:39:33 +0100

    Remove comments

--- app/services/schedule_transaction.rb
+++ app/services/schedule_transaction.rb
@@ -10,13 +10,7 @@ class ScheduleTransaction < BaseService
 
   def call(bot)
     interval = @parse_interval.call(bot)
-    puts  "INTERVAL: #{interval.inspect}"
     bot.reload
-    puts  "INTERVAL: #{@parse_interval.call(bot)}"
-    puts  "Time.now: #{Time.now}"
-    puts  "Interval from now #{interval.from_now}"
-    puts  "Interval from now timestamp #{interval.from_now.to_i}"
-    puts  "next_transaction_time_stamp #{interval.since(bot.last_transaction.created_at).to_i }"
     @make_transaction_worker.perform_at(interval.since(bot.last_transaction.created_at).to_i, bot.id)
   end
 end
