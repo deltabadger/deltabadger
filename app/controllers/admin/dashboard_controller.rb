1e70db69869236ac846e1d3b5652399ecfb47afb
Tomasz Balon <7355928+balontomasz@users.noreply.github.com>
2020-01-07 23:09:03 +0100

    Improve admin panel

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,10 +1,37 @@
 class Admin::DashboardController < Admin::ApplicationController
   def index
+    transations_by_exchange = Exchange.all.map(&present_exchange)
+
     render :index, locals: {
       number_of_all_users: User.count,
       number_of_unlimited_plans: SubscriptionsRepository.new.all_current_unlimited_count,
       number_of_all_bots: Bot.count,
-      number_of_all_transactions: Transaction.count
+      number_of_all_transactions: Transaction.count,
+      transations_by_exchange: transations_by_exchange
     }
+  end
+
+  private
+
+  ExchangeTransactions = Struct.new(
+    :name,
+    :successful_transactions,
+    :failed_transactions,
+    :total
+  )
+
+  def present_exchange
+    lambda do |exchange|
+      transactions_repository = TransactionsRepository.new
+      successful = transactions_repository.count_by_status_and_exchange(:success, exchange)
+      failed = transactions_repository.count_by_status_and_exchange(:failure, exchange)
+
+      ExchangeTransactions.new(
+        exchange.name.capitalize,
+        successful,
+        failed,
+        successful + failed
+      )
+    end
   end
 end
