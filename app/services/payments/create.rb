df67f15d7da13a3dd1c2db3a2f7b07f5531ebe95
Tomasz Balon <balontomasz@gmail.com>
2020-01-07 01:18:24 +0100

    Support diferent currencies in payment

--- app/services/payments/create.rb
+++ app/services/payments/create.rb
@@ -2,7 +2,8 @@ module Payments
   class Create < BaseService
     COST_EU = ENV.fetch('UNLIMITED_SUBSCRYPTION_COST_AMOUNT__EU')
     COST_OTHER = ENV.fetch('UNLIMITED_SUBSCRYPTION_COST_AMOUNT__OTHER')
-    CURRENCY = ENV.fetch('UNLIMITED_SUBSCRYPTION_COST_CURRENCY')
+    CURRENCY_EU = ENV.fetch('UNLIMITED_SUBSCRYPTION_COST_CURRENCY__EU')
+    CURRENCY_OTHER = ENV.fetch('UNLIMITED_SUBSCRYPTION_COST_CURRENCY__OTHER')
 
     def initialize(
       client: Payments::Client.new,
@@ -24,14 +25,14 @@ module Payments
       user = params.fetch(:user)
       payment_result = @client.create_payment(
         price: cost(payment),
-        currency: CURRENCY,
+        currency: currency(payment),
         email: user.email
       )
 
       if payment_result.success?
         @payments_repository.create(
           payment_result.data.slice(:payment_id, :status, :total)
-          .merge(currency: CURRENCY).merge(params)
+          .merge(currency: currency(payment)).merge(params)
         )
       end
 
@@ -39,6 +40,10 @@ module Payments
     end
 
     private
+
+    def currency(payment)
+      payment.eu? ? CURRENCY_EU : CURRENCY_OTHER
+    end
 
     def cost(payment)
       payment.eu? ? COST_EU : COST_OTHER
