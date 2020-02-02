b4de8114144fc1fd1819c97291754e39f1b4f9ab
Jan <jan@janklosowski.com>
2020-02-02 22:22:22 +0100

    add varela font

--- app/services/exchange_api/map_errors/kraken.rb
+++ app/services/exchange_api/map_errors/kraken.rb
@@ -2,7 +2,7 @@ module ExchangeApi::MapErrors
   class Kraken < ExchangeApi::MapErrors::Base
     def errors_mapping
       {
-        'EGeneral:Permission denied' => 'Permission denied. Check API settings.',
+        'EGeneral:Permission denied' => 'Insufficient permissions or unverified account.',
         'EOrder:Insufficient funds' => 'Insufficient funds',
         'EGeneral:Invalid arguments:volume' => 'Offer funds are not exceeding minimums'
       }
