cc58482223cb4f4b5c26a2f2140f17bba9b1f88c
Jan <jan@janklosowski.com>
2020-05-02 17:34:04 +0200

    various design changes

--- app/javascript/deltabadger/components/BotDetails/Transactions.jsx
+++ app/javascript/deltabadger/components/BotDetails/Transactions.jsx
@@ -75,9 +75,14 @@ export const Transactions = ({ bot, active }) => (
         ))}
       </tbody>
     </table>
-    <p className="db-smallinfo"><i className="material-icons-round">error_outline</i> The table above shows only the last ten transactions.<br/>The full report is available to download in CSV format below.</p>
+    <p className="db-smallinfo">
+      <svg class="db-svg-icon db-svg--inactive db-svg--table-disclaimer" xmlns="http://www.w3.org/2000/svg" width="24" viewBox="0 0 24 24"><path d="M12 7c.6 0 1 .5 1 1v4c0 .6-.5 1-1 1s-1-.5-1-1V8c0-.6.5-1 1-1zm0-5a10 10 0 100 20 10 10 0 000-20zm0 18a8 8 0 110-16 8 8 0 010 16zm1-3h-2v-2h2v2z"/></svg>
+      The table above shows only the last ten transactions.<br/>The full report is available to download in CSV format below.
+    </p>
     <div className="db-bot-info__footer">
-      <a href={`/api/bots/${bot.id}/transactions_csv`} className="btn btn-link btn--export-to-csv"><i className="material-icons-round mr-2">cloud_download</i><span> Download .csv</span> </a>
+      <a href={`/api/bots/${bot.id}/transactions_csv`} className="btn btn-link btn--export-to-csv">
+      <svg className="btn__svg-icon db-svg-icon db-svg-icon--download" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M19.35 10.04A7.49 7.49 0 0012 4C9.11 4 6.6 5.64 5.35 8.04A5.994 5.994 0 000 14c0 3.31 2.69 6 6 6h13c2.76 0 5-2.24 5-5 0-2.64-2.05-4.78-4.65-4.96zM17 13l-4.65 4.65c-.2.2-.51.2-.71 0L7 13h3V9h4v4h3z"/></svg>
+      <span className="ml-3"> Download .csv</span> </a>
     </div>
   </div>
 )
