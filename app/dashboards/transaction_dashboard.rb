59149f222d8ffb3affdffff8050df505aea0ac95
Grzegorz Wcisło <grzegorz.oskar.wcislo@gmail.com>
2020-05-19 12:29:01 +0200

    Apply rubocop offense autofix and fix/ignore the rest

--- app/dashboards/transaction_dashboard.rb
+++ app/dashboards/transaction_dashboard.rb
@@ -1,4 +1,4 @@
-require "administrate/base_dashboard"
+require 'administrate/base_dashboard'
 
 class TransactionDashboard < Administrate::BaseDashboard
   # ATTRIBUTE_TYPES
@@ -18,7 +18,7 @@ class TransactionDashboard < Administrate::BaseDashboard
     currency: Field::String.with_options(searchable: false),
     created_at: Field::DateTime,
     updated_at: Field::DateTime,
-    error_messages: Field::String,
+    error_messages: Field::String
   }.freeze
 
   # COLLECTION_ATTRIBUTES
@@ -27,41 +27,41 @@ class TransactionDashboard < Administrate::BaseDashboard
   # By default, it's limited to four items to reduce clutter on index pages.
   # Feel free to add, remove, or rearrange items.
   COLLECTION_ATTRIBUTES = %i[
-  bot
-  id
-  offer_id
-  rate
-  amount
+    bot
+    id
+    offer_id
+    rate
+    amount
   ].freeze
 
   # SHOW_PAGE_ATTRIBUTES
   # an array of attributes that will be displayed on the model's show page.
   SHOW_PAGE_ATTRIBUTES = %i[
-  bot
-  id
-  offer_id
-  rate
-  amount
-  market
-  status
-  currency
-  created_at
-  updated_at
-  error_messages
+    bot
+    id
+    offer_id
+    rate
+    amount
+    market
+    status
+    currency
+    created_at
+    updated_at
+    error_messages
   ].freeze
 
   # FORM_ATTRIBUTES
   # an array of attributes that will be displayed
   # on the model's form (`new` and `edit`) pages.
   FORM_ATTRIBUTES = %i[
-  bot
-  offer_id
-  rate
-  amount
-  market
-  status
-  currency
-  error_messages
+    bot
+    offer_id
+    rate
+    amount
+    market
+    status
+    currency
+    error_messages
   ].freeze
 
   # COLLECTION_FILTERS
