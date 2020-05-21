c1db79790d1b839c6b1ea9a13f85c8ebe4b521e2
Grzegorz Wcisło <grzegorz.wcislo@upsidelab.io>
2020-05-21 10:35:45 +0200

    Add CHF, GBP and CAD to Kraken

--- app/models/exchange.rb
+++ app/models/exchange.rb
@@ -1,7 +1,7 @@
 class Exchange < ApplicationRecord
   def currencies
     case name.downcase
-    when 'kraken' then %w[USD EUR]
+    when 'kraken' then %w[USD EUR CHF GBP CAD]
     when 'bitbay' then %w[USD EUR PLN]
     else
       %w[USD EUR]
