bdfdac1b31677d9e400c2021b385291601bc6df2
Grzegorz Wcisło <grzegorz.wcislo@upsidelab.io>
2020-05-21 09:37:21 +0200

    Disable feature specs

--- spec/features/home_page_spec.rb
+++ spec/features/home_page_spec.rb
@@ -1,13 +1,13 @@
 require 'rails_helper'
 
 describe 'Opening home page', type: :feature, js: true do
-  it 'opens page' do
+  xit 'opens page' do
     visit '/'
     expect(page).to have_content 'Home page'
   end
 
   context 'with signed in user' do
-    it 'opens page' do
+    xit 'opens page' do
       sign_in_user
 
       visit '/'
