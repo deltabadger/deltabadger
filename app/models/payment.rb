81cec26771ba0a9baf4b09f10cee8dbde55506eb
Grzegorz Wcisło <grzegorz.wcislo@upsidelab.io>
2020-05-22 12:13:48 +0200

    Fix globee status handling and add globee status tracking

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -2,6 +2,7 @@ class Payment < ApplicationRecord
   belongs_to :user
 
   enum currency: %i[USD EUR PLN]
+  # we only use unpaid, cancelled, paid
   enum status: %i[unpaid pending paid confirmed failure cancelled]
 
   validates :first_name, :last_name, :birth_date, :user, presence: true
