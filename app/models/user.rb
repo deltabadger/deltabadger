27f2016c0a05949b625e80822d7fb01c5c861433
Grzegorz Wcisło <grzegorz.wcislo@upsidelab.io>
2020-06-09 13:01:59 +0200

    Add wip affiliate form

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
 
+  has_one :affiliate
   has_many :api_keys
   has_many :exchanges, through: :api_keys
   has_many :bots
