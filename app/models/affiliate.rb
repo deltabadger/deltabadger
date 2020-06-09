fcc869a9d3cd26f71bf4ec258e4a8af87a8746be
Grzegorz Wcisło <grzegorz.wcislo@upsidelab.io>
2020-06-09 13:39:45 +0200

    Add bitcoin address validation

--- app/models/affiliate.rb
+++ app/models/affiliate.rb
@@ -1,6 +1,16 @@
 class Affiliate < ApplicationRecord
   belongs_to :user
 
-  validates :first_name, :last_name, :birth_date, :user, :btc_address, :code, presence: true
+  validates :first_name, :last_name, :birth_date, :user, :code, presence: true
   validates_inclusion_of :eu, in: [true, false]
+
+  validate :btc_address, :valid_btc_address
+
+  private
+
+  def valid_btc_address
+    return if ::Bitcoin.valid_address?(btc_address)
+
+    errors.add(:btc_address, 'has to be valid')
+  end
 end
