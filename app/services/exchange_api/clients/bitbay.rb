844a00bf68b0ac897f6b2f512721a9ea6f38eab1
Grzegorz Wcisło <grzegorz.wcislo@upsidelab.io>
2020-06-19 10:14:00 +0200

    Rename retry to recoverable

--- app/services/exchange_api/clients/bitbay.rb
+++ app/services/exchange_api/clients/bitbay.rb
@@ -31,7 +31,7 @@ module ExchangeApi
 
         Result::Success.new(BidAskPrice.new(bid, ask))
       rescue StandardError
-        Result::Failure.new('Could not fetch current price from Bitbay', data: { retry: true })
+        Result::Failure.new('Could not fetch current price from Bitbay', data: { recoverable: true })
       end
 
       def buy(settings)
@@ -66,7 +66,7 @@ module ExchangeApi
         response = JSON.parse(Faraday.post(url, body, headers(body)).body)
         parse_response(response)
       rescue StandardError
-        Result::Failure.new('Could not make Bitbay order', data: { retry: true })
+        Result::Failure.new('Could not make Bitbay order', data: { recoverable: true })
       end
 
       def parse_response(response)
@@ -98,7 +98,7 @@ module ExchangeApi
       def error_to_failure(error)
         mapped_error = @map_errors.call(error)
         Result::Failure.new(
-          *mapped_error.message, data: { retry: mapped_error.recoverable }
+          *mapped_error.message, data: { recoverable: mapped_error.recoverable }
         )
       end
     end
