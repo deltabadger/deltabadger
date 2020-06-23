c78831a3e0766e9bd4b6397887769a576df25b78
Grzegorz Wcisło <grzegorz.wcislo@upsidelab.io>
2020-06-23 10:00:03 +0200

    Add bot restart delay calculation specs

--- app/services/calculate_restart_delay.rb
+++ app/services/calculate_restart_delay.rb
@@ -1,8 +1,10 @@
 class CalculateRestartDelay
   BASE_DELAY = 15.minutes.freeze
 
-  def call(bot)
-    multiplier = 2**(bot.restarts - 1)
+  def call(restarts)
+    raise ArgumentError, 'restarts must be positive' if restarts < 1
+
+    multiplier = 2**(restarts - 1)
     BASE_DELAY * multiplier
   end
 end
