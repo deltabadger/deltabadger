73503d64a4a07ce1947b4573219de06c3fe4a016
Grzegorz Wcisło <grzegorz.wcislo@upsidelab.io>
2020-06-23 16:17:52 +0200

    Add delay compensation to bots

--- db/migrate/20200619114111_add_restart_to_bots.rb
+++ db/migrate/20200619114111_add_restart_to_bots.rb
@@ -1,5 +1,6 @@
 class AddRestartToBots < ActiveRecord::Migration[5.2]
   def change
     add_column :bots, :restarts, :integer, null: false, default: 0
+    add_column :bots, :delay, :integer, null: false, default: 0
   end
 end
