3e632c865bb677fc04274cd511ff046704321ed7
Grzegorz Wcisło <grzegorz.wcislo@upsidelab.io>
2020-06-25 13:12:36 +0200

    Fix transaction retry scheduling

--- app/services/schedule_transaction.rb
+++ app/services/schedule_transaction.rb
@@ -12,7 +12,11 @@ class ScheduleTransaction < BaseService
   end
 
   def call(bot)
-    decrement_bot_interval(bot, last_transaction_at, next_transaction_at)
+    if bot.restarts.zero? && bot.delay.positive?
+      bot = decrease_delay(bot)
+    elsif bot.restarts.positive?
+      bot = increase_delay(bot)
+    end
 
     next_transaction_at = next_bot_transaction_at.call(bot)
     make_transaction_worker.perform_at(
@@ -23,11 +27,18 @@ class ScheduleTransaction < BaseService
 
   private
 
-  def decrement_bot_interval(bot)
-    return unless bot.restarts.zero? && bot.delay.positive?
-
+  def decrease_delay(bot)
     interval = parse_interval.call(bot).to_i
-    bots_repository.update(bot.id, delay: [bot.delay - interval, 0].max)
+    new_delay = [bot.delay - bot.current_delay, 0].max
+    new_current_delay = [new_delay, interval].min
+    bots_repository.update(bot.id, delay: new_delay, current_delay: new_current_delay)
+  end
+
+  def increase_delay(bot)
+    last_transaction_at = bot.last_transaction.created_at
+    next_transaction_at = next_bot_transaction_at.call(bot)
+    new_delay = (next_transaction_at - last_transaction_at).to_i
+    bots_repository.update(bot.id, delay: bot.delay + new_delay)
   end
 
   attr_reader :bots_repository, :make_transaction_worker, :parse_interval, :next_bot_transaction_at
