a45ac416782354a22ebf59ce1f780771cf992d8d
Grzegorz Wcisło <grzegorz.wcislo@upsidelab.io>
2020-06-25 11:29:47 +0200

    Refactor intervals

--- app/services/schedule_transaction.rb
+++ app/services/schedule_transaction.rb
@@ -2,24 +2,33 @@ class ScheduleTransaction < BaseService
   def initialize(
     bots_repository: BotsRepository.new,
     make_transaction_worker: MakeTransactionWorker,
-    parse_interval: ParseInterval.new
+    parse_interval: ParseInterval.new,
+    next_bot_transaction_at: NextBotTransactionAt.new
   )
     @bots_repository = bots_repository
     @make_transaction_worker = make_transaction_worker
     @parse_interval = parse_interval
+    @next_bot_transaction_at = next_bot_transaction_at
   end
 
   def call(bot)
-    interval = parse_interval.call(bot)
-    bots_repository.update(bot.id, delay: [bot.delay - interval, 0].max) if bot.delay != 0
-    bot.reload
+    decrement_bot_interval(bot, last_transaction_at, next_transaction_at)
+
+    next_transaction_at = next_bot_transaction_at.call(bot)
     make_transaction_worker.perform_at(
-      interval.since(bot.last_transaction.created_at).to_i,
+      next_transaction_at,
       bot.id
     )
   end
 
   private
 
-  attr_reader :bots_repository, :make_transaction_worker, :parse_interval
+  def decrement_bot_interval(bot)
+    return unless bot.restarts.zero? && bot.delay.positive?
+
+    interval = parse_interval.call(bot)
+    bots_repository.update(bot.id, delay: [bot.delay - interval, 0].max)
+  end
+
+  attr_reader :bots_repository, :make_transaction_worker, :parse_interval, :next_bot_transaction_at
 end
