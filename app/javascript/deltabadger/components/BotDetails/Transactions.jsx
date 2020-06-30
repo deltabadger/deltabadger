3a26aaa439bd2f55814ff559aa40e4d6a21b36ee
Grzegorz Wcisło <grzegorz.oskar.wcislo@gmail.com>
2020-06-30 14:27:16 +0200

    Rename class to className in jsx

--- app/javascript/deltabadger/components/BotDetails/Transactions.jsx
+++ app/javascript/deltabadger/components/BotDetails/Transactions.jsx
@@ -76,7 +76,7 @@ export const Transactions = ({ bot, active }) => (
       </tbody>
     </table>
     <p className="db-smallinfo">
-      <svg class="db-svg-icon db-svg--inactive db-svg--table-disclaimer" xmlns="http://www.w3.org/2000/svg" width="24" viewBox="0 0 24 24"><path d="M12 7c.6 0 1 .5 1 1v4c0 .6-.5 1-1 1s-1-.5-1-1V8c0-.6.5-1 1-1zm0-5a10 10 0 100 20 10 10 0 000-20zm0 18a8 8 0 110-16 8 8 0 010 16zm1-3h-2v-2h2v2z"/></svg>
+      <svg className="db-svg-icon db-svg--inactive db-svg--table-disclaimer" xmlns="http://www.w3.org/2000/svg" width="24" viewBox="0 0 24 24"><path d="M12 7c.6 0 1 .5 1 1v4c0 .6-.5 1-1 1s-1-.5-1-1V8c0-.6.5-1 1-1zm0-5a10 10 0 100 20 10 10 0 000-20zm0 18a8 8 0 110-16 8 8 0 010 16zm1-3h-2v-2h2v2z"/></svg>
       The table above shows only the last ten transactions.<br/>The full report is available to download in CSV format below.
     </p>
     <div className="db-bot-info__footer">
