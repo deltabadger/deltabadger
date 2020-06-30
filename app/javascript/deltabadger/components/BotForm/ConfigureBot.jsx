9e7a8e8ca6e7211526e0718beabaa9c9ebc4ea74
Grzegorz Wcisło <grzegorz.wcislo@upsidelab.io>
2020-06-30 13:37:27 +0200

    Prevent bots from creating multiple times

--- app/javascript/deltabadger/components/BotForm/ConfigureBot.jsx
+++ app/javascript/deltabadger/components/BotForm/ConfigureBot.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 
-export const ConfigureBot = ({ currentExchange, handleReset, handleSubmit, errors }) => {
+export const ConfigureBot = ({ currentExchange, handleReset, handleSubmit, disable, errors }) => {
   const [type, setType] = useState("buy");
   const [price, setPrice] = useState("");
   const [currency, setCurrency] = useState("USD");
@@ -16,7 +16,7 @@ export const ConfigureBot = ({ currentExchange, handleReset, handleSubmit, error
     </div>
   )
 
-  const disableSubmit = price.trim() == ''
+  const disableSubmit = disable || price.trim() == ''
 
   const _handleSubmit = (evt) => {
     evt.preventDefault();
