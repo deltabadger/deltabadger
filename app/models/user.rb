b51a498722c77b68151e65878572a4ba7bd64198
Grzegorz Wcisło <grzegorz.oskar.wcislo@gmail.com>
2020-07-02 15:25:39 +0200

    Add affiliate links and referrer association

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable, :confirmable
 
   has_one :affiliate
+  belongs_to :referrer, class_name: 'Affiliate', optional: true
   has_many :api_keys
   has_many :exchanges, through: :api_keys
   has_many :bots
