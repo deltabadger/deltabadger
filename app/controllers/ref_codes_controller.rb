ae39c34c1c3c142e8b0bbafc1221fe16e81db606
Grzegorz Wcisło <grzegorz.wcislo@upsidelab.io>
2020-07-03 11:08:19 +0200

    Rubocop changes to ref codes

--- app/controllers/ref_codes_controller.rb
+++ app/controllers/ref_codes_controller.rb
@@ -3,15 +3,15 @@ class RefCodesController < ApplicationController
     affiliate = Affiliate.active.find_by(code: code)
     valid, flash = if affiliate
                      session[:referrer_id] = affiliate.id
-                     [true, { notice: "Applied affiliate code #{code}"}]
+                     [true, { notice: "Applied affiliate code #{code}" }]
                    else
-                     [false, { alert: "Invalid affiliate code #{code}"}]
+                     [false, { alert: "Invalid affiliate code #{code}" }]
                    end
 
     if !user_signed_in?
       redirect_to new_user_registration_path, flash: flash
     elsif current_user.referrer_id
-      redirect_to dashboard_path, flash: { alert: "You have already used an affiliate code" }
+      redirect_to dashboard_path, flash: { alert: 'You have already used an affiliate code' }
     elsif valid
       redirect_to ref_code_confirm_path, flash: flash
     else
