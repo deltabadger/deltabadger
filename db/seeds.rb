8ad627feb24df64f75e31b078ca4517162ef7a74
Grzegorz Wcisło <grzegorz.wcislo@upsidelab.io>
2020-07-17 09:20:20 +0200

    Add new subscription plans

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,8 +3,9 @@ Exchange.find_or_create_by!(name: 'Deribit')
 Exchange.find_or_create_by!(name: 'BitBay')
 Exchange.find_or_create_by!(name: 'BitClude')
 
-free = SubscriptionPlan.find_or_create_by!(name: 'free')
-unlimited = SubscriptionPlan.find_or_create_by!(name: 'unlimited')
+saver = SubscriptionPlan.find_or_create_by!(name: 'saver', cost_eu: 0, cost_other: 0, unlimited: false, years: 1, credits: 500)
+investor = SubscriptionPlan.find_or_create_by!(name: 'investor', cost_eu: 20, cost_other: 20, unlimited: true, years: 1, credits: 500)
+hodler = SubscriptionPlan.find_or_create_by!(name: 'hodler', cost_eu: 60, cost_other: 60, unlimited: true, years: 4, credits: 500)
 
 User.find_or_create_by(
   email: "test@test.com"
@@ -18,13 +19,13 @@ User.find_or_create_by(
 ) do |user|
   user.password = "polopolo"
   user.confirmed_at = user.confirmed_at || Time.now
-  user.subscriptions << Subscription.new(subscription_plan: unlimited, end_time: Time.now - 30.days)
+  user.subscriptions << Subscription.new(subscription_plan: investor, end_time: Time.now - 30.days)
 end
 
 User.find_or_create_by(
-  email: "unlimited@test.com"
+  email: "investor@test.com"
 ) do |user|
   user.password = "polopolo"
   user.confirmed_at = user.confirmed_at || Time.now
-  user.subscriptions << Subscription.new(subscription_plan: unlimited, end_time: Time.now + 1.year)
+  user.subscriptions << Subscription.new(subscription_plan: investor, end_time: Time.now + 1.year)
 end
