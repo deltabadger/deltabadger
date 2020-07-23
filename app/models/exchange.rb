06ef1535ae3f68c7a6a3bc93ac9cc8e07d515fa2
Grzegorz Wcisło <grzegorz.oskar.wcislo@gmail.com>
2020-07-23 10:58:01 +0200

    Remove BitClude and Deribit

--- app/models/exchange.rb
+++ app/models/exchange.rb
@@ -1,14 +1,12 @@
 class Exchange < ApplicationRecord
   KRAKEN_CURRENCIES = %w[USD USDT USDC AUD DAI EUR CHF GBP CAD].freeze
   BITBAY_CURRENCIES = %w[USD USDC EUR PLN].freeze
-  BITCLUDE_CURRENCIES = %w[USD EUR PLN GBP].freeze
   DEFAULT_CURRENCIES = %w[USD EUR].freeze
 
   def currencies
     case name.downcase
     when 'kraken' then KRAKEN_CURRENCIES
     when 'bitbay' then BITBAY_CURRENCIES
-    when 'bitclude' then BITCLUDE_CURRENCIES
     else DEFAULT_CURRENCIES
     end
   end
