d5e2e4499513a4a755d43cb3af38b33c0e03a476
Grzegorz Wcisło <grzegorz.oskar.wcislo@gmail.com>
2020-08-06 13:11:09 +0200

    Add Binance bot transactions

--- app/models/exchange.rb
+++ app/models/exchange.rb
@@ -1,12 +1,14 @@
 class Exchange < ApplicationRecord
   KRAKEN_CURRENCIES = %w[USD USDT USDC AUD DAI EUR CHF GBP CAD].freeze
   BITBAY_CURRENCIES = %w[USD USDC EUR PLN].freeze
+  BINANCE_CURRENCIES = %w[USDT USDC USDS BUSD TUSD EUR GBP AUD PAX TRY].freeze
   DEFAULT_CURRENCIES = %w[USD EUR].freeze
 
   def currencies
     case name.downcase
     when 'kraken' then KRAKEN_CURRENCIES
     when 'bitbay' then BITBAY_CURRENCIES
+    when 'binance' then BINANCE_CURRENCIES
     else DEFAULT_CURRENCIES
     end
   end
