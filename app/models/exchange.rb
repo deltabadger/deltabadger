6e0c2bb16138edb518594372d797e0adabdbde28
Grzegorz Wcisło <grzegorz.oskar.wcislo@gmail.com>
2020-08-07 09:52:35 +0200

    Fix exchange ruby code style

--- app/models/exchange.rb
+++ app/models/exchange.rb
@@ -1,14 +1,14 @@
 class Exchange < ApplicationRecord
-  KRAKEN_CURRENCIES = %w[USD USDT USDC AUD DAI EUR CHF GBP CAD].freeze
-  BITBAY_CURRENCIES = %w[USD USDC EUR PLN].freeze
   BINANCE_CURRENCIES = %w[USDT USDC USDS BUSD TUSD EUR GBP AUD PAX TRY].freeze
+  BITBAY_CURRENCIES = %w[USD USDC EUR PLN].freeze
+  KRAKEN_CURRENCIES = %w[USD USDT USDC AUD DAI EUR CHF GBP CAD].freeze
   DEFAULT_CURRENCIES = %w[USD EUR].freeze
 
   def currencies
     case name.downcase
-    when 'kraken' then KRAKEN_CURRENCIES
-    when 'bitbay' then BITBAY_CURRENCIES
     when 'binance' then BINANCE_CURRENCIES
+    when 'bitbay' then BITBAY_CURRENCIES
+    when 'kraken' then KRAKEN_CURRENCIES
     else DEFAULT_CURRENCIES
     end
   end
