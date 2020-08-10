b5f95dfe2f5f6c81204cd2ec2a0efc1bf9b10491
Jan <jan@janklosowski.com>
2020-08-10 16:42:39 +0200

    Binance info

--- app/javascript/deltabadger/components/BotForm/AddApiKey/BinanceInstructions.jsx
+++ app/javascript/deltabadger/components/BotForm/AddApiKey/BinanceInstructions.jsx
@@ -4,20 +4,18 @@ import React from 'react'
 export const BinanceInstructions = () => (
   <div className="db-exchange-instructions db-exchange-instructions--bitbay">
     <div className="alert alert-success mx-0" role="alert">
-      <b className="alert-heading mb-2">How to get API keys from Bitbay:</b>
+      <b className="alert-heading mb-2">How to get API keys from Binance:</b>
       <hr/>
       <ol>
-        <li>Login to your <a href="https://auth.bitbay.net/ref/Hhb7ZrAv2GrA" target="_blank" rel="noopener">Bitbay</a> account.</li>
-        <li>In user menu (top right corner) go to <b>API Settings</b>.</li>
-        <li>Press <b>Add New Key</b>.</li>
-        <li>Set permissions for:
-          <ul>
-            <li><b>Get orders and market configurations</b></li>
-            <li><b>Manage orders and change market configurations</b></li>
-          </ul>
-        </li>
-        <li>Press <b>Create</b>.</li>
-        <li>Copy and paste your new API keys into the form above.</li>
+        <li>Login to your <a href="https://auth.bitbay.net/ref/Hhb7ZrAv2GrA" target="_blank" rel="noopener">Binance</a> account.</li>
+        <li>In user menu (round icon in the top right corner) go to <b>API Management</b>.</li>
+        <li>Name the new API key by pressing "Create".</li>
+        <li>Confirm creation with one-time code(s).</li>
+        <li>Make sure that <b>Enable Spot & Margin Trading</b> is checked.</li>
+        <li>If you want to restrict access to the key only for Deltabadger, click <b>Edit restrictions</b>, check <b>Restrict access to trusted IPs only (Recommended)</b>, and add this IP in the next step: <b>3.18.70.231</b></li>
+        <li>Press <b>Save</b> and confirm with another one-time code if necessary.</li>
+        <li>Remember that the Secret Key is visible only during creation, so make sure you copy both keys before leaving the view.</li>
+        <li>Paste both keys into the inputs above.</li>
       </ol>
     </div>
   </div>
