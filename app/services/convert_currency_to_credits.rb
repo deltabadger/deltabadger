b882341201f126b267988a9629485a24418ac5c5
Grzegorz Wcisło <grzegorz.wcislo@upsidelab.io>
2020-08-11 13:18:13 +0200

    Change conversion rate 'direction'

--- app/services/convert_currency_to_credits.rb
+++ app/services/convert_currency_to_credits.rb
@@ -1,6 +1,6 @@
 class ConvertCurrencyToCredits < BaseService
   def call(amount:, currency:)
     conversion_rate = ConversionRate.find_by(currency: currency.downcase)&.rate || BigDecimal(1)
-    amount * conversion_rate
+    amount / conversion_rate
   end
 end
