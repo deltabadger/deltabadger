ee320509526e9605f92c5fd7ccaa862a5f5df2be
Grzegorz Wcisło <grzegorz.wcislo@upsidelab.io>
2020-08-12 13:45:14 +0200

    Prevent users from downgrading their plans

--- app/services/payments/validators/create.rb
+++ app/services/payments/validators/create.rb
@@ -1,14 +1,25 @@
 module Payments::Validators
   class Create
-    def call(payment)
+    def call(payment, user)
       if payment.valid?
-        Result::Success.new
+        if upgrade?(user.subscription.subscription_plan, payment.subscription_plan)
+          Result::Success.new
+        else
+          Result.new(
+            data: payment,
+            errors: ['Selected subscription plan is not available']
+          )
+        end
       else
         Result.new(
           data: payment,
           errors: payment.errors.full_messages
         )
       end
+    end
+
+    def upgrade?(old_plan, new_plan)
+      SubscriptionPlansRepository.new.upgrade?(old_plan, new_plan)
     end
   end
 end
