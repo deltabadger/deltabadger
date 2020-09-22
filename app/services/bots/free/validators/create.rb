31ef7f56d78a951a6d8cc0c92e397b90a08ec5d4
Karol Szuster <43988137+kshalot@users.noreply.github.com>
2020-09-22 15:49:03 +0200

    Add limit order forms for creation and edition

--- app/services/bots/free/validators/create.rb
+++ app/services/bots/free/validators/create.rb
@@ -16,18 +16,20 @@ module Bots::Free::Validators
     class BotSettings
       include ActiveModel::Validations
 
-      attr_reader :interval, :currency, :type, :order_type, :price, :allowed_currencies
+      attr_reader :interval, :currency, :type, :order_type, :price, :percentage, :allowed_currencies
 
       INTERVALS = %w[month week day hour].freeze
       TYPES = %w[buy sell].freeze
       ORDER_TYPES = %w[market limit].freeze
 
-      validates :interval, :currency, :type, :price, presence: true
+      validates :interval, :currency, :type, :order_type, :price, presence: true
       validate :allowed_currency
       validates :interval, inclusion: { in: INTERVALS }
       validates :type, inclusion: { in: TYPES }
       validates :order_type, inclusion: { in: ORDER_TYPES }
+      validate :percentage_present_if_limit_order
       validates :price, numericality: { only_float: true, greater_than: 0 }
+      validates :percentage, numericality: { only_float: true, greater_than: 0, smaller_than: 100 }
       validate :interval_within_limit
 
       def initialize(params, allowed_currencies)
@@ -36,6 +38,7 @@ module Bots::Free::Validators
         @type = params.fetch('type')
         @order_type = params.fetch('order_type')
         @price = params.fetch('price').to_f
+        @percentage = params.fetch('percentage').to_f
         @allowed_currencies = allowed_currencies
       end
 
@@ -55,6 +58,12 @@ module Bots::Free::Validators
         )
 
         errors.add(:base, result.errors.first) if result.failure?
+      end
+
+      def percentage_present_if_limit_order
+        return if order_type == 'limit' && percentage.present?
+
+        errors.add(:base, 'Specify percentage when creating a limit order')
       end
     end
   end
