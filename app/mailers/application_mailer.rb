b4f8296eb31164490b58c96adc87373d89f49b1e
Karol Szuster <43988137+kshalot@users.noreply.github.com>
2020-09-29 11:18:45 +0200

    Add DCA profit footer to mails

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,19 @@
 class ApplicationMailer < ActionMailer::Base
   default from: ENV['NOTIFICATIONS_SENDER']
   layout 'mailer'
+
+  def initialize(profit_calculator: GetDcaProfit.new)
+    super()
+    @dca_profit = calculate_profit(profit_calculator)
+  end
+
+  private
+
+  def calculate_profit(profit_calculator)
+    today = Date.today
+    profit_result = profit_calculator.call(today - 365, today)
+    return 0 unless profit_result.success?
+
+    profit_result.data.to_i
+  end
 end
