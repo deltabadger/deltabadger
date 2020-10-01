669bc594d9b8815b67acc4aa75faa5960cb332c0
Karol Szuster <karol.szuster@upsidelab.io>
2020-10-01 13:07:32 +0200

    Fix some validators not accepting keyword-only arguments

--- app/services/exchange_api/validators/bitbay/validator.rb
+++ app/services/exchange_api/validators/bitbay/validator.rb
@@ -6,7 +6,7 @@ module ExchangeApi
 
         URL = 'https://api.bitbay.net/rest/trading/history/transactions'.freeze
 
-        def validate_credentials(api_key, api_secret)
+        def validate_credentials(api_key:, api_secret:)
           request = Faraday.get(URL, {}, headers(api_key, api_secret, ''))
           return false if request.status != 200
 
