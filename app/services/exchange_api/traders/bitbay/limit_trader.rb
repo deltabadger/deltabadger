72edf76e76931a80eab8aecd198bf0f6d122709e
Karol Szuster <karol.szuster@upsidelab.io>
2020-10-02 13:23:02 +0200

    Fix undefined variable 'currency' in BitBay limit orders

--- app/services/exchange_api/traders/bitbay/limit_trader.rb
+++ app/services/exchange_api/traders/bitbay/limit_trader.rb
@@ -5,14 +5,14 @@ module ExchangeApi
     module Bitbay
       class LimitTrader < BaseTrader
         def buy(currency:, price:, percentage:)
-          buy_params = get_buy_params(price, percentage)
+          buy_params = get_buy_params(currency, price, percentage)
           return buy_params unless buy_params.success?
 
           place_order(currency, buy_params.data.to_json)
         end
 
         def sell(currency:, price:, percentage:)
-          sell_params = get_sell_params(price, percentage)
+          sell_params = get_sell_params(currency, price, percentage)
           return sell_params unless sell_params.success?
 
           place_order(currency, sell_params.data.to_json)
@@ -20,7 +20,7 @@ module ExchangeApi
 
         private
 
-        def get_buy_params(price, percentage)
+        def get_buy_params(currency, price, percentage)
           rate = current_ask_price(currency)
           return rate unless rate.success?
 
@@ -28,7 +28,7 @@ module ExchangeApi
           Result::Success.new(common_order_params(price).merge(offerType: 'buy', rate: limit_rate))
         end
 
-        def get_sell_params(price, percentage)
+        def get_sell_params(currency, price, percentage)
           rate = current_bid_price(currency)
           return rate unless rate.success?
 
