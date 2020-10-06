fbda5215de96ae6b57ec8515e643521e670c17b8
Karol Szuster <43988137+kshalot@users.noreply.github.com>
2020-10-06 12:44:06 +0200

    Add all filters to Binance market

--- app/services/exchange_api/markets/binance/market.rb
+++ app/services/exchange_api/markets/binance/market.rb
@@ -6,6 +6,7 @@ module ExchangeApi
 
         PRICE_FILTER = 'PRICE_FILTER'.freeze
         MIN_NOTIONAL = 'MIN_NOTIONAL'.freeze
+        LOT_SIZE = 'LOT_SIZE'.freeze
 
         def minimum_order_price(symbol)
           symbol_info = fetch_symbol_info(symbol)
@@ -43,15 +44,35 @@ module ExchangeApi
           Result::Success.new(found_symbol)
         end
 
+        def get_minimum_order_price(symbol_info)
+          price_filter = find_filter(symbol_info, PRICE_FILTER)
+          return 0 if price_filter.nil?
+
+          min_price = price_filter['minPrice']
+          min_price.to_f
+        end
+
+        def get_minimum_lot_size(symbol_info)
+          lot_filter = find_filter(symbol_info, LOT_SIZE)
+          return 0 if lot_filter.nil?
+
+          min_lot = lot_filter['minQty']
+          min_lot.to_f
+        end
+
         def get_minimum_quote_notional(symbol_info)
-          filters = symbol_info['filters']
-          notional_filter = filters.find do |filter|
-            filter['filterType'] == MIN_NOTIONAL
-          end
+          notional_filter = find_filter(symbol_info, MIN_NOTIONAL)
           return 0 if notional_filter.nil?
 
-          min_price = notional_filter['minPrice']
-          min_price.to_f
+          min_notional = notional_filter['minNotional']
+          min_notional.to_f
+        end
+
+        def find_filter(symbol_info, target_filter)
+          filters = symbol_info['filters']
+          filters.find do |filter|
+            filter['filterType'] == target_filter
+          end
         end
       end
     end
