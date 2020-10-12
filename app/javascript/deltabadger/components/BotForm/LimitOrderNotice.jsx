4342c4d07e6323106fca9041c17c79915ee41672
deltabadger <d@deltabadgersMBP.lan>
2020-10-12 08:07:08 +0200

    small input fixes

--- app/javascript/deltabadger/components/BotForm/LimitOrderNotice.jsx
+++ app/javascript/deltabadger/components/BotForm/LimitOrderNotice.jsx
@@ -3,8 +3,7 @@ import React from "react";
 export default () => {
   return (
     <small className="alert alert-warning db-alert--annotation">
-        <sup>*</sup> The limit order bot is an experimental feature. The bot opens orders but does not track if they have been filled. Our backtesting showed that for buy orders, 2.0% below the price worked optimal in the past. <a href="#">Read more</a>
+        <sup>*</sup> The limit order bot is an experimental feature. The bot opens orders, but does not track if they get filled. Our backtesting proved that for buy orders historically 2% gave the best results.
     </small>
   )
 }
-
