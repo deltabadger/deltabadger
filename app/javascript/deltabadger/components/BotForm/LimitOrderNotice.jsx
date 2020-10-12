e21c66f34433484afbd02d32ba7c79198b5f351c
deltabadger <d@deltabadgers-MacBook-Pro.local>
2020-10-12 13:16:43 +0200

    fixed info

--- app/javascript/deltabadger/components/BotForm/LimitOrderNotice.jsx
+++ app/javascript/deltabadger/components/BotForm/LimitOrderNotice.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default () => {
   return (
     <small className="alert alert-warning db-alert--annotation">
-        <sup>*</sup> The limit order bot is an experimental feature. The bot opens orders, but does not track if they get filled. Our backtesting proved that for buy orders historically 2% gave the best results.
+        The limit order bot is an experimental feature. The bot opens orders but does not track if they get filled. Our backtesting proved that historically for buy orders, ~2% gave the best results. Remember that it may take some time until those orders will get filled. Values below 0.5% should result in a relatively fast resolution, so it's an excellent place to start if you want to play with it. Limit bot DCA works the best with <i>smart intervals</i> when the number of transactions is larger.
     </small>
   )
 }
