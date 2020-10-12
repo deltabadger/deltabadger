9276babf1ad838409e67eed7055d21740810be43
Karol Szuster <karol.szuster@upsidelab.io>
2020-10-12 16:52:06 +0200

    Display all assets in configure bot dropdowns

--- app/services/exchange_api/traders/binance/market_trader.rb
+++ app/services/exchange_api/traders/binance/market_trader.rb
@@ -2,12 +2,14 @@ module ExchangeApi
   module Traders
     module Binance
       class MarketTrader < ExchangeApi::Traders::Binance::BaseTrader
-        def buy(symbol:, price:)
+        def buy(base:, quote:, price:)
+          symbol = @market.symbol(base, quote)
           buy_params = get_buy_params(symbol, price)
           place_order(buy_params)
         end
 
-        def sell(symbol:, price:)
+        def sell(base:, quote:, price:)
+          symbol = @market.symbol(base, quote)
           sell_params = get_sell_params(symbol, price)
           place_order(sell_params)
         end
