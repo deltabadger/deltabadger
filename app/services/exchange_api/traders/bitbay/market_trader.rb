1bbf7c6dffe72f97ee413310b0372f30b1a11ce9
Karol Szuster <43988137+kshalot@users.noreply.github.com>
2020-10-13 14:02:47 +0200

    Fix minor bugs in multiple cryptos

--- app/services/exchange_api/traders/bitbay/market_trader.rb
+++ app/services/exchange_api/traders/bitbay/market_trader.rb
@@ -4,23 +4,23 @@ module ExchangeApi
       class MarketTrader < ExchangeApi::Traders::Bitbay::BaseTrader
         def buy(base:, quote:, price:)
           symbol = @market.symbol(base, quote)
-          buy_params = get_buy_params(price)
+          buy_params = get_buy_params(symbol, price)
           place_order(symbol, buy_params)
         end
 
         def sell(base:, quote:, price:)
           symbol = @market.symbol(base, quote)
-          sell_params = get_sell_params(price)
+          sell_params = get_sell_params(symbol, price)
           place_order(symbol, sell_params)
         end
 
         private
 
-        def get_buy_params(price)
+        def get_buy_params(symbol, price)
           common_order_params.merge(offerType: 'buy', price: transaction_price(symbol, price))
         end
 
-        def get_sell_params(price)
+        def get_sell_params(symbol, price)
           common_order_params.merge(offerType: 'sell', price: transaction_price(symbol, price))
         end
 
