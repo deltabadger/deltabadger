eab2434f4d9cc4429fb55d78d5fefb62ccd406a4
matkaczmarek <mat_kaczmarek@outlook.com>
2020-10-19 14:52:00 +0200

    Move const variables to symbol utils

--- app/javascript/deltabadger/utils/symbols.js
+++ app/javascript/deltabadger/utils/symbols.js
@@ -5,3 +5,13 @@ export const renameSymbol = (s) => {
     return s.replace(/(^X|^Z)([A-Z]{3}[A-Z]*)/, "$2")
         .replace(/^XBT/, "BTC")
 }
+
+export const getSpecialSymbols = (name, isBase) => {
+    const shouldRenameSymbols = shouldRename(name)
+    const BTC = shouldRenameSymbols ? "XXBT" : "BTC"
+    const ETH = shouldRenameSymbols ? "XETH" : "ETH"
+    const EUR = shouldRenameSymbols ? "ZEUR" : "EUR"
+    const USD = shouldRenameSymbols ? "ZUSD" : "USD"
+
+    return isBase ? [BTC, ETH] : [EUR, USD]
+}
