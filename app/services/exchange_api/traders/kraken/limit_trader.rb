a1fe378447986fd400b7c56fa90856b0e4114f05
Karol Szuster <karol.szuster@upsidelab.io>
2020-11-10 14:44:19 +0100

    Sleep Kraken market order worker before querying closed orders

--- app/services/exchange_api/traders/kraken/limit_trader.rb
+++ app/services/exchange_api/traders/kraken/limit_trader.rb
@@ -22,6 +22,23 @@ module ExchangeApi
 
         private
 
+        def parse_response(response)
+          created_order = response.fetch('result')
+          offer_id = created_order.fetch('txid').first
+          order_data = orders.fetch(offer_id)
+          rate = if order_data.fetch('status') == 'open'
+                   order_data.fetch('descr').fetch('price').to_f
+                 else # closed
+                   order_data.fetch('price').to_f
+                 end
+          amount = order_data.fetch('vol').to_f
+          {
+            offer_id: offer_id,
+            rate: rate,
+            amount: amount
+          }
+        end
+
         def orders
           open_orders = @client.open_orders.dig('result', 'open')
           closed_orders = @client.closed_orders.dig('result', 'closed') # In case a limit order gets fulfilled automatically
