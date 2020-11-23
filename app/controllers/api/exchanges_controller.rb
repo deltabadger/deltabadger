547ceca5fa764eb2c68c0cf2df92f986b57a0cb9
mkaczmarek <mat_kaczmarek@outlook.com>
2020-11-23 15:14:24 +0100

    Fix rubocop problems.

--- app/controllers/api/exchanges_controller.rb
+++ app/controllers/api/exchanges_controller.rb
@@ -3,7 +3,7 @@ module Api
     def index
       build_data = lambda do |exchange|
         owned = current_user.exchanges.select(:id)
-        symbols_query = current_user.subscription_name == "hodler" ? exchange.symbols : exchange.non_hodler_symbols
+        symbols_query = current_user.subscription_name == 'hodler' ? exchange.symbols : exchange.non_hodler_symbols
         symbols = symbols_query.success? ? symbols_query.data : []
         {
           id: exchange.id,
