055b9a1e2cef15eb67ed32f53e19f8b3f560a687
Jan Klosowski <jan@deltabadger.com>
2020-12-25 23:53:28 +0100

    many small changes

--- app/javascript/deltabadger/components/BotDetails/Info.jsx
+++ app/javascript/deltabadger/components/BotDetails/Info.jsx
@@ -4,7 +4,7 @@ export const Info = ({ bot, active }) => (
   <div className={`tab-pane pl-4 pr-4 ${active ? 'active' : ''}`} id="info" role="tabpanel" aria-labelledby="info-tab">
     <div className="db-showif db-showif--pick-exchange">
       <p className="mt-2"><b>Exchanges</b></p>
-      <p>Deltabadger works with cryptoexchanges. At the moment, we support <a href="https://www.binance.com/en/register?ref=NUYVIP6R" target="_blank" rel="noopener">Binance</a>, <a href="https://auth.bitbay.net/ref/Hhb7ZrAv2GrA" target="_blank" rel="noopener" title="Bitbay">Bitbay</a> and <a href="https://r.kraken.com/deltabadger" target="_blank" rel="noopener" title="Kraken">Kraken</a>. We recommend Kraken as a reputable exchange that has never been hacked. Use Bitbay if you want to make your purchases in PLN.</p>
+      <p>Deltabadger works with cryptoexchanges. At the moment, we support <a href="https://www.binance.com/en/register?ref=NUYVIP6R" target="_blank" rel="nofollow">Binance</a>, <a href="https://auth.bitbay.net/ref/Hhb7ZrAv2GrA" target="_blank" rel="nofollow" title="Bitbay">Bitbay</a> and <a href="https://r.kraken.com/deltabadger" target="_blank" rel="nofollow" title="Kraken">Kraken</a>. We recommend Kraken as a reputable exchange that has never been hacked. Use Bitbay if you want to make your purchases in PLN.</p>
       <p>To make your bot useful, first, you need to verify your exchange account and transfer some funds there.</p>
       <p>However, don't keep your coins at any exchange too long. Login to your account and do regularly withdraws at least once a month.</p>
       <p>We will add more exchanges in the future. Let us know if you are interested in a particular one.</p>
@@ -22,9 +22,9 @@ export const Info = ({ bot, active }) => (
     <p className="mt-2"><b>New to dollar-cost averaging?</b></p>
     <p>Those links will help:</p>
     <ul className="mb-5">
-      <li><a href="https://www.youtube.com/watch?v=dltaIrhUUvY" target="_blank" rel="noopener">DCA for Beginners (video)</a></li>
-      <li><a href="https://medium.com/predict/the-power-of-dollar-cost-averaging-into-bitcoin-2fad7fb12ce6" target="_blank" rel="noopener">The power of DCA into Bitcoin</a></li>
-      <li><a href="https://dcabtc.com/" target="_blank" rel="noopener">DCA Simulator</a></li>
+      <li><a href="https://www.youtube.com/watch?v=dltaIrhUUvY" target="_blank" rel="nofollow">DCA for Beginners (video)</a></li>
+      <li><a href="https://medium.com/predict/the-power-of-dollar-cost-averaging-into-bitcoin-2fad7fb12ce6" target="_blank" rel="nofollow">The power of DCA into Bitcoin</a></li>
+      <li><a href="https://dcabtc.com/" target="_blank" rel="nofollow">DCA Simulator</a></li>
     </ul>
   </div>
 )
