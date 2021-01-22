56fae8564aaf7cd9f214a51d78629a4bcf260591
mkaczmarek <mat_kaczmarek@outlook.com>
2021-01-22 15:05:49 +0100

    Add unavailable pair error.

--- app/services/exchange_api/map_errors/coinbase.rb
+++ app/services/exchange_api/map_errors/coinbase.rb
@@ -2,7 +2,8 @@ module ExchangeApi::MapErrors
   class Coinbase < ExchangeApi::MapErrors::Base
     def errors_mapping
       {
-        'Insufficient funds' => Error.new('Insufficient funds', false)
+        'Insufficient funds' => Error.new('Insufficient funds', false),
+        'Trading pair not available' => Error.new('Pair not available', false)
       }.freeze
     end
   end
