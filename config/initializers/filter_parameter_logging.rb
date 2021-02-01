7c8bdff7234728924411f70134b88dcebbf35f9e
Karol Szuster <karol.szuster@upsidelab.io>
2021-02-01 09:10:04 +0100

    Fix 'passphrase' not being filtered in logs

--- config/initializers/filter_parameter_logging.rb
+++ config/initializers/filter_parameter_logging.rb
@@ -1,7 +1,7 @@
 # Be sure to restart your server when you modify this file.
 
 # Configure sensitive parameters which will be filtered from the log file.
-Rails.application.config.filter_parameters += [:password, :key, :secret]
+Rails.application.config.filter_parameters += %i[password key secret passphrase]
 
 Raven.configure do |config|
   config.sanitize_fields = Rails.application.config.filter_parameters.map(&:to_s)
