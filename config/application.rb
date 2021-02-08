33782123a46b5996fb429ed9a27d496974df6bf5
Grzegorz Wcisło <grzegorz.oskar.wcislo@gmail.com>
2021-02-08 10:25:43 +0100

    Start working on translations

--- config/application.rb
+++ config/application.rb
@@ -30,6 +30,10 @@ module Deltabadger
     # Don't generate system test files.
     config.generators.system_tests = nil
 
+    config.i18n.available_locales = [:en, :pl]
+    config.i18n.default_locale = :en
+    config.i18n.fallbacks = true
+
     config.to_prepare do
       Devise::Mailer.layout "mailer"
     end
