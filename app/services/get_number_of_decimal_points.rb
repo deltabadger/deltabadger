265dc258dee0e14648b5d1194ed06b56d5a0323e
mkaczmarek <mat_kaczmarek@outlook.com>
2021-02-12 13:22:47 +0100

    Change number of decimals function.

--- app/services/get_number_of_decimal_points.rb
+++ app/services/get_number_of_decimal_points.rb
@@ -1,9 +1,6 @@
 class GetNumberOfDecimalPoints < BaseService
   def call(number)
-    number_str = number.to_s
-    return 0 unless number_str.include? '.'
-
-    number_str.split('.')[1].length
+    -/.*e(.*)/.match(sprintf('%e', number))[1].to_i
   end
 end
 
