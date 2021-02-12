4179ca6342a5016713a374dc593d3186a5a24e49
mkaczmarek <mat_kaczmarek@outlook.com>
2021-02-12 13:22:47 +0100

    Change number of decimals function.

--- app/services/exchange_api/map_errors/ftx.rb
+++ app/services/exchange_api/map_errors/ftx.rb
@@ -2,7 +2,8 @@ module ExchangeApi::MapErrors
   class Ftx < ExchangeApi::MapErrors::Base
     def errors_mapping
       {
-        'Not enough balances' => Error.new('Insufficient funds', false)
+        'Not enough balances' => Error.new('Insufficient funds', false),
+        'Not approved to trade this product' => Error.new('Pair not available', false)
       }.freeze
     end
   end
