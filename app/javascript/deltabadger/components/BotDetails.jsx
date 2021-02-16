b8fa2ec1dc29e2adcb13d8b5c9ff9427ac42ef6f
Grzegorz Wcisło <grzegorz.wcislo@upsidelab.io>
2021-02-16 14:21:36 +0100

    Extract bot details tab translations

--- app/javascript/deltabadger/components/BotDetails.jsx
+++ app/javascript/deltabadger/components/BotDetails.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import I18n from 'i18n-js'
 import { Transactions } from './BotDetails/Transactions';
 import { Logs } from './BotDetails/Logs';
 import { Info } from './BotDetails/Info';
@@ -11,43 +12,55 @@ export const BotDetails = ({ bot }) => {
 
   const tabs = [
     {
-      label: "Statistics",
+      label: I18n.t('bots.details.stats.title'),
       active: statisticsActive,
       visible: isNotEmpty(bot.transactions),
       id: 'stats-tab',
+      tabpanelId: 'statistics',
     },
     {
-      label: "Log",
+      label: I18n.t('bots.details.log.title'),
       active: logActive,
       visible: isNotEmpty(bot.logs),
       id: 'log-tab',
+      tabpanelId: 'log',
     },
     {
-      label: "Info",
+      label: I18n.t('bots.details.info.title'),
       active: infoActive,
       visible: true,
       id: 'info-tab',
+      tabpanelId: 'info',
     }
   ];
 
 
-  const builTab = ({ label, active, visible, id }, index) => (
+  const buildTab = ({ label, active, id, tabpanelId }, index) => (
     <li className="nav-item" key={index}>
-      <a className={`nav-link ${active ? 'active' : ''}`} id={id} data-toggle="tab" href={`#${label.toLowerCase()}`} role="tab" aria-controls={label.toLowerCase()} aria-selected="true">{label}</a>
+      <a
+        className={`nav-link ${active ? 'active' : ''}`}
+        id={id}
+        data-toggle="tab"
+        href={`#${tabpanelId}`}
+        role="tab"
+        aria-controls={tabpanelId}
+        aria-selected={active}
+      >
+        {label}
+      </a>
     </li>
   )
 
   return (
     <div className="db-bots__item db-bots__item--data">
-      <ul className="nav nav-tabs" id="myTab" role="tablist">
-        {tabs.filter(e => e.visible).map(builTab)}
+      <ul className="nav nav-tabs" role="tablist">
+        {tabs.filter(e => e.visible).map(buildTab)}
       </ul>
-      <div className="tab-content" id="myTabContent">
+      <div className="tab-content">
         <Transactions bot={bot} active={statisticsActive} />
         <Logs bot={bot} active={logActive}/>
         <Info bot={bot} active={infoActive} />
       </div>
-
     </div>
   )
 }
