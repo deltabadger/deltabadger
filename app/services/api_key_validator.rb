362d9062294241fc211a797e210577800ca28971
Grzegorz Wcisło <grzegorz.wcislo@upsidelab.io>
2021-02-18 17:03:25 +0100

    Extract some error translations

--- app/services/api_key_validator.rb
+++ app/services/api_key_validator.rb
@@ -5,8 +5,8 @@ class ApiKeyValidator < BaseService
 
   def call(api_key)
     validator = @get_validator.call(api_key.exchange_id)
-    return Result::Failure.new('Invalid tokens') unless api_key.valid?
-    return Result::Failure.new('Invalid tokens') unless validator.validate_credentials(get_params(api_key))
+    return Result::Failure.new(I18n.t('errors.invalid_api_keys')) unless api_key.valid?
+    return Result::Failure.new(I18n.t('errors.invalid_api_keys')) unless validator.validate_credentials(get_params(api_key))
 
     Result::Success.new
   end
