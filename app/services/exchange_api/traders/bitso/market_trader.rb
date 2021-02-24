9dab324ed2cd22b8a8c0c2a0c379086eaf8ae4c8
matkaczmarek <mat_kaczmarek@outlook.com>
2021-02-24 17:08:04 +0100

    Refactor code.

--- app/services/exchange_api/traders/bitso/market_trader.rb
+++ app/services/exchange_api/traders/bitso/market_trader.rb
@@ -3,16 +3,14 @@ module ExchangeApi
     module Bitso
       class MarketTrader < ExchangeApi::Traders::Bitso::BaseTrader
         def buy(base:, quote:, price:, force_smart_intervals:)
-          symbol = @market.symbol(base, quote)
-          buy_params = get_buy_params(symbol, price, force_smart_intervals)
+          buy_params = get_params(base, quote, price, force_smart_intervals, 'buy')
           return buy_params unless buy_params.success?
 
           place_order(buy_params.data)
         end
 
         def sell(base:, quote:, price:, force_smart_intervals:)
-          symbol = @market.symbol(base, quote)
-          sell_params = get_sell_params(symbol, price, force_smart_intervals)
+          sell_params = get_params(base, quote, price, force_smart_intervals, 'sell')
           return sell_params unless sell_params.success?
 
           place_order(sell_params.data)
@@ -20,7 +18,19 @@ module ExchangeApi
 
         private
 
-        def get_buy_params(symbol, price, force_smart_intervals)
+        def get_params(base, quote, price, force_smart_intervals, side)
+          symbol = @market.symbol(base, quote)
+          common_params = common_order_params(symbol, price, force_smart_intervals)
+          return common_params unless common_params.success?
+
+          Result::Success.new(
+            common_params.data.merge(
+              side: side
+            )
+          )
+        end
+
+        def common_order_params(symbol, price, force_smart_intervals)
           quote_decimals = @market.quote_decimals(symbol)
           return quote_decimals unless quote_decimals.success?
 
@@ -28,30 +38,11 @@ module ExchangeApi
           return price_above_minimums unless price_above_minimums.success?
 
           Result::Success.new(
-            common_order_params(symbol).merge(
+            super(symbol).merge(
               minor: price_above_minimums.data.to_s,
-              side: 'buy'
+              type: 'market'
             )
           )
-        end
-
-        def get_sell_params(symbol, price, force_smart_intervals)
-          quote_decimals = @market.quote_decimals(symbol)
-          return quote_decimals unless quote_decimals.success?
-
-          price_above_minimums = transaction_price(symbol, price, force_smart_intervals)
-          return price_above_minimums unless price_above_minimums.success?
-
-          Result::Success.new(
-            common_order_params(symbol).merge(
-              minor: price_above_minimums.data.to_s,
-              side: 'sell'
-            )
-          )
-        end
-
-        def common_order_params(symbol)
-          super.merge(type: 'market')
         end
       end
     end
