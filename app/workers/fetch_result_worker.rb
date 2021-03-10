ca6834365b9490bbc30fefcf697d3d99fc798a5b
matkaczmarek <mat_kaczmarek@outlook.com>
2021-03-10 16:53:14 +0100

    Change fake trader.

--- app/workers/fetch_result_worker.rb
+++ app/workers/fetch_result_worker.rb
@@ -1,8 +1,8 @@
 class FetchResultWorker
   include Sidekiq::Worker
 
-  def perform(bot_id, offer_id, fixing_price)
-    FetchOrderResult.call(bot_id, offer_id, fixing_price)
+  def perform(bot_id, result_parameters, fixing_price)
+    FetchOrderResult.call(bot_id, result_parameters, fixing_price)
   rescue StandardError => e # prevent job from retrying
     Raven.capture_exception(e)
   end
