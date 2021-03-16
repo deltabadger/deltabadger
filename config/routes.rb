151b185e889637a80dd288fa743918ba758ddc61
matkaczmarek <mat_kaczmarek@outlook.com>
2021-03-16 15:39:26 +0100

    Set scope in routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,100 +1,102 @@
 require 'sidekiq/web'
 
 Rails.application.routes.draw do
-  namespace :settings do
-    get '/', action: :index
-    patch :hide_welcome_banner
-    patch :update_password
-    patch :update_email
-    delete 'remove_api_key/:id', action: :remove_api_key, as: :remove_api_key
-  end
-
-  namespace :upgrade do
-    get '/', action: :index
-    post :pay
-    get :payment_success
-    post :payment_callback
-  end
-
-  resource :affiliate, path: 'referral-program', only: [:new, :create, :show] do
-    get ':token/confirm_btc_address', action: 'confirm_btc_address', as: :confirm_btc_address
-    patch :update_visible_info
-    patch :update_btc_address
-  end
-
-  get '/ref/:code', to: 'ref_codes#apply_code', as: 'ref_code'
-  post '/ref/accept', to: 'ref_codes#accept'
-
-  namespace :admin do
-    resources :users, except: [:destroy]
-    resources :affiliates, except: [:destroy] do
-      get :wallet_csv, on: :collection
-      get :accounting_csv, on: :collection
-      post :mark_as_exported, on: :collection
-      post :mark_as_paid, on: :collection
+  scope "/:lang" do
+    namespace :settings do
+      get '/', action: :index
+      patch :hide_welcome_banner
+      patch :update_password
+      patch :update_email
+      delete 'remove_api_key/:id', action: :remove_api_key, as: :remove_api_key
     end
-    resources :api_keys, except: [:edit, :update]
-    resources :bots
-    resources :conversion_rates
-    resources :exchanges
-    resources :transactions
-    resources :subscriptions
-    resources :subscription_plans
-    resources :payments do
-      get :csv, on: :collection
+
+    namespace :upgrade do
+      get '/', action: :index
+      post :pay
+      get :payment_success
+      post :payment_callback
     end
-    resources :vat_rates
 
-    get :dashboard, to: 'dashboard#index'
-
-    root to: "dashboard#index"
-  end
-
-  post '/newsletter/add_email', to: 'newsletter#add_email'
-  namespace :api do
-    get '/subscriptions/check', to: 'subscriptions#check'
-    resources :api_keys, only: [:create]
-    resources :exchanges, only: [:index]
-    resources :bots, except: [:new, :edit] do
-      post :stop, on: :member
-      post :start, on: :member
-      get :transactions_csv, to: 'transactions#csv'
-      get 'charts/portfolio_value_over_time', to: 'charts#portfolio_value_over_time'
-      get :restart_params
+    resource :affiliate, path: 'referral-program', only: [:new, :create, :show] do
+      get ':token/confirm_btc_address', action: 'confirm_btc_address', as: :confirm_btc_address
+      patch :update_visible_info
+      patch :update_btc_address
     end
-  end
 
-  devise_for :users, skip: [:registrations]
+    get '/ref/:code', to: 'ref_codes#apply_code', as: 'ref_code'
+    post '/ref/accept', to: 'ref_codes#accept'
 
-  as :user do
-    scope :users do
-      get '/cancel', to: 'users/registrations#cancel', as: 'cancel_user_registration'
-      get '/sign_up', to: 'users/registrations#new', as: 'new_user_registration'
-      post '/', to: 'users/registrations#create', as: 'user_registration'
+    namespace :admin do
+      resources :users, except: [:destroy]
+      resources :affiliates, except: [:destroy] do
+        get :wallet_csv, on: :collection
+        get :accounting_csv, on: :collection
+        post :mark_as_exported, on: :collection
+        post :mark_as_paid, on: :collection
+      end
+      resources :api_keys, except: [:edit, :update]
+      resources :bots
+      resources :conversion_rates
+      resources :exchanges
+      resources :transactions
+      resources :subscriptions
+      resources :subscription_plans
+      resources :payments do
+        get :csv, on: :collection
+      end
+      resources :vat_rates
+
+      get :dashboard, to: 'dashboard#index'
+
+      root to: "dashboard#index"
     end
+
+    post '/newsletter/add_email', to: 'newsletter#add_email'
+    namespace :api do
+      get '/subscriptions/check', to: 'subscriptions#check'
+      resources :api_keys, only: [:create]
+      resources :exchanges, only: [:index]
+      resources :bots, except: [:new, :edit] do
+        post :stop, on: :member
+        post :start, on: :member
+        get :transactions_csv, to: 'transactions#csv'
+        get 'charts/portfolio_value_over_time', to: 'charts#portfolio_value_over_time'
+        get :restart_params
+      end
+    end
+
+    devise_for :users, skip: [:registrations]
+
+    as :user do
+      scope :users do
+        get '/cancel', to: 'users/registrations#cancel', as: 'cancel_user_registration'
+        get '/sign_up', to: 'users/registrations#new', as: 'new_user_registration'
+        post '/', to: 'users/registrations#create', as: 'user_registration'
+      end
+    end
+
+    root to: 'home#index'
+
+    get '/dashboard', to: 'home#dashboard', as: :dashboard
+    get '/terms-and-conditions', to: 'home#terms_and_conditions', as: :terms_and_conditions
+    get '/privacy-policy', to: 'home#privacy_policy', as: :privacy_policy
+    get '/cookies-policy', to: 'home#cookies_policy', as: :cookies_policy
+    get '/contact', to: 'home#contact', as: :contact
+    get '/about', to: 'home#about', as: :about
+    get '/referral-program', to: 'home#referral_program', as: :referral_program
+    get '/thank-you', to: 'home#confirm_registration', as: :confirm_registration
+    get '/cryptocurrency-dollar-cost-averaging', to: 'home#dollar_cost_averaging', as: :dollar_cost_averaging
+
+    get '/terms_and_conditions' => redirect('/terms-and-conditions')
+    get '/privacy_policy' => redirect('/privacy-policy')
+    get '/cookies_policy' => redirect('/cookies-policy')
+    get '/referral_program' => redirect('/referral-program')
+    get '/sitemap' => 'sitemap#index', :defaults => { :format => 'xml' }
+
+    authenticate :user, lambda { |u| u.admin? } do
+      mount Sidekiq::Web => '/sidekiq'
+    end
+
+    get '*path' => redirect('/')
   end
-
-  root to: 'home#index'
-
-  get '/dashboard', to: 'home#dashboard', as: :dashboard
-  get '/terms-and-conditions', to: 'home#terms_and_conditions', as: :terms_and_conditions
-  get '/privacy-policy', to: 'home#privacy_policy', as: :privacy_policy
-  get '/cookies-policy', to: 'home#cookies_policy', as: :cookies_policy
-  get '/contact', to: 'home#contact', as: :contact
-  get '/about', to: 'home#about', as: :about
-  get '/referral-program', to: 'home#referral_program', as: :referral_program
-  get '/thank-you', to: 'home#confirm_registration', as: :confirm_registration
-  get '/cryptocurrency-dollar-cost-averaging', to: 'home#dollar_cost_averaging', as: :dollar_cost_averaging
-
-  get '/terms_and_conditions' => redirect('/terms-and-conditions')
-  get '/privacy_policy' => redirect('/privacy-policy')
-  get '/cookies_policy' => redirect('/cookies-policy')
-  get '/referral_program' => redirect('/referral-program')
-  get '/sitemap' => 'sitemap#index', :defaults => { :format => 'xml' }
-
-  authenticate :user, lambda { |u| u.admin? } do
-    mount Sidekiq::Web => '/sidekiq'
-  end
-
-  get '*path' => redirect('/')
 end
