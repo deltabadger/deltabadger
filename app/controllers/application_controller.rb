b050887ad62a0fd2234ef4e88637a1f60f319475
mkaczmarek <mat_kaczmarek@outlook.com>
2021-03-17 15:57:07 +0100

    Better redirecting.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,7 @@ class ApplicationController < ActionController::Base
 
   def set_locale
     I18n.locale = extract_locale || I18n.default_locale
+    params[:lang] = I18n.locale
   end
 
   protected
