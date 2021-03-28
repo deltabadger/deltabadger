3366826847ba290120569ad622e672cf424c27c6
Jan Kłosowski <jklosowski@g2a.com>
2021-03-28 05:35:08 +0200

    relative size

--- app/javascript/deltabadger/components/Spinner.jsx
+++ app/javascript/deltabadger/components/Spinner.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 
 export const Spinner = () => (
   <div className="db-spinner-positioner">
-    <svg className="spinner" width="20px" height="20px" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+    <svg className="spinner" width="2rem" height="2rem" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
       <circle className="path" fill="none" strokeWidth="4" strokeLinecap="round" cx="12" cy="12" r="10"></circle>
     </svg>
   </div>
