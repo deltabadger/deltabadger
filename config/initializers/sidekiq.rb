20e3ddb6552b096319cb6d7cf5269b33047be44a
matkaczmarek <mat_kaczmarek@outlook.com>
2021-04-12 16:57:51 +0200

    Change redis url.

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,8 +1,8 @@
 Sidekiq.configure_server do |config|
   config.average_scheduled_poll_interval = 1
-  config.redis = { url: 'redis-staging.qtxguz.ng.0001.use2.cache.amazonaws.com:6379' }
+  config.redis = { url: 'redis://redis-staging.qtxguz.ng.0001.use2.cache.amazonaws.com:6379' }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: 'redis-staging.qtxguz.ng.0001.use2.cache.amazonaws.com:6379' }
+  config.redis = { url: 'redis://redis-staging.qtxguz.ng.0001.use2.cache.amazonaws.com:6379' }
 end
