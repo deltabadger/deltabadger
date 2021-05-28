e864ffa56cb19bf2067c564c272e5236e5c1c2af
mkaczmarek <mat_kaczmarek@outlook.com>
2021-05-28 14:48:42 +0200

    Rollback routing by ip in case of btc-pay-server.

--- app/services/notifications/fomo_events.rb
+++ app/services/notifications/fomo_events.rb
@@ -10,11 +10,12 @@ module Notifications
       @client = Fomo.new(AUTHORIZATION_TOKEN)
     end
 
-    def plan_bought(first_name:, ip_address:, plan_name:)
+    def plan_bought(first_name:, country: nil, ip_address: nil, plan_name:)
       event = FomoEvent.new
       event.event_type_id = plan_name == 'hodler' ? HODLER_TEMPLATE_ID : INVESTOR_TEMPLATE_ID
       event.first_name = first_name
-      event.ip_address = ip_address
+      event.ip_address = ip_address unless ip_address.nil?
+      event.country = country unless country.nil?
 
       @client.create_event(event)
     end
--- app/services/payments/update.rb
+++ app/services/payments/update.rb
@@ -47,7 +47,7 @@ module Payments
 
       @fomo_notifications.plan_bought(
         first_name: payment.first_name,
-        ip_address: params.fetch('ip_address', params[:ip_address]),
+        country: payment.country,
         plan_name: payment.subscription_plan.name
       )
     end
