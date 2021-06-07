ec825fe8f92b93b69c873ab03d3d12678e27de35
mkaczmarek <mat_kaczmarek@outlook.com>
2021-06-07 12:39:48 +0200

    Make migration automatic

--- db/migrate/20210601134055_add_two_factor_authorization_to_users.rb
+++ db/migrate/20210601134055_add_two_factor_authorization_to_users.rb
@@ -2,5 +2,7 @@ class AddTwoFactorAuthorizationToUsers < ActiveRecord::Migration[5.2]
   def change
     add_column :users, :otp_secret_key, :string
     add_column :users, :otp_module, :integer, default: 0
+
+    Rake::Task['update_users_with_otp_secret_key'].invoke
   end
 end
