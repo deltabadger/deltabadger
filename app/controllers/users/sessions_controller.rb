1763c2156908dd756bc354c116fd08deca1fba9e
mkaczmarek <mat_kaczmarek@outlook.com>
2021-06-07 12:45:44 +0200

    Refactor code

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -6,22 +6,19 @@ class Users::SessionsController < Devise::SessionsController
       if params[:user][:otp_code_token].empty?
         continue_sign_in(resource, resource_name)
       else
-        sign_out resource
-        redirect_to new_user_session_path, alert: I18n.t('errors.messages.bad_credentials')
+        abort_sign_in(I18n.t('errors.messages.bad_credentials'))
       end
 
     elsif resource&.otp_module_enabled?
-      if params[:user][:otp_code_token].size.positive?
+      if params[:user][:otp_code_token].size.present?
         if resource.authenticate_otp(params[:user][:otp_code_token], drift: 60)
           continue_sign_in(resource, resource_name)
         else
-          sign_out resource
-          redirect_to new_user_session_path, alert: I18n.t('errors.messages.bad_credentials')
+          abort_sign_in(I18n.t('errors.messages.bad_credentials'))
         end
 
       else
-        sign_out resource
-        redirect_to new_user_session_path, alert: I18n.t('errors.messages.empty_two_fa_token')
+        abort_sign_in(I18n.t('errors.messages.empty_two_fa_token'))
       end
 
     end
@@ -34,5 +31,10 @@ class Users::SessionsController < Devise::SessionsController
     sign_in(resource_name, resource)
     yield resource if block_given?
     respond_with resource, location: after_sign_in_path_for(resource)
+  end
+
+  def abort_sign_in(error_message)
+    sign_out resource
+    redirect_to new_user_session_path, alert: error_message
   end
 end
