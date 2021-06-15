0cdbf8426299b28304c34df87ad1c4800575c81c
mkaczmarek <mat_kaczmarek@outlook.com>
2021-06-15 16:12:28 +0200

    Add btcBoughtDayAgo metric.

--- app/controllers/metrics_controller.rb
+++ app/controllers/metrics_controller.rb
@@ -8,6 +8,7 @@ class MetricsController < ApplicationController
       btcBoughtDayAgo: convert_to_satoshis(TransactionsRepository.new.total_btc_bought_day_ago)
     }
 
+    # FIXME: Move to CORS before release
     response.set_header('Access-Control-Allow-Origin', '*')
     render json: { data: output_params }.to_json
   end
