0bc56adc9777e85c527027d7014450c5b79125bf
mkaczmarek <mat_kaczmarek@outlook.com>
2021-06-18 13:24:07 +0200

    Fix Cors and move telegram fetching to service.

--- app/controllers/metrics_controller.rb
+++ app/controllers/metrics_controller.rb
@@ -2,18 +2,13 @@ require 'json'
 
 class MetricsController < ApplicationController
   def index
-    telegram_token = ENV.fetch('TELEGRAM_BOT_TOKEN')
-    telegram_group_id = ENV.fetch('TELEGRAM_GROUP_ID')
-    url = "https://api.telegram.org/bot#{telegram_token}/getChatMembersCount?chat_id=#{telegram_group_id}"
-    request = Faraday.get(url)
-    res = JSON.parse(request.body)
+    telegram_metrics = FetchTelegramMetrics.new.call
 
     output_params = {
       liveBots: BotsRepository.new.count_with_status('working'),
       btcBought: convert_to_satoshis(TransactionsRepository.new.total_btc_bought),
-      btcBoughtDayAgo: convert_to_satoshis(TransactionsRepository.new.total_btc_bought_day_ago),
-      membersCounter: res.fetch('result')
-    }
+      btcBoughtDayAgo: convert_to_satoshis(TransactionsRepository.new.total_btc_bought_day_ago)
+    }.merge(telegram_metrics)
 
     render json: { data: output_params }.to_json
   end
