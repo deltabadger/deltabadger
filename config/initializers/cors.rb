6a0b91ac1efdbdf09663f8030fd9fa0868c6ed8a
mkaczmarek <mat_kaczmarek@outlook.com>
2021-06-18 15:12:19 +0200

    Add localhost to origins.

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,7 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://deltabadger.com'
+    origins 'https://deltabadger.com', /localhost:\d{4}/, /127\.0\.0\.1:\d{4}/,
+            'deltabadger.com'
     resource '/metrics', headers: :any, methods: :get
   end
 end
