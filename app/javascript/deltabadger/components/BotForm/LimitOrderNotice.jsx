3a0f292098098f5c0bf5a027bdb7998720c295a2
Jan Kłosowski <jklosowski@g2a.com>
2021-06-23 07:33:29 +0200

    bot option styles

--- app/javascript/deltabadger/components/BotForm/LimitOrderNotice.jsx
+++ app/javascript/deltabadger/components/BotForm/LimitOrderNotice.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import I18n from 'i18n-js'
 
 export default () => (
-  <div className="pt-3 pb-3">
+  <div className="">
     <br></br>
     <sup>*</sup> {I18n.t('bots.setup.limit_warning')}
   </div>
