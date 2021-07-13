29cdc73acc8a058cfc4bd2a9680c3ca8f99ead5e
mkaczmarek <mat_kaczmarek@outlook.com>
2021-07-13 11:05:45 +0200

    Mark canceled order as failed on Coinbase Pro.

--- app/services/exchange_api/map_errors/coinbase_pro.rb
+++ app/services/exchange_api/map_errors/coinbase_pro.rb
@@ -3,7 +3,8 @@ module ExchangeApi::MapErrors
     def errors_mapping
       {
         'Insufficient funds' => Error.new('Insufficient funds', false),
-        'Trading pair not available' => Error.new('Pair not available', false)
+        'Trading pair not available' => Error.new('Pair not available', false),
+        'Order was canceled' => Error.new('Order was canceled', true)
       }.freeze
     end
   end
