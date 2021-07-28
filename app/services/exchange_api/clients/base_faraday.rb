bd19989a156cd72ab0ff3b2ffe23cb03238504cf
Witold Kęsek <49924891+witoldkesek@users.noreply.github.com>
2021-07-28 16:31:53 +0200

    All cachable API market calls are now cached

--- app/services/exchange_api/clients/base_faraday.rb
+++ app/services/exchange_api/clients/base_faraday.rb
@@ -10,8 +10,7 @@ module ExchangeApi
       def caching_client(url_base, expire_time = ENV['DEFAULT_MARKET_CACHING_TIME'])
         Faraday.new(url: url_base) do |builder|
           builder.use :manual_cache,
-                      expires_in: expire_time,
-                      logger: Rails.logger
+                      expires_in: expire_time
           builder.adapter Faraday.default_adapter
         end
       end
