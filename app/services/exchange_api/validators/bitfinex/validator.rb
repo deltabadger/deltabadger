f3d9a9a86dd718f6edc22b15c8700a19df7a898f
mkaczmarek <mat_kaczmarek@outlook.com>
2021-08-09 14:44:49 +0200

    Integrate frontend with Bitfinex.

--- app/services/exchange_api/validators/bitfinex/validator.rb
+++ app/services/exchange_api/validators/bitfinex/validator.rb
@@ -4,7 +4,7 @@ module ExchangeApi
       class Validator < BaseValidator
         include ExchangeApi::Clients::Bitfinex
         FAKE_ORDER_ID = '9' * 10
-        ORDER_NOT_FOUND_CODE = 10001
+        ORDER_NOT_FOUND_MESSAGE = 'id: invalid'.freeze
 
         def validate_credentials(api_key:, api_secret:)
           path = '/auth/w/order/cancel'
@@ -14,9 +14,8 @@ module ExchangeApi
           request = Faraday.post(url, body.to_json, headers(api_key, api_secret, body, path))
           response = JSON.parse(request.body)
 
-          response[1] == ORDER_NOT_FOUND_CODE
-        rescue StandardError => e
-          puts e
+          response[2] == ORDER_NOT_FOUND_MESSAGE
+        rescue StandardError
           false
         end
       end
