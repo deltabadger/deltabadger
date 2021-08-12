fe65b5c815b07dcc38d6f25d8ea3fc4b0e7e09e8
witoldkesek <witoldkesek@gmail.com>
2021-08-12 11:24:17 +0200

    Top 10 bots are now updated from routing /top_bots_update and kept in cache for 25 hours.

--- app/controllers/metrics_controller.rb
+++ app/controllers/metrics_controller.rb
@@ -1,6 +1,7 @@
 require 'json'
 
 class MetricsController < ApplicationController
+  TOP_BOTS_KEY = 'TOP_BOTS_CACHE_KEY'.freeze
   def index
     telegram_metrics = FetchTelegramMetrics.new.call
 
@@ -14,21 +15,38 @@ class MetricsController < ApplicationController
   end
 
   def top_ten_bots
+    render json: Rails.cache.exist?(TOP_BOTS_KEY) ? Rails.cache.read(TOP_BOTS_KEY).to_json : top_bots_update
+  end
+
+  def top_bots_update
     top_ten_bots = BotsRepository.new.list_top_ten
-    bots_array = top_ten_bots.map do |key, value|
+    new_top_bots = top_ten_bots.map do |key, value|
       BotCount.new(key, value)
     end
-    render json: bots_array.to_json
+    old_top_bots = Rails.cache.exist?(TOP_BOTS_KEY) ? Rails.cache.read(TOP_BOTS_KEY) : new_top_bots
+    new_top_bots.each_with_index do |new_bot, new_index|
+      old_top_bots.each_with_index do |old_bot, old_index|
+        next unless new_bot.name == old_bot.name
+
+        new_bot.is_up = true if new_index < old_index
+      end
+    end
+    Rails.cache.write(TOP_BOTS_KEY, new_top_bots, expires_in: 25.hour)
+    render json: new_top_bots.to_json
   end
 
   private
 
   class BotCount
+    attr_reader :name, :count, :is_up
+    attr_writer :is_up
     def initialize(name, count, is_up = false)
       @name = name
       @count = count
       @is_up = is_up
     end
+
+
   end
 
   def convert_to_satoshis(amount)
