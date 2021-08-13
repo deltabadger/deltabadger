6b3fbe402065641031303e462317832539cd2987
Witold Kęsek <49924891+witoldkesek@users.noreply.github.com>
2021-08-13 15:36:47 +0200

    Added Telegram Bot feature.

--- Gemfile
+++ Gemfile
@@ -31,6 +31,7 @@ gem 'sentry-raven'
 gem 'bitcoin-ruby', git: 'https://github.com/lian/bitcoin-ruby', branch: 'master', require: 'bitcoin'
 gem 'recaptcha'
 gem 'i18n-js', '~> 3.8.0'
+gem 'telegram-bot', '~> 0.14.4'
 
 group :development, :test do
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- app/repositories/bots_repository.rb
+++ app/repositories/bots_repository.rb
@@ -3,6 +3,9 @@ class BotsRepository < BaseRepository
     'XDG': 'DOGE',
     'XBT': 'BTC'
   }.freeze
+
+  TOP_BOTS_KEY = 'TOP_BOTS_CACHE_KEY'.freeze
+
   def by_id_for_user(user, id)
     user.bots.without_deleted.find(id)
   end
@@ -29,7 +32,41 @@ class BotsRepository < BaseRepository
     Bot
   end
 
+  def top_ten_bots
+    Rails.cache.exist?(TOP_BOTS_KEY) ? Rails.cache.read(TOP_BOTS_KEY) : top_bots_update
+  end
+
+  def top_bots_update
+    top_ten_bots = list_top_ten
+    new_top_bots = top_ten_bots.map do |key, value|
+      BotCount.new(key, value)
+    end
+    old_top_bots = Rails.cache.exist?(TOP_BOTS_KEY) ? Rails.cache.read(TOP_BOTS_KEY) : new_top_bots
+    new_top_bots.each_with_index do |new_bot, new_index|
+      found = false
+      old_top_bots.each_with_index do |old_bot, old_index|
+        next unless new_bot.name == old_bot.name
+
+        found = true
+        new_bot.is_up = true if new_index < old_index
+      end
+      new_bot.is_up = true unless found
+    end
+    Rails.cache.write(TOP_BOTS_KEY, new_top_bots, expires_in: 25.hour)
+    new_top_bots
+  end
+
   private
+
+  class BotCount
+    attr_reader :name, :count, :is_up
+    attr_writer :is_up
+    def initialize(name, count, is_up = false)
+      @name = name
+      @count = count
+      @is_up = is_up
+    end
+  end
 
   def most_popular_bots(amount)
     all_bots_hash = Bot.group("bots.settings->>'base'")
