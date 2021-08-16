ba5485fcc8b91ee2929ac62c5bc633654ccdc379
witoldkesek <witoldkesek@gmail.com>
2021-08-16 09:45:28 +0200

    Envs corrected.

--- config/initializers/telegram-bot.rb
+++ config/initializers/telegram-bot.rb
@@ -1,6 +1,6 @@
 # Configuration of Telegram bot
 Telegram.bots_config = {
-  default: ENV['TELEGRAM_CHAT_BOT_TOKEN']
+  default: ENV['TELEGRAM_BOT_TOKEN']
 }
 config.telegram_updates_controller.session_store = Rails.cache
 Telegram.bot == Telegram.bots[:default]
