6333a38c96e2df3f2c1e89351961ceb642312596
witoldkesek <witoldkesek@gmail.com>
2021-08-16 11:14:13 +0200

    Hook deleted during initialization.

--- config/initializers/telegram-bot.rb
+++ config/initializers/telegram-bot.rb
@@ -2,6 +2,9 @@
 Telegram.bots_config = {
   default: ENV['TELEGRAM_BOT_TOKEN']
 }
-config.telegram_updates_controller.session_store = Rails.cache
+Rails.application.configure do
+  config.telegram_updates_controller.session_store = Rails.cache
+end
 Telegram.bot == Telegram.bots[:default]
+Telegram.bot.delete_webhook
 Telegram.bot.get_updates
