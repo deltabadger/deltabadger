d0ad8b28de743bec5b4f31bf760b0a2f7bc825d4
witoldkesek <witoldkesek@gmail.com>
2021-08-23 10:12:09 +0200

    Fixed reply_text issues.

--- app/controllers/telegram_webhooks_controller.rb
+++ app/controllers/telegram_webhooks_controller.rb
@@ -3,7 +3,7 @@ class TelegramWebhooksController < Telegram::Bot::UpdatesController
 
   def start!(*)
     reply_text = "Welcome to Deltabadger bot.\nType /help for available commands."
-    respond_with :message, text: reply_text
+    respond_with :message, text: reply_text[:reply_text]
   end
 
   def help!(*)
@@ -11,11 +11,11 @@ class TelegramWebhooksController < Telegram::Bot::UpdatesController
     reply_text += "\n/start - Greeting"
     reply_text += "\n/help  - This list of commands."
     reply_text += "\n/top10 - List of top 10 currencies by the number of bots."
-    respond_with :message, text: reply_text
+    respond_with :message, text: reply_text[:reply_text]
   end
 
   def top10!(*)
     reply_text = BotsRepository.new.top_bots_text
-    respond_with :message, text: reply_text, parse_mode: 'html'
+    respond_with :message, text: reply_text[:reply_text], parse_mode: 'html'
   end
 end
