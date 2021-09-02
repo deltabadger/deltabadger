410451506be769beb17fdd6dc45608f183c07804
witoldkesek <witoldkesek@gmail.com>
2021-09-02 14:34:16 +0200

    Convert Scientific reverted because changes are not necessary.

--- app/services/convert_scientific_to_decimal.rb
+++ app/services/convert_scientific_to_decimal.rb
@@ -1,12 +1,5 @@
 class ConvertScientificToDecimal
-  def call(number, precision = 0)
-    number_string = BigDecimal(number.to_s).to_s('F')
-    if precision.positive?
-      zeros_amount = precision - ((number_string.length - 1) - number_string.index('.'))
-      zeros_amount.times do
-        number_string += '0'
-      end
-    end
-    number_string
+  def call(number)
+    BigDecimal(number.to_s).to_s('F')
   end
 end
