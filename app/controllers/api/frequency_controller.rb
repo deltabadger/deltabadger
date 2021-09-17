451ad7843a935df38be84ed0e611287857ac4b62
witoldkesek <witoldkesek@gmail.com>
2021-09-17 12:42:53 +0200

    Final changes and fixes

--- app/controllers/api/frequency_controller.rb
+++ app/controllers/api/frequency_controller.rb
@@ -2,11 +2,7 @@ module Api
   class FrequencyController < ApplicationController
     def limit_exceeded
       limit_exceeded = CheckExceededFrequency.call(params[:exchange_id], params[:type], params[:price], params[:base], params[:quote], params[:currency_of_minimum], params[:interval], params[:forceSmartIntervals], params[:smartIntervalsValue])
-      render json: { limit_exceeded: limit_exceeded[:limit_exceeded], new_intervals_value: limit_exceeded[:new_intervals_value]}.to_json
-    end
-
-    def limit
-      render json: { frequency_limit: ENV['ORDERS_FREQUENCY_LIMIT'] }.to_json
+      render json: { limit_exceeded: limit_exceeded[:limit_exceeded], new_intervals_value: limit_exceeded[:new_intervals_value], frequency_limit: limit_exceeded[:frequency_limit]}.to_json
     end
   end
 end
