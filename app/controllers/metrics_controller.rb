2f5cf82850678ad2fb044d823746f93c3fec632f
witoldkesek <witoldkesek@gmail.com>
2021-09-30 09:55:31 +0200

    Metrics are now being cached.

--- app/controllers/metrics_controller.rb
+++ app/controllers/metrics_controller.rb
@@ -1,7 +1,10 @@
 require 'json'
 
 class MetricsController < ApplicationController
+  METRICS_CACHE_KEY = 'METRICS_CACHE_KEY'.freeze
   def index
+    return render json: { data: Rails.cache.read(METRICS_CACHE_KEY) }.to_json if Rails.cache.exist?(METRICS_CACHE_KEY)
+
     telegram_metrics = FetchTelegramMetrics.new.call
 
     output_params = {
@@ -9,7 +12,7 @@ class MetricsController < ApplicationController
       btcBought: convert_to_satoshis(TransactionsRepository.new.total_btc_bought),
       btcBoughtDayAgo: convert_to_satoshis(TransactionsRepository.new.total_btc_bought_day_ago)
     }.merge(telegram_metrics)
-
+    Rails.cache.write(METRICS_CACHE_KEY,output_params,expires_in: 1.minute)
     render json: { data: output_params }.to_json
   end
 
