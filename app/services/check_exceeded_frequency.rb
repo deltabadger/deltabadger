25075c01a9a92767aede43ad778678864df24ec3
witoldkesek <witoldkesek@gmail.com>
2021-10-04 15:11:09 +0200

    Code refactoring and code review suggestions applied.

--- app/services/check_exceeded_frequency.rb
+++ app/services/check_exceeded_frequency.rb
@@ -1,27 +1,27 @@
 class CheckExceededFrequency < BaseService
-  def call(exchange_id, type, price, base, quote, currency_of_minimum, interval, force_smart_intervals, smart_intervals_value)
+  def call(params)
+    frequency_limit = ENV['ORDERS_FREQUENCY_LIMIT']
+    exchange_id = params[:exchange_id]
+    type = params[:type]
+    price = params[:price].to_f
+    base = params[:base]
+    quote = params[:quote]
+    currency_of_minimum = params[:currency_of_minimum]
+    interval = params[:interval]
+    force_smart_intervals = params[:forceSmartIntervals]
+    smart_intervals_value = params[:smartIntervalsValue].to_f
     return false unless force_smart_intervals == 'true'
 
-    frequency_limit = ENV['ORDERS_FREQUENCY_LIMIT']
-    market = ExchangeApi::Markets::Get.new.call(exchange_id)
-    symbol = market.symbol(base, quote)
-    market_price = market.current_price(symbol).data
-    price = price.to_f
-    smart_intervals_value = smart_intervals_value.to_f
-    currency_in_quote = currency_of_minimum == quote
-    smart_intervals_value /= market_price if currency_in_quote
-    price /= market_price if %w[market_buy limit_buy].include?(type)
+    market = get_market(exchange_id)
+    symbol = get_symbol(market, base, quote)
+    market_price = get_market_price(market, symbol)
+    defined_in_quote = defined_in_quote?(currency_of_minimum, quote)
+    smart_intervals_in_base = get_smart_intervals_in_base(smart_intervals_value, market_price, defined_in_quote)
+    price_in_base = get_price_in_base(price, market_price, type)
 
-    frequency = price / smart_intervals_value
-    frequency /= duration_in_hours(interval)
-    limit_exceeded = frequency.to_f > frequency_limit.to_f
-    new_intervals_value = limit_exceeded ? price/frequency_limit.to_f : 0
-    if currency_in_quote
-      new_intervals_value *= market_price
-      new_intervals_value = new_intervals_value.ceil(2)
-    else
-      new_intervals_value = new_intervals_value.ceil(5)
-    end
+    frequency = get_frequency(price_in_base, smart_intervals_in_base, interval)
+    limit_exceeded = limit_exceeded?(frequency, frequency_limit)
+    new_intervals_value = limit_exceeded ? get_new_intervals_value(price, frequency_limit, interval, market_price, defined_in_quote, market, symbol, type) : 0
     {
       limit_exceeded: limit_exceeded,
       new_intervals_value: new_intervals_value,
@@ -30,6 +30,56 @@ class CheckExceededFrequency < BaseService
   end
 
   private
+
+  def defined_in_quote?(currency_of_minimum, quote)
+    currency_of_minimum == quote
+  end
+
+  def get_market(exchange_id)
+    ExchangeApi::Markets::Get.new.call(exchange_id)
+  end
+
+  def get_symbol(market, base, quote)
+    market.symbol(base, quote)
+  end
+
+  def get_market_price(market, symbol)
+    market.current_price(symbol).data
+  end
+
+  def get_price_in_base(price, market_price, type)
+    return price / market_price if %w[market_buy limit_buy].include?(type)
+
+    price
+  end
+
+  def get_smart_intervals_in_base(smart_intervals_value, market_price, defined_in_quote)
+    return smart_intervals_value / market_price if defined_in_quote
+
+    smart_intervals_value
+  end
+
+  def get_frequency(price_in_base, smart_intervals_in_base, interval)
+    frequency = price_in_base / smart_intervals_in_base
+    frequency /= duration_in_hours(interval)
+    frequency
+  end
+
+  def limit_exceeded?(frequency, frequency_limit)
+    frequency.to_f > frequency_limit.to_f
+  end
+
+  def get_new_intervals_value(price, frequency_limit, interval, market_price, defined_in_quote, market, symbol, type)
+    new_intervals_value = price / (frequency_limit.to_f * duration_in_hours(interval))
+    if defined_in_quote
+      new_intervals_value *= market_price unless %w[market_buy limit_buy].include?(type)
+      new_intervals_value = new_intervals_value.ceil(market.quote_decimals(symbol).data)
+    else
+      new_intervals_value /= market_price if %w[market_buy limit_buy].include?(type)
+      new_intervals_value = new_intervals_value.ceil(market.base_decimals(symbol).data)
+    end
+    new_intervals_value
+  end
 
   def duration_in_hours(interval)
     {
