41add7a119fc776fa0e3724213ff0d94def5e032
mkaczmarek <mat_kaczmarek@outlook.com>
2021-10-13 16:26:26 +0200

    Implement withdrawal key validators.

---
+++ app/services/exchange_api/validators/bitbay/withdrawal_validator.rb
@@ -0,0 +1,13 @@
+module ExchangeApi
+  module Validators
+    module Bitbay
+      class WithdrawalValidator < BaseValidator
+        def validate_credentials(api_key:, api_secret:)
+          true
+        rescue StandardError
+          false
+        end
+      end
+    end
+  end
+end
