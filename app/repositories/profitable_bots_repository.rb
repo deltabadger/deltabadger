46ac1df612fb78eb2e3f8b5e16ba1c0fafdaa238
witoldkesek <witoldkesek@gmail.com>
2021-10-21 09:43:36 +0200

    Changed "null" to "nil" in function definition.

--- app/repositories/profitable_bots_repository.rb
+++ app/repositories/profitable_bots_repository.rb
@@ -7,7 +7,7 @@ class ProfitableBotsRepository
   def profitable_bots_data(created_before)
     bot_totals = ActiveRecord::Base.connection.execute("select * from bots_total_amounts where created_at < '#{created_before}'")
     filtered_bots_map = bot_totals.to_a.map do |bot|
-      profitable_or_null(bot)
+      profitable_or_nil(bot)
     end.compact
     profitable_bots_map = filtered_bots_map.filter { |bot| bot }
     (profitable_bots_map.length / filtered_bots_map.length.to_f * 100).ceil(2)
@@ -15,7 +15,7 @@ class ProfitableBotsRepository
 
   private
 
-  def profitable_or_null(bot)
+  def profitable_or_nil(bot)
     settings = JSON.parse(bot['settings'])
     current_price = current_price(bot['exchange_id'], settings['base'], settings['quote'])
     return nil if current_price.data.nil? || bot['total_amount'].nil? || !current_price.success?
