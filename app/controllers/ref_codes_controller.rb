e7c83bc1c903ca5b8c7329c2adf9bdc460f3d6b8
witoldkesek <witoldkesek@gmail.com>
2021-10-21 13:02:08 +0200

    Code blocks the possibility for the user to use their own code.

--- app/controllers/ref_codes_controller.rb
+++ app/controllers/ref_codes_controller.rb
@@ -9,6 +9,8 @@ class RefCodesController < ApplicationController
 
     if current_user.referrer_id.present?
       redirect_to dashboard_path, flash: { notice: I18n.t('affiliates.discount.already_used') }
+    elsif current_user.id == affiliate.id
+      redirect_to referral_program_path, flash: { notice: I18n.t('affiliates.discount.own_link_used') }
     else
       @affiliate = affiliate
     end
