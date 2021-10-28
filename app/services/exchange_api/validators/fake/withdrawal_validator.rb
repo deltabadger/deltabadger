553d4b81663fce8e87216cb6d138bea6aae15882
matkaczmarek <mat_kaczmarek@outlook.com>
2021-10-28 11:45:02 +0200

    Fix fake validator.

--- app/services/exchange_api/validators/fake/withdrawal_validator.rb
+++ app/services/exchange_api/validators/fake/withdrawal_validator.rb
@@ -4,7 +4,7 @@ module ExchangeApi
       class WithdrawalValidator < BaseValidator
         SUCCESS = true
 
-        def validate_credentials(api_key:, api_secret:) # rubocop:disable Lint/UnusedMethodArgument
+        def validate_credentials(api_key:, api_secret:, passphrase: nil) # rubocop:disable Lint/UnusedMethodArgument
           SUCCESS
         end
       end
