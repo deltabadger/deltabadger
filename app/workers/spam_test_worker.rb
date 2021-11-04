8ba1f7824ea1dd236317b9f07f25aff02b207c98
witoldkesek <witoldkesek@gmail.com>
2021-11-04 08:38:30 +0100

    Concurrecy test job added.

---
+++ app/workers/spam_test_worker.rb
@@ -0,0 +1,11 @@
+class SpamTestWorker
+  include Sidekiq::Worker
+
+  def perform
+    convert_to_satoshis(TransactionsRepository.new.total_btc_bought)
+    ProfitableBotsRepository.new.profitable_bots_data(Time.now)
+    SpamTestWorker.perform_at(Time.now + 10.second)
+  rescue StandardError => e # prevent job from retrying
+    Raven.capture_exception(e)
+  end
+end
