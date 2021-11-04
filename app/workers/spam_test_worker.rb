ee1a232247d513c65da3b25e6d93f261d8d8c4f6
witoldkesek <witoldkesek@gmail.com>
2021-11-04 09:54:59 +0100

    Removed "convert to satoshis" from Test Worker.

--- app/workers/spam_test_worker.rb
+++ app/workers/spam_test_worker.rb
@@ -2,9 +2,9 @@ class SpamTestWorker
   include Sidekiq::Worker
 
   def perform
-    convert_to_satoshis(TransactionsRepository.new.total_btc_bought)
+    puts TransactionsRepository.new.total_btc_bought
     ProfitableBotsRepository.new.profitable_bots_data(Time.now)
-    SpamTestWorker.perform_at(Time.now + 3.second)
+    SpamTestWorker.perform_at(Time.now + 5.second)
   rescue StandardError => e # prevent job from retrying
     Raven.capture_exception(e)
   end
