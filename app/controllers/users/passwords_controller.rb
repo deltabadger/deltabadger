c78b62d3c3b8bac51b691e8bf10ce21e8f227dbd
mkaczmarek <mat_kaczmarek@outlook.com>
2021-11-10 12:02:53 +0100

    Refactor code.

--- app/controllers/users/passwords_controller.rb
+++ app/controllers/users/passwords_controller.rb
@@ -1,23 +1,14 @@
 class Users::PasswordsController < Devise::PasswordsController
   def update
     self.resource = resource_class.with_reset_password_token(resource_params[:reset_password_token])
+    return super if resource&.otp_module_disabled?
 
-    if resource&.otp_module_disabled?
-      if params[:user][:otp_code_token].empty?
-        super
-      else
-        abort_update(I18n.t('errors.messages.bad_credentials_with_code'), :password)
-      end
-    elsif resource&.otp_module_enabled?
-      if params[:user][:otp_code_token].present?
-        if resource.authenticate_otp(params[:user][:otp_code_token], drift: 60)
-          super
-        else
-          abort_update(I18n.t('errors.messages.bad_2fa_code'), :otp_code_token)
-        end
-      else
-        abort_update(I18n.t('errors.messages.empty_two_fa_token'), :otp_code_token)
-      end
+    if params[:user][:otp_code_token].present?
+      return super if resource.authenticate_otp(params[:user][:otp_code_token], drift: 60)
+
+      abort_update(I18n.t('errors.messages.bad_2fa_code'), :otp_code_token)
+    else
+      abort_update(I18n.t('errors.messages.empty_two_fa_token'), :otp_code_token)
     end
   end
 
