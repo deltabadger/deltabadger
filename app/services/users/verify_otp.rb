51405f0b8075bddc47f475ccdc00c1e14c4b2609
mkaczmarek <mat_kaczmarek@outlook.com>
2021-11-17 15:30:02 +0100

    Remove unnecessary to_datetime.

--- app/services/users/verify_otp.rb
+++ app/services/users/verify_otp.rb
@@ -5,7 +5,7 @@ module Users
       last_otp_at = totp.verify(code, after: user.last_otp_at)
       return false if last_otp_at.nil?
 
-      user.update(last_otp_at: Time.at(last_otp_at).to_datetime)
+      user.update(last_otp_at: Time.at(last_otp_at))
       true
     end
   end
