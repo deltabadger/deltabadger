01ad0a7f7084f2ebbc671bbac8ad5e6ad987a4af
mkaczmarek <mat_kaczmarek@outlook.com>
2021-11-22 12:33:01 +0100

    Change metrics schedule to 6 hours.

--- app/workers/update_metrics_worker.rb
+++ app/workers/update_metrics_worker.rb
@@ -2,6 +2,6 @@ class UpdateMetricsWorker
   include Sidekiq::Worker
   def perform
     MetricsRepository.new.update_metrics
-    UpdateMetricsWorker.perform_at(Time.now + 1.day)
+    UpdateMetricsWorker.perform_at(Time.now + 6.hours)
   end
 end
