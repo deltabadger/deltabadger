f19e6996945860d5499f7a2436899aa9334d88a1
Witold Kęsek <49924891+witoldkesek@users.noreply.github.com>
2021-12-01 11:08:50 +0100

    Button added on payments view.

--- config/routes.rb
+++ config/routes.rb
@@ -20,6 +20,7 @@ Rails.application.routes.draw do
     resources :subscription_plans
     resources :payments do
       get :csv, on: :collection
+      get :csv_wire, on: :collection
       put '/confirm/:id', action: :confirm, as: :confirm, on: :collection
     end
     resources :vat_rates
