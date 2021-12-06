ad1ea54b17bf5d3ab580449cc8a94646582275e4
witoldkesek <witoldkesek@gmail.com>
2021-12-06 14:03:14 +0100

    Optimizing profitable bots repository.

--- app/repositories/metrics_repository.rb
+++ app/repositories/metrics_repository.rb
@@ -4,18 +4,33 @@ class MetricsRepository < BaseRepository
     telegram_metrics = FetchTelegramMetrics.new.call
     Scenic.database.refresh_materialized_view(:bots_total_amounts, concurrently: true, cascade: false)
     redis_client = Redis.new(url: ENV.fetch('REDIS_AWS_URL'))
-    profitable_bots_repository = ProfitableBotsRepository.new
+    profitable_bots_data = ProfitableBotsRepository.new.profitable_bots_data
     output_params = {
       liveBots: BotsRepository.new.count_with_status('working'),
       btcBought: convert_to_satoshis(TransactionsRepository.new.total_btc_bought),
       btcBoughtDayAgo: convert_to_satoshis(TransactionsRepository.new.total_btc_bought_day_ago),
-      profitBotsTillNow: profitable_bots_repository.profitable_bots_data(Time.now),
-      profitBots3MothsAgo: profitable_bots_repository.profitable_bots_data(Time.now - 3.month),
-      profitBots6MothsAgo: profitable_bots_repository.profitable_bots_data(Time.now - 6.month),
-      profitBots12MothsAgo: profitable_bots_repository.profitable_bots_data(Time.now - 12.month)
+      profitBotsTillNow: profitable_bots_data[3],
+      profitBots3MothsAgo: profitable_bots_data[2],
+      profitBots6MothsAgo: profitable_bots_data[1],
+      profitBots12MothsAgo: profitable_bots_data[0]
     }.merge(telegram_metrics)
     redis_client.set(METRICS_KEY, output_params.to_json)
     FeesService.new.update_fees
+  end
+
+  def measure_avg_time_of_execution
+    time_sum = 0
+    times = []
+    10.times do
+      t1 = Time.now
+      update_metrics
+      execution_time = Time.now - t1
+      time_sum += execution_time
+      times.push(execution_time)
+    end
+    avg_time = time_sum / 10
+    puts times
+    puts "Average time : #{avg_time}"
   end
 
   def metrics_data
--- app/repositories/profitable_bots_repository.rb
+++ app/repositories/profitable_bots_repository.rb
@@ -4,13 +4,28 @@ class ProfitableBotsRepository
     @markets_dictionary = {}
   end
 
-  def profitable_bots_data(created_before)
-    bot_totals = ActiveRecord::Base.connection.execute("select * from bots_total_amounts where created_at < '#{created_before}'")
+  def profitable_bots_data
+    bot_totals = ActiveRecord::Base.connection.execute('select * from bots_total_amounts')
     filtered_bots_map = bot_totals.to_a.map do |bot|
       profitable_or_nil(bot)
     end.compact
-    profitable_bots_map = filtered_bots_map.filter { |bot| bot }
-    (profitable_bots_map.length / filtered_bots_map.length.to_f * 100).ceil(2)
+    bots_array = [
+      filtered_bots_map.filter { |x| x[1] <= Time.now - 12.month },
+      filtered_bots_map.filter { |x| x[1] > Time.now - 12.month && x[1] <= Time.now - 6.month },
+      filtered_bots_map.filter { |x| x[1] > Time.now - 6.month && x[1] <= Time.now - 3.month },
+      filtered_bots_map.filter { |x| x[1] > Time.now - 3.month }
+    ]
+    total_amount_of_bots = 0
+    total_amount_of_bots_in_profit = 0
+    profit_array = []
+    bots_array.each do |bots|
+      total_amount_of_bots += bots.length
+      total_amount_of_bots_in_profit += bots.filter { |bot| bot[0] }.length
+      profit_array.push((total_amount_of_bots_in_profit / total_amount_of_bots.to_f * 100).ceil(2))
+    end
+    #profitable_bots_map = filtered_bots_map.filter { |bot| bot }
+    #(profitable_bots_map.length / filtered_bots_map.length.to_f * 100).ceil(2)
+    profit_array
   end
 
   private
@@ -21,7 +36,7 @@ class ProfitableBotsRepository
     return nil if current_price.data.nil? || bot['total_amount'].nil? || !current_price.success?
 
     current_value = bot['total_amount'].to_f * current_price.data.to_f
-    current_value.to_f > bot['total_cost'].to_f
+    [current_value.to_f > bot['total_cost'].to_f, bot['created_at']]
   end
 
   def current_price(exchange_id, base, quote)
