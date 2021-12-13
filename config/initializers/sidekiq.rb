b2fecae78dad0836fad263c60adc747e87db4cd5
mkaczmarek <mat_kaczmarek@outlook.com>
2021-12-13 13:04:25 +0100

    Remove logging.

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -6,10 +6,3 @@ end
 Sidekiq.configure_client do |config|
   config.redis = { url: ENV.fetch('REDIS_AWS_URL') }
 end
-
-# just for checking on staging if correct query is executed, I'll delete it before pushing to master
-# TODO remove it after staging
-Rails.logger = Sidekiq.logger
-Sidekiq.logger.level = Logger::DEBUG
-ActiveRecord::Base.logger.level = Logger::DEBUG
-ActiveRecord::Base.logger = Sidekiq.logger
