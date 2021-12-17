060526ef05484845e6f82f8aecc3f3af230aea0d
Witold Kęsek <49924891+witoldkesek@users.noreply.github.com>
2021-12-17 09:30:50 +0100

    Subaccounts working in front and backend, cosmetic changes still coming.

--- app/services/bots/free/format_params/create.rb
+++ app/services/bots/free/format_params/create.rb
@@ -13,6 +13,8 @@ module Bots
           smart_intervals_value
           price_range_enabled
           price_range
+          useSubaccount
+          selectedSubaccount
         ].freeze
 
         def call(params)
