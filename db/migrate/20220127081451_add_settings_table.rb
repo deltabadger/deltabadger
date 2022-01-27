47dd08df911395af6fa9fbd111fc49d9e3a196b1
witoldkesek <witoldkesek@gmail.com>
2022-01-27 15:31:01 +0100

    Removed unnecessary migration.

--- db/migrate/20220127081451_add_settings_table.rb
+++
@@ -1,9 +0,0 @@
-class AddSettingsTable < ActiveRecord::Migration[5.2]
-  def change
-    create_table :settings do |t|
-      t.string :name
-      t.string :value
-      t.timestamps
-    end
-  end
-end
--- db/migrate/20220127133024_add_setting_flags_table.rb
+++ db/migrate/20220127133024_add_setting_flags_table.rb
@@ -1,6 +1,5 @@
 class AddSettingFlagsTable < ActiveRecord::Migration[5.2]
   def change
-    drop_table :settings
     create_table :setting_flags do |t|
       t.string :name
       t.boolean :value
