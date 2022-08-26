205b69d87856798bda72b91aebdd93b3d2e5ba89
Raza Hussain <mr.raza.hussain@gmail.com>
2022-08-26 08:22:37 +0500

    Rails upgraded from 5.2 to 6.0

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,12 +1,3 @@
 const { environment } = require('@rails/webpacker')
-const erb =  require('./loaders/erb')
 
-environment.loaders.get('sass').use.splice(-1, 0, {
-  loader: 'resolve-url-loader',
-  options: {
-    attempts: 1
-  }
-});
-
-environment.loaders.prepend('erb', erb)
 module.exports = environment
