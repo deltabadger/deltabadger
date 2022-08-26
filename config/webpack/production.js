7df9fa818198f117b60d73bd78e4b8c8c09fbaff
Raza Hussain <mr.raza.hussain@gmail.com>
2022-08-26 08:22:37 +0500

    Rails upgraded from 5.2 to 6.0

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -1,7 +1,5 @@
 process.env.NODE_ENV = process.env.NODE_ENV || 'production'
 
 const environment = require('./environment')
-const dotenv = require('dotenv')
-dotenv.config()
 
 module.exports = environment.toWebpackConfig()
