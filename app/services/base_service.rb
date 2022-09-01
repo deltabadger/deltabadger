5126c37d7200622b858744bf1742618c221a6eaf
Raza Hussain <mr.raza.hussain@gmail.com>
2022-09-01 19:31:26 +0500

    bots

--- app/services/base_service.rb
+++ app/services/base_service.rb
@@ -2,6 +2,10 @@ require 'result'
 
 class BaseService
   def self.call(*args)
+    puts "==========================="
+    puts "=========== #{args.inspect}================"
+    puts "==========================="
+    return if args.empty?
     new.call(*args)
   end
 
