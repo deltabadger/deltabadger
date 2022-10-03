535d03e911de9c8be85c360188417b8195586174
Roman Girej <113096497+RomanGirej@users.noreply.github.com>
2022-10-03 14:38:49 +0300

    Added Legendary Badger Plan

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -4,6 +4,7 @@ class Admin::DashboardController < Admin::ApplicationController
       number_of_all_users: User.count,
       number_of_investor_plans: SubscriptionsRepository.new.all_current_count('investor'),
       number_of_hodler_plans: SubscriptionsRepository.new.all_current_count('hodler'),
+      number_of_legendary_badger_plans: SubscriptionsRepository.new.all_current_count('legendary_badger'),
       number_of_all_bots: Bot.count,
       number_of_working_bots: BotsRepository.new.count_with_status('working')
     }
