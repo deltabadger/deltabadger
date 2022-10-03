76dfc0b0c9b796b68c512899754c62681f5d222e
RomanGirej <romangirej2@gmail.com>
2022-10-03 14:38:49 +0300

    Added Legendary Badger Plan

--- app/javascript/deltabadger/components/TradingBot.jsx
+++ app/javascript/deltabadger/components/TradingBot.jsx
@@ -485,7 +485,7 @@ const BotTemplate = ({
               className="hide-when-running">*</sup>
 
               { isLimitSelected() && <small className="hide-when-running"><LimitOrderNotice/></small> }
-              { !showLimitOrders && <a href={`/${document.body.dataset.locale}/upgrade`} className="bot input bot-input--hodler-only--before">Hodler only</a> }
+              { !showLimitOrders && <a href={`/${document.body.dataset.locale}/upgrade`} className="bot input bot-input--hodler-only--before">Hodler and Legendary Badger only</a> }
             </div>
 
           </label>
@@ -523,7 +523,7 @@ const BotTemplate = ({
                 size={ Math.max(priceRange.high.length, 1) }
               />
               <RawHTML tag="span">{splitTranslation(I18n.t((isLegacySell() || isSellOffer()) ? 'bots.price_range_sell_html' :'bots.price_range_buy_html', {currency: settings.quote}))[2]}</RawHTML>
-              { !showLimitOrders && <a href={`/${document.body.dataset.locale}/upgrade`} className="bot input bot-input--hodler-only--before">Hodler only</a> }
+              { !showLimitOrders && <a href={`/${document.body.dataset.locale}/upgrade`} className="bot input bot-input--hodler-only--before">Hodler and Legendary Badger only</a> }
             </div>
           </label>
 
