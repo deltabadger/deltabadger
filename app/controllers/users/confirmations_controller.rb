fea9fe9c550047d93e748545bb2d0d331e6f602e
RomanGirej <romangirej2@gmail.com>
2022-11-09 12:36:52 +0200

    Remove byebug

--- app/controllers/users/confirmations_controller.rb
+++ app/controllers/users/confirmations_controller.rb
@@ -5,7 +5,6 @@ class Users::ConfirmationsController < Devise::ConfirmationsController
   # GET /resource/confirmation?confirmation_token=abcdef
   def show
     super do
-      byebug
       if params[:new_user] == 'true'
         SendgridMailToList.new.call(@user)
         ZapierMailToList.new.call(@user)
