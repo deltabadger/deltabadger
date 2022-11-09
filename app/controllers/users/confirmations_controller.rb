81d44da269c33030b8451c48a8aaf78dcc2fe984
RomanGirej <romangirej2@gmail.com>
2022-11-09 12:16:28 +0200

    Improved the code to send emails to Sendgrid and Zapier only when registering

--- app/controllers/users/confirmations_controller.rb
+++ app/controllers/users/confirmations_controller.rb
@@ -5,8 +5,11 @@ class Users::ConfirmationsController < Devise::ConfirmationsController
   # GET /resource/confirmation?confirmation_token=abcdef
   def show
     super do
-      SendgridMailToList.new.call(@user)
-      ZapierMailToList.new.call(@user)
+      byebug
+      if params[:new_user] == 'true'
+        SendgridMailToList.new.call(@user)
+        ZapierMailToList.new.call(@user)
+      end
     end
   end
 end
