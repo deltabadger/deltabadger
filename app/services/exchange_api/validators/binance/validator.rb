202cb7cb4d2a548419091175a6978ef8a39fcfc0
RomanGirej <romangirej2@gmail.com>
2022-11-30 17:41:23 +0200

    Added Binance requests logging

--- app/services/exchange_api/validators/binance/validator.rb
+++ app/services/exchange_api/validators/binance/validator.rb
@@ -11,14 +11,20 @@ module ExchangeApi
         ORDER_DOES_NOT_EXIST = -2011
 
         def validate_credentials(api_key:, api_secret:)
-          request = signed_client(api_key, api_secret, @url_base).delete(
+          client = signed_client(api_key, api_secret, @url_base)
+          binance_log.info(client)
+          binance_log.info("DELETE 'order' #{{symbol: 'ETHBTC', orderId: '9' * 10}}")
+          request = client.delete(
             'order',
             symbol: 'ETHBTC',
             orderId: '9' * 10
           )
           response = JSON.parse(request.body)
+          binance_log.info(response)
           response['code'] == ORDER_DOES_NOT_EXIST
-        rescue StandardError
+        rescue StandardError => e
+          binance_log.error(e.inspect)
+          binance_log.error(e.backtrace)
           false
         end
       end
