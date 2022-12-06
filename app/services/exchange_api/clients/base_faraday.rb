be3d5e914b4011845baff510e4abf41d0e3a853a
klos <jan@deltabadger.com>
2022-12-06 18:15:11 +0200

    Revert "Added Binance proxy to BaseFaraday module"
    This reverts commit ff3ff2f690138be9ab1f5825fbd213b4920987c5.

--- app/services/exchange_api/clients/base_faraday.rb
+++ app/services/exchange_api/clients/base_faraday.rb
@@ -2,22 +2,17 @@ module ExchangeApi
   module Clients
     module BaseFaraday
       def base_client(url_base)
-        Faraday.new(attributes(url_base)) do |conn|
+        Faraday.new(url: url_base) do |conn|
           conn.adapter Faraday.default_adapter
         end
       end
 
       def caching_client(url_base, expire_time = ENV['DEFAULT_MARKET_CACHING_TIME'])
-        Faraday.new(attributes(url_base)) do |builder|
+        Faraday.new(url: url_base) do |builder|
           builder.use :manual_cache,
                       expires_in: expire_time
           builder.adapter Faraday.default_adapter
         end
-      end
-
-      def attributes(url_base)
-        attributes = { url: url_base }
-        attributes.merge!({ proxy: ENV.fetch('EU_PROXY_IP') }) if url_base.in? [BinanceEnum::EU_URL_BASE, BinanceEnum::EU_WITHDRAWAL_URL_BASE]
       end
     end
   end
