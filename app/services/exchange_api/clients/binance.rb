80977a1828893a0fefb1a0dd9b636010676a7247
RomanGirej <romangirej2@gmail.com>
2022-12-07 15:16:54 +0200

    Added Binance logging

--- app/services/exchange_api/clients/binance.rb
+++ app/services/exchange_api/clients/binance.rb
@@ -42,7 +42,7 @@ module ExchangeApi
       end
 
       def binance_log
-        @binance_log ||= Logger.new("log/binance_log_7.log")
+        @binance_log ||= Logger.new("log/binance_log_8.log")
       end
     end
   end
