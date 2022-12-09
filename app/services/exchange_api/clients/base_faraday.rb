511974d0a4db50ceac24b996f979bfd6801f92c8
Roman Girej <113096497+RomanGirej@users.noreply.github.com>
2022-12-09 12:34:20 +0200

    Removed Binance test logging

--- app/services/exchange_api/clients/base_faraday.rb
+++ app/services/exchange_api/clients/base_faraday.rb
@@ -2,48 +2,24 @@ module ExchangeApi
   module Clients
     module BaseFaraday
       def base_client(url_base)
-        binance_log.info("base_client(url_base)")
-        connector = Faraday.new(attributes(url_base)) do |conn|
-          binance_log.info("base_client Faraday body")
+        Faraday.new(attributes(url_base)) do |conn|
           conn.adapter Faraday.default_adapter
         end
-        binance_log.info("base_client connector")
-        binance_log.info(connector)
-        connector
-      rescue => e
-        binance_log.info("=== ExchangeApi::Clients::BaseFaraday.base_client() rescue => e ===")
-        binance_log.error(e.inspect)
       end
 
       def caching_client(url_base, expire_time = ENV['DEFAULT_MARKET_CACHING_TIME'])
-        binance_log.info("caching_client(url_base, expire_time = ENV['DEFAULT_MARKET_CACHING_TIME'])")
-        connector = Faraday.new(attributes(url_base)) do |builder|
-          binance_log.info("caching_client Faraday body")
+        Faraday.new(attributes(url_base)) do |builder|
           builder.use :manual_cache,
                       expires_in: expire_time
           builder.adapter Faraday.default_adapter
         end
-        binance_log.info("caching_client connector")
-        binance_log.info(connector)
-        connector
-      rescue => e
-        binance_log.info("=== ExchangeApi::Clients::BaseFaraday.caching_client() rescue => e ===")
-        binance_log.error(e.inspect)
       end
 
       def attributes(url_base)
-        binance_log.info("attributes")
         attributes = { url: url_base }
         attributes.merge!({ proxy: ENV.fetch('EU_PROXY_IP') }) if url_base.in? [BinanceEnum::EU_URL_BASE, BinanceEnum::EU_WITHDRAWAL_URL_BASE]
-        binance_log.info(url_base)
-        binance_log.info(url_base.in? [BinanceEnum::EU_URL_BASE, BinanceEnum::EU_WITHDRAWAL_URL_BASE])
-        binance_log.info(attributes)
-        attributes
       end
 
-      def binance_log
-        @binance_log ||= Logger.new("log/binance_log_10.log")
-      end
     end
   end
 end
