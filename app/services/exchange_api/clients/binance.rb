5adb49db7aac3abc8e6be398a1f9391c87cc93f4
RomanGirej <romangirej2@gmail.com>
2022-12-09 12:34:20 +0200

    Removed Binance test logging

--- app/services/exchange_api/clients/binance.rb
+++ app/services/exchange_api/clients/binance.rb
@@ -20,30 +20,17 @@ module ExchangeApi
       end
 
       def signed_client(api_key, api_secret, url_base)
-        binance_log.info("=== faraday_connector ===")
-        binance_log.info(url_base)
-        binance_log.info(url_base.in? [BinanceEnum::EU_URL_BASE, BinanceEnum::EU_WITHDRAWAL_URL_BASE])
-        binance_log.info(ENV.fetch('EU_PROXY_IP'))
-
         attributes = { url: url_base }
         attributes.merge!({ proxy: ENV.fetch('EU_PROXY_IP') }) if url_base.in? [BinanceEnum::EU_URL_BASE, BinanceEnum::EU_WITHDRAWAL_URL_BASE]
 
-        connector = Faraday.new(attributes) do |conn|
+        Faraday.new(attributes) do |conn|
           conn.headers['X-MBX-APIKEY'] = api_key
           conn.use AddTimestamp
           conn.use AddSignature, api_secret
           conn.adapter Faraday.default_adapter
         end
-
-        binance_log.info("connector")
-        binance_log.info(connector)
-
-        connector
       end
 
-      def binance_log
-        @binance_log ||= Logger.new("log/binance_log_10.log")
-      end
     end
   end
 end
