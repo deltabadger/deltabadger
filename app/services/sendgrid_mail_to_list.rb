6e53029d9e4b0de5e4803c52aae0b91a5b21c5ad
klos <jan@deltabadger.com>
2023-01-22 02:02:32 +0200

    fix passing first name to sendgrid

--- app/services/sendgrid_mail_to_list.rb
+++ app/services/sendgrid_mail_to_list.rb
@@ -84,9 +84,9 @@ class SendgridMailToList < BaseService
     body.fetch('id')
   end
 
-  def add_email_to_list(user, email_list_ids = list_ids)
+  def add_email_to_list(user, list_ids)
     user_first_name_only = user.name.split.first.capitalize
-    response = Faraday.post(CONTACTS_URL, add_email_request_body(email, user_first_name_only).to_json, headers.merge({'list_ids': list_ids.join(',')}))
+    response = Faraday.post(CONTACTS_URL, add_email_request_body(user.email, user_first_name_only).to_json, headers.merge({'list_ids': list_ids.join(',')}))
     body = JSON.parse(response.body)
 
     raise StandardError, body["errors"] unless response.status == 202
