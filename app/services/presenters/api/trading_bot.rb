4baa39cb416881d755fc7ed46274da579c5f4fce
RomanGirej <romangirej2@gmail.com>
2023-08-31 16:48:39 +0300

    Added TransactionDaily model

--- app/services/presenters/api/trading_bot.rb
+++ app/services/presenters/api/trading_bot.rb
@@ -12,8 +12,9 @@ module Presenters
       end
 
       def call(bot)
-        transactions = bot.transactions.where(status: 'success').order(created_at: :desc)
-        skipped_transactions = bot.transactions.where(status: 'skipped').order(created_at: :desc)
+        transactions = bot.transactions.where(status: 'success').order(created_at: :desc).first(10)
+        transactions_daily = bot.transactions_daily.order(created_at: :desc)
+        skipped_transactions = bot.transactions.where(status: 'skipped').order(created_at: :desc).first(10)
         logs = bot.transactions.order(id: :desc).first(10)
 
         {
@@ -23,10 +24,10 @@ module Presenters
           exchangeName: bot.exchange.name,
           exchangeId: bot.exchange.id,
           status: bot.status,
-          transactions: transactions.first(10).map(&method(:present_transaction)),
-          skippedTransactions: skipped_transactions.first(10).map(&method(:present_transaction)),
+          transactions: transactions.map(&method(:present_transaction)),
+          skippedTransactions: skipped_transactions.map(&method(:present_transaction)),
           logs: logs.map(&method(:present_log)),
-          stats: present_stats(bot, transactions),
+          stats: present_stats(bot, transactions_daily),
           nowTimestamp: Time.now.to_i,
           nextResultFetchingTimestamp: next_result_fetching_timestamp(bot),
           nextTransactionTimestamp: next_transaction_timestamp(bot)
