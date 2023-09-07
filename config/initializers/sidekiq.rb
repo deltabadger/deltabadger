9a1ec6f5181f0abedef603ededbfd76439f6105b
klos <jan@deltabadger.com>
2023-09-07 13:26:55 +0300

    updates sidekiq

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,10 +1,10 @@
 Sidekiq.configure_server do |config|
   config.average_scheduled_poll_interval = 1
   config.redis = { url: ENV.fetch('REDIS_AWS_URL') }
+  config.logger = Sidekiq::Logger.new($stdout, level: :info)
+  config.logger.formatter = Sidekiq::Logger::Formatters::Pretty.new
 end
 
 Sidekiq.configure_client do |config|
   config.redis = { url: ENV.fetch('REDIS_AWS_URL') }
 end
-
-Sidekiq.logger = Logger.new(Rails.root.join("log/sidekiq.log"))
