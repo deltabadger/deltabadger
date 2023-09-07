9d71eb0031a0c12fcc3c58c0640d054a4f37ee9e
klos <jan@deltabadger.com>
2023-09-07 17:59:09 +0300

    new aggregate columns

--- app/services/charts/portfolio_value_over_time/data.rb
+++ app/services/charts/portfolio_value_over_time/data.rb
@@ -10,34 +10,33 @@ module Charts::PortfolioValueOverTime
 
     def query
       <<~SQL
-        with data as (
-          select
-            created_at,
-            rate * amount as invested,
-            rate,
-            amount
-          from daily_transaction_aggregates
-          where bot_id = ? and status = 0
-        ),
-        windowed_data as (
-          select
-            created_at,
-            sum(invested) over (order by created_at asc rows between unbounded preceding and current row) as total_invested,
-            sum(amount) over (order by created_at asc rows between unbounded preceding and current row) as total_accumulated,
-            rate
-          from data
-        )
-        
+      with data as (
         select
           created_at,
-          total_invested,
-          rate * total_accumulated as current_value,
-          total_accumulated
-        from windowed_data
+          rate * amount as invested,
+          rate,
+          amount
+        from daily_transaction_aggregates
+        where bot_id = ? and status = 0
+      ),
+      windowed_data as (
+        select
+          created_at,
+          sum(invested) over (order by created_at asc) as total_invested,
+          sum(amount) over (order by created_at asc) as total_accumulated,
+          rate
+        from data
+      )
+
+      select
+        created_at,
+        total_invested,
+        rate * total_accumulated as current_value,
+        total_accumulated
+      from windowed_data
+
       SQL
     end
-
-
 
     def present_data
       ->(row) { row.slice('created_at', 'total_invested', 'current_value').values }
