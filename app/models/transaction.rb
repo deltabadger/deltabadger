7b6ccc7248721f70241a5ab9540aff62c02feeea
Jan Klosowski <jan@deltabadger.com>
2023-09-08 00:36:53 +0300

    calculating total values

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -13,7 +13,6 @@ class Transaction < ApplicationRecord
 
   def price
     return 0.0 unless rate.present?
-
     amount * rate
   end
 
@@ -22,13 +21,36 @@ class Transaction < ApplicationRecord
   def set_daily_transaction_aggregate
     return unless success?
 
+    # initialize repositories
     transactions_repository = TransactionsRepository.new
     daily_transaction_aggregates_repository = DailyTransactionAggregateRepository.new
-    daily_transaction_aggregate = daily_transaction_aggregates_repository.today_for_bot(bot).first
-    return daily_transaction_aggregates_repository.create(attributes.except('id')) unless daily_transaction_aggregate
 
+    # get today's and last aggregates
+    daily_transaction_aggregate = daily_transaction_aggregates_repository.today_for_bot(bot).first
+    last_aggregate = daily_transaction_aggregate || daily_transaction_aggregates_repository.where(bot: bot).last
+
+    # get today's transactions
     bot_transactions = transactions_repository.today_for_bot(bot)
-    daily_transaction_aggregate_new_data = {rate: bot_transactions.sum(&:rate)/bot_transactions.count, amount: bot_transactions.sum(&:amount)}
-    daily_transaction_aggregates_repository.update(daily_transaction_aggregate.id, daily_transaction_aggregate_new_data)
+    return if bot_transactions.empty?
+
+    # calculate new data
+    total_amount = (last_aggregate&.total_amount || 0) + bot_transactions.sum(&:amount)
+    total_invested = (last_aggregate&.total_invested || 0) + bot_transactions.sum(&:price)
+
+    new_data = {
+      rate: bot_transactions.sum(&:rate) / bot_transactions.count,
+      amount: bot_transactions.sum(&:amount),
+      total_amount: total_amount,
+      total_invested: total_invested,
+      total_value: total_amount * (bot_transactions.sum(&:rate) / bot_transactions.count)
+    }
+
+    # save new data
+    if daily_transaction_aggregate
+      daily_transaction_aggregates_repository.update(daily_transaction_aggregate.id, new_data)
+    else
+      daily_transaction_aggregates_repository.create(new_data.except('id'))
+    end
   end
+
 end
