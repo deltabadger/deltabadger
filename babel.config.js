d0e02f1cd3e1f43c88caa37734e664b1c1953830
Jan Klosowski <jan@deltabadger.com>
2023-12-25 17:23:06 +0200

    remove duplicate from babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -82,12 +82,6 @@ module.exports = function(api) {
           "loose": true
         }
       ],
-      [
-        '@babel/plugin-proposal-private-methods',
-        {
-          "loose": true
-        }
-      ],
       isProductionEnv && [
         'babel-plugin-transform-react-remove-prop-types',
         {
