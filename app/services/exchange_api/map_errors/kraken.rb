ba62a5edcea5880cdfa161ea0251ca08de548102
Jan Klosowski <jan@deltabadger.com>
2023-12-28 20:40:51 +0200

    small fix

--- app/services/exchange_api/map_errors/kraken.rb
+++ app/services/exchange_api/map_errors/kraken.rb
@@ -8,11 +8,11 @@ module ExchangeApi::MapErrors
         'EGeneral:Invalid arguments:volume' => Error.new('Offer funds are not exceeding minimums', true),
         'EGeneral:Permission denied' => Error.new('Insufficient permissions or unverified account.', false),
         'EGeneral:Temporary lockout' => Error.new('Funds are not exceeding minimums', true),
-        'EOrder:Insufficient funds' => Error.new('Insufficient funds', true),
+        'EOrder:Insufficient funds' => Error.new('Insufficient funds', false),
         'EOrder:Orders limit exceeded' => Error.new('Action limit was exceeded', true),
         'EService:Market in cancel_only mode' => Error.new('Funds are not exceeding minimums', true),
         'EService:Unavailable' => Error.new('The exchange is currently unavailable', true),
-        'Out of funds' => Error.new('Insufficient funds', true)
+        'Out of funds' => Error.new('Insufficient funds', false)
       }
     end
   end
