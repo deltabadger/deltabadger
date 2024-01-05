d5e209763570345aaf29ed83f86a9b360ae32419
Jan Klosowski <jan@deltabadger.com>
2024-01-05 19:31:14 +0200

    add new kucoin error

--- app/services/exchange_api/map_errors/kucoin.rb
+++ app/services/exchange_api/map_errors/kucoin.rb
@@ -2,7 +2,8 @@ module ExchangeApi::MapErrors
   class Kucoin < ExchangeApi::MapErrors::Base
     def errors_mapping
       {
-        'Balance insufficient!' => Error.new('Insufficient funds', false)
+        'Balance insufficient!' => Error.new('Insufficient funds', false),
+        'too many request' => Error.new('API reached limit of requests. We\'ll try again.', true)
       }.freeze
     end
   end
