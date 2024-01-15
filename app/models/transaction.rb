b34af0537acbd2270970c6cd75e7ac29cd089d48
Guillem <guillemavila@gmail.com>
2024-01-15 22:01:53 +0100

    handle transactions in DB with no rate or amount

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -19,20 +19,25 @@ class Transaction < ApplicationRecord
 
   private
 
+  # rubocop:disable Metrics/CyclomaticComplexity, Metrics/PerceivedComplexity
   def set_daily_transaction_aggregate
     return unless success?
 
     transactions_repository = TransactionsRepository.new
     daily_transaction_aggregates_repository = DailyTransactionAggregateRepository.new
     daily_transaction_aggregate = daily_transaction_aggregates_repository.today_for_bot(bot).first
-
     return daily_transaction_aggregates_repository.create(attributes.except('id')) unless daily_transaction_aggregate
 
     bot_transactions = transactions_repository.today_for_bot(bot)
+    bot_transactions_with_rate = bot_transactions.reject { |t| t.rate.nil? }
+    bot_transactions_with_amount = bot_transactions.reject { |t| t.amount.nil? }
+    return if bot_transactions_with_rate.count.zero? || bot_transactions_with_amount.count.zero?
+
     daily_transaction_aggregate_new_data = {
-      rate: bot_transactions.sum(&:rate) / bot_transactions.count,
-      amount: bot_transactions.sum(&:amount)
+      rate: bot_transactions_with_rate.sum(&:rate) / bot_transactions_with_rate.count.to_f,
+      amount: bot_transactions_with_amount.sum(&:amount)
     }
     daily_transaction_aggregates_repository.update(daily_transaction_aggregate.id, daily_transaction_aggregate_new_data)
   end
+  # rubocop:enable Metrics/CyclomaticComplexity, Metrics/PerceivedComplexity
 end
