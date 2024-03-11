e5681bac9383698663b126cc8f350e95600b3fe6
Guillem <guillemavila@gmail.com>
2024-03-11 01:09:14 +0100

    fix DcaProfitGetter

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -2,9 +2,9 @@ class ApplicationMailer < ActionMailer::Base
   default from: ENV['NOTIFICATIONS_SENDER']
   layout 'mailer'
 
-  def initialize(profit_calculator: GetDcaProfit.new)
+  def initialize
     super()
-    @dca_profit = profit_calculator.call(1.year.ago, Time.current)
+    @dca_profit = DcaProfitGetter.call(1.year.ago, Time.current)
   rescue StandardError
     @dca_profit = Result::Failure.new
   end
