ae139b51058e5b0983554de31e9cea7125175e66
Guillem <guillemavila@gmail.com>
2024-03-29 19:36:04 +0100

    first attempt

--- app/controllers/legendary_badger_controller.rb
+++ app/controllers/legendary_badger_controller.rb
@@ -3,5 +3,24 @@ class LegendaryBadgerController < ApplicationController
 
   layout 'legendary_badger'
 
-  def show; end
+  def show
+    @subscription = current_user.subscription
+    puts @subscription.inspect
+  end
+
+  def update
+    if current_user.subscription.update(legendary_badger_params)
+      redirect_to legendary_badger_path, notice: 'Address was successfully added.'
+    else
+      flash.now[:alert] = "#{current_user.subscription.eth_address} is an invalid Ethereum address. Please check your input."
+      @subscription = current_user.subscription.reload
+      render :show
+    end
+  end
+
+  private
+
+  def legendary_badger_params
+    params.require(:subscription).permit(:eth_address)
+  end
 end
