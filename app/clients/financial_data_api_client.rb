a9fcb1167d14a6f3b52fee9b453c811ed8d5313a
Guillem <guillemavila@gmail.com>
2024-04-20 01:40:59 +0200

    Create financial_data_api_client.rb

---
+++ app/clients/financial_data_api_client.rb
@@ -0,0 +1,45 @@
+class FinancialDataApiClient < ApplicationClient
+  URL = ENV.fetch('FINANCIAL_API_URL').freeze
+
+  def self.connection
+    @connection ||= Faraday.new(url: URL, **OPTIONS) do |config|
+      config.request :json
+      config.response :json
+      config.response :raise_error
+      config.response :logger, Rails.logger, headers: false, bodies: true, log_level: :debug
+      config.adapter :net_http_persistent do |http|
+        http.idle_timeout = 100
+      end
+    end
+  end
+
+  def metrics(symbols, allocations, benchmark, start, strategy)
+    with_rescue do
+      response = self.class.connection.get do |req|
+        req.url 'metrics'
+        req.params = {
+          symbols: symbols,
+          allocations: allocations,
+          benchmark: benchmark,
+          start: start,
+          strategy: strategy
+        }
+      end
+      Result::Success.new(response.body)
+    end
+  end
+
+  def smart_allocations(symbols, start, strategy)
+    with_rescue do
+      response = self.class.connection.get do |req|
+        req.url 'smart-allocations'
+        req.params = {
+          symbols: symbols,
+          start: start,
+          strategy: strategy
+        }
+      end
+      Result::Success.new(response.body)
+    end
+  end
+end
