7d5cee073bcaa9dd0cbfc22304f0f0c44a9e5ce2
Guillem <guillemavila@gmail.com>
2024-04-21 01:16:02 +0200

    sample chart

--- app/controllers/portfolio_analyzer_controller.rb
+++ app/controllers/portfolio_analyzer_controller.rb
@@ -1,4 +1,16 @@
 class PortfolioAnalyzerController < ApplicationController
   def index
+    client = FinancialDataApiClient.new
+
+    # allocations_result = client.smart_allocations('BTC/USDT,ETH/USDT', '2021-01-01', 'fixed')
+    # @allocations = allocations_result.data
+    # puts allocations_result.data
+
+    metrics_result = client.metrics('BTC/USDT,ETH/USDT', '0.6819, 0.3181', '^GSPC', '2021-01-01', 'fixed')
+    @metrics = metrics_result.data
+    # puts metrics_result.data
+
+    @data_labels = metrics_result.data['timeSeries']['labels']
+    @data_series = metrics_result.data['timeSeries']['series']
   end
 end
