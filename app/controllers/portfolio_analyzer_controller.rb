b7eef21273365b1794212dc3858a5e96626104e8
Guillem <guillemavila@gmail.com>
2024-04-21 15:43:48 +0200

    Update portfolio_analyzer_controller.rb

--- app/controllers/portfolio_analyzer_controller.rb
+++ app/controllers/portfolio_analyzer_controller.rb
@@ -3,17 +3,17 @@ class PortfolioAnalyzerController < ApplicationController
     client = FinancialDataApiClient.new
 
     allocations_result = client.smart_allocations('BTC/USDT,ETH/USDT', '2021-01-01', 'fixed')
-    if allocations_result.success?
-      allocations = allocations_result.data[2].join(',')
-      puts allocations
+    return if allocations_result.failure?
 
-      metrics_result = client.metrics('BTC/USDT,ETH/USDT', allocations, '^GSPC', '2021-01-01', 'fixed')
-      if metrics_result.success?
-        @metrics = metrics_result.data['metrics']
+    allocations = allocations_result.data[2].join(',')
+    puts allocations
 
-        @data_labels = metrics_result.data['timeSeries']['labels']
-        @data_series = metrics_result.data['timeSeries']['series']
-      end
-    end
+    metrics_result = client.metrics('BTC/USDT,ETH/USDT', allocations, '^GSPC', '2021-01-01', 'fixed')
+    return if metrics_result.failure?
+
+    @metrics = metrics_result.data['metrics']
+
+    @data_labels = metrics_result.data['timeSeries']['labels']
+    @data_series = metrics_result.data['timeSeries']['series']
   end
 end
