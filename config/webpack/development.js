898ab02107b5e9879ca3a2ed7c46d7a91a2be1e5
Guillem <guillemavila@gmail.com>
2024-04-29 12:25:35 +0200

    enable auto reload webpack-dev-server

--- config/webpack/development.js
+++ config/webpack/development.js
@@ -2,4 +2,17 @@ process.env.NODE_ENV = process.env.NODE_ENV || "development";
 
 const environment = require("./environment");
 
+environment.config.merge({
+//   target: "web",
+  devServer: {
+    liveReload: true,
+    watchContentBase: true,
+    contentBase: [
+      "app/views",
+      "app/assets",
+      "public",
+    ],
+  },
+});
+
 module.exports = environment.toWebpackConfig();
