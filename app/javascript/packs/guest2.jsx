62aaaae7f2f49f2c90cb5b44eabdbda3b93ac74a
Guillem <guillemavila@gmail.com>
2024-04-29 13:54:43 +0200

    prettier

--- app/javascript/packs/guest2.jsx
+++ app/javascript/packs/guest2.jsx
@@ -1,32 +1,28 @@
-import "@hotwired/turbo-rails"
-import "../controllers"
-import React from 'react'
-import { createRoot } from 'react-dom/client'
-import I18n from 'i18n-js/index.js.erb'
-import style from '../deltabadger/styles/main.scss'
-import { CookieBanner } from '../deltabadger/components/CookieBanner';
-import { NewsletterForm } from '../deltabadger/components/NewsletterForm';
+import "@hotwired/turbo-rails";
+import "../controllers";
+import React from "react";
+import { createRoot } from "react-dom/client";
+import I18n from "i18n-js/index.js.erb";
+import style from "../deltabadger/styles/main.scss";
+import { CookieBanner } from "../deltabadger/components/CookieBanner";
+import { NewsletterForm } from "../deltabadger/components/NewsletterForm";
 
-require.context('../images', true)
+require.context("../images", true);
 
-I18n.locale = document.head.dataset.locale || I18n.defaultLocale
+I18n.locale = document.head.dataset.locale || I18n.defaultLocale;
 
-if (document.getElementById('cookie_consent')) {
-  document.addEventListener('DOMContentLoaded', () => {
-    createRoot(
-      document.getElementById('cookie_consent')
-    ).render(
+if (document.getElementById("cookie_consent")) {
+  document.addEventListener("DOMContentLoaded", () => {
+    createRoot(document.getElementById("cookie_consent")).render(
       <CookieBanner />
-    )
-  })
+    );
+  });
 }
 
-if (document.getElementById('newsletter')) {
-  document.addEventListener('DOMContentLoaded', () => {
-    createRoot(
-      document.getElementById('newsletter')
-    ).render(
+if (document.getElementById("newsletter")) {
+  document.addEventListener("DOMContentLoaded", () => {
+    createRoot(document.getElementById("newsletter")).render(
       <NewsletterForm />
-    )
-  })
+    );
+  });
 }
