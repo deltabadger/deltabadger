84592a3e9abe3cc4f593819197cde778a9c1cc14
Guillem <guillemavila@gmail.com>
2024-05-09 22:08:28 +0200

    micro fix

--- app/services/portfolio_analyzer_manager/query_assets_getter.rb
+++ app/services/portfolio_analyzer_manager/query_assets_getter.rb
@@ -6,7 +6,7 @@ module PortfolioAnalyzerManager
       all_asset_tickers = PortfolioAnalyzerManager::AllAssetTickersGetter.call
       return all_asset_tickers if all_asset_tickers.failure?
 
-      available_asset_tickers = all_asset_tickers.data - portfolio.assets.map(&:ticker)
+      available_asset_tickers = all_asset_tickers.data.map(&:upcase) - portfolio.assets.map(&:ticker)
       query_assets_tickers = available_asset_tickers.filter { |a| a.include?(query.upcase) }
       query_assets = query_assets_tickers.map { |a| Asset.new(ticker: a, portfolio_id: portfolio.id) }
       Result::Success.new(query_assets)
