a64fd1e04a561ccd2dd5caf0a31d42e200c45a10
Jan Klosowski <jan@deltabadger.com>
2024-05-09 18:06:10 +0300

    body classes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+    def html_class
+        classes = []
+        classes << "db-view--logged-in" if user_signed_in?
+        classes << "db-view--#{controller_name}-#{action_name}"
+        classes.join(' ')
+    end
 end
