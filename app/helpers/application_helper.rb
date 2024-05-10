e56cf83547a0f598d89716e2f255051ea2a85fb5
Jan Klosowski <jan@deltabadger.com>
2024-05-10 16:04:43 +0300

    partial styles for asset partial

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,8 @@
 module ApplicationHelper
     def html_class
         classes = []
-        classes << "db-view--logged-in" if user_signed_in?
-        classes << "db-view--#{controller_name}-#{action_name}"
+        classes << "view--logged-in" if user_signed_in?
+        classes << "view--#{controller_name}-#{action_name}"
         classes.join(' ')
     end
 end
