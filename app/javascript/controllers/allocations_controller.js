ccaa018dce133192356b1338fde929c6df47e075
Guillem <guillemavila@gmail.com>
2024-05-10 14:49:43 +0200

    adjustments

--- app/javascript/controllers/allocations_controller.js
+++ app/javascript/controllers/allocations_controller.js
@@ -1,15 +1,23 @@
-import { Controller } from "@hotwired/stimulus"
+import { Controller } from "@hotwired/stimulus";
 
 // Connects to data-controller="allocations"
 export default class extends Controller {
-  static targets = ["allocation"]
+  static targets = ["allocation", "allocationDisplay"];
 
   update(event) {
-    const newValue = event.target.value;
+    const isRangeInput = event.target.type === "range";
+    const formattedInput = parseFloat(event.target.value.replace(',','.').replace('%', '').trim())
+    let newValue = 0;
+    if (!isNaN(formattedInput)) {
+      newValue = Math.min(1, Math.max(0, isRangeInput ? formattedInput : formattedInput / 100));
+    }
     this.allocationTargets.forEach((target) => {
       if (target !== event.target) {
         target.value = newValue;
       }
+    });
+    this.allocationDisplayTargets.forEach((display) => {
+      display.value = String((newValue * 100).toFixed(2)) + "%";
     });
   }
 }
