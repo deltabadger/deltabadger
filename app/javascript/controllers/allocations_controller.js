37fb771a997001dffa41fa83d6469ef2ee59135e
Guillem <guillemavila@gmail.com>
2024-05-10 13:23:57 +0200

    simplify allocations controller

--- app/javascript/controllers/allocations_controller.js
+++ app/javascript/controllers/allocations_controller.js
@@ -4,23 +4,12 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   static targets = ["allocation"]
 
-  connect() {
-    this.updateAllTargets(); // Optional, to synchronize on load
-  }
-
   update(event) {
     const newValue = event.target.value;
     this.allocationTargets.forEach((target) => {
       if (target !== event.target) {
         target.value = newValue;
       }
-    });
-  }
-
-  updateAllTargets() {
-    const initialValue = this.allocationTargets[0]?.value;
-    this.allocationTargets.forEach((target) => {
-      target.value = initialValue;
     });
   }
 }
