7fcdce2706571df8b617bba04e0ea1fece1e1eb1
Guillem <guillemavila@gmail.com>
2024-05-12 12:06:03 +0200

    fix sliders

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_05_05_113832) do
+ActiveRecord::Schema.define(version: 2024_05_12_095443) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -68,6 +68,7 @@ ActiveRecord::Schema.define(version: 2024_05_05_113832) do
     t.float "allocation", default: 0.0, null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "color"
     t.index ["portfolio_id"], name: "index_assets_on_portfolio_id"
   end
 
