1c51ba4f21a35c3102ad852d17e1e5b1215e95a9
Guillem <guillemavila@gmail.com>
2024-05-18 18:07:17 +0200

    keep smart allocation enabled

--- app/models/asset.rb
+++ app/models/asset.rb
@@ -6,9 +6,6 @@ class Asset < ApplicationRecord
   validates :ticker, presence: true
   validates :allocation, presence: true, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 1 }
 
-  after_create :update_portfolio_smart_allocation_on
-  after_destroy :update_portfolio_smart_allocation_on
-
   enum category: %i[crypto stock index bond], _prefix: :category # add _prefix to avoid conflict with index method
 
   def symbol
@@ -17,11 +14,5 @@ class Asset < ApplicationRecord
 
   def source
     category_crypto? ? 'binance' : 'yfinance'
-  end
-
-  private
-
-  def update_portfolio_smart_allocation_on
-    portfolio.update(smart_allocation_on: portfolio.allocations_are_smart?)
   end
 end
