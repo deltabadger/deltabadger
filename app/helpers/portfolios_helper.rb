0aff5b6a5972241e6e09b24316e41e8c0362f5ce
Guillem <guillemavila@gmail.com>
2024-05-29 22:16:40 +0200

    add backtest_start_date warning

--- app/helpers/portfolios_helper.rb
+++ app/helpers/portfolios_helper.rb
@@ -1,6 +1,9 @@
 module PortfoliosHelper
-  def render_turbo_stream_backtest_results(backtest)
-    turbo_stream.replace 'backtest-results', partial: 'portfolios/backtest_results', locals: { backtest: backtest }
+  def render_turbo_stream_backtest_results(backtest, portfolio)
+    turbo_stream.replace 'backtest-results', partial: 'portfolios/backtest_results', locals: {
+      backtest: backtest,
+      portfolio: portfolio
+    }
   end
 
   def render_turbo_stream_portfolio_assets(portfolio)
