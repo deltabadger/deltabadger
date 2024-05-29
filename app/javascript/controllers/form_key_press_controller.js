35a23f4bd568178a552acbb0a9c0ebc53a763244
Guillem <guillemavila@gmail.com>
2024-05-29 22:36:10 +0200

    handle fast typing for asset search

--- app/javascript/controllers/form_key_press_controller.js
+++ app/javascript/controllers/form_key_press_controller.js
@@ -1,5 +1,8 @@
 import { Controller } from "@hotwired/stimulus"
 
+// Used to automatically search on key press, similar to TradingView.
+// Listens to any key press that is a letter, clicks the button, and sets the input value to the key pressed
+
 // Connects to data-controller="form-key-press"
 export default class extends Controller {
   static targets = ["button"];
@@ -15,33 +18,38 @@ export default class extends Controller {
     window.removeEventListener("modalIsOpen", this.handleModalIsOpenBound);
     window.removeEventListener("keydown", this.handleKeyPressBound);
   }
-  
+
   #handleModalIsOpen(event) {
-    if (event.detail) {
-      window.removeEventListener("keydown", this.handleKeyPressBound);
-    } else {
+    if (!event.detail) {
       window.addEventListener("keydown", this.handleKeyPressBound);
     }
   }
 
   #handleKeyPress(event) {
-    const waitForInputAndFillItWithKey = () => {
+    this.keysPressed = this.keysPressed || [];
+
+    const waitForInputAndFillItWithKeysPressed = () => {
 
       // Dynamically find the input target after the turbo frame has loaded
       const formElement = document.querySelector('[data-form-key-press-target="form"]');
       const inputElement = document.querySelector('[data-form-key-press-target="input"]');
 
       if (inputElement) {
-        inputElement.value = event.key;
+        inputElement.value = this.keysPressed.join("");
+        this.keysPressed = [];
+        window.removeEventListener("keydown", this.handleKeyPressBound);
         formElement.requestSubmit();
       } else {
-        setTimeout(waitForInputAndFillItWithKey, 10);
+        setTimeout(waitForInputAndFillItWithKeysPressed, 50);
       }
     };
 
     if (this.#isLetter(event)) {
-      this.buttonTarget.click();
-      waitForInputAndFillItWithKey();
+      this.keysPressed.push(event.key);
+      if (this.keysPressed.length === 1) {
+        this.buttonTarget.click();
+        waitForInputAndFillItWithKeysPressed();
+      }
     }
   }
 
