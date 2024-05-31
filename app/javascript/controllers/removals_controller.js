1da871284f379cc11216945b5c6a459f45860334
Guillem <guillemavila@gmail.com>
2024-05-31 02:11:45 +0200

    better banner handling

--- app/javascript/controllers/removals_controller.js
+++ app/javascript/controllers/removals_controller.js
@@ -5,4 +5,11 @@ export default class extends Controller {
   remove() {
     this.element.remove()
   }
+
+  submitAndRemove(event) {
+    event.preventDefault()
+    const form = this.element.querySelector('form');
+    form.requestSubmit();
+    this.element.remove();
+  }
 }
