a36014990b8d1c07e4462de2a2561cd30add6788
Jan Klosowski <jan@deltabadger.com>
2024-06-14 23:51:33 +0300

    Translations and better colors

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,17 +10,31 @@ module ApplicationHelper
     turbo_stream.prepend 'flash', partial: 'layouts/flash'
   end
 
-  def protect_ticker_text_contrast(bg_color)
+  def ensure_contrast(bg_color, contrast_ratio)
     # Convert the background color to RGB
-    r = bg_color[1..2].hex
-    g = bg_color[3..4].hex
-    b = bg_color[5..6].hex
+    r = bg_color[1..2].hex / 255.0
+    g = bg_color[3..4].hex / 255.0
+    b = bg_color[5..6].hex / 255.0
 
-    # Calculate the luminance 
-    # Default:   0.299 * r + 0.587 * g + 0.114 * b
-    luminance = (0.262 * r + 0.587 * g + 0.114 * b) / 255
+    # Calculate the relative luminance
+    r = r <= 0.03928 ? r / 12.92 : ((r + 0.055) / 1.055) ** 2.4
+    g = g <= 0.03928 ? g / 12.92 : ((g + 0.055) / 1.055) ** 2.4
+    b = b <= 0.03928 ? b / 12.92 : ((b + 0.055) / 1.055) ** 2.4
 
-    # Return black or white text based on luminance
-    luminance > 0.659 ? "var(--ticker-text-dark)" : "#fff"
+    luminance = 0.2126 * r + 0.7152 * g + 0.0722 * b
+
+    # Calculate contrast ratio
+    def contrast_ratio(l1, l2)
+      (l1 + 0.05) / (l2 + 0.05)
+    end
+
+    white_luminance = 1.0
+    black_luminance = 0.0
+
+    white_contrast = contrast_ratio(white_luminance, luminance)
+    black_contrast = contrast_ratio(luminance, black_luminance)
+
+    # Ensure contrast ratio
+    white_contrast >= contrast_ratio ? "#fff" : "var(--text-dark)"
   end
 end
