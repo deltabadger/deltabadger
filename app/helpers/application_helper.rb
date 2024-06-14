721757e76092762e384431192a2e8a322b26731e
Jan Klosowski <jan@deltabadger.com>
2024-06-14 20:33:32 +0300

    improved tickers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,4 +9,18 @@ module ApplicationHelper
   def render_turbo_stream_flash_messages
     turbo_stream.prepend 'flash', partial: 'layouts/flash'
   end
+
+  def protect_ticker_text_contrast(bg_color)
+    # Convert the background color to RGB
+    r = bg_color[1..2].hex
+    g = bg_color[3..4].hex
+    b = bg_color[5..6].hex
+
+    # Calculate the luminance 
+    # Default:   0.299 * r + 0.587 * g + 0.114 * b
+    luminance = (0.262 * r + 0.587 * g + 0.114 * b) / 255
+
+    # Return black or white text based on luminance
+    luminance > 0.659 ? "var(--ticker-text-dark)" : "#fff"
+  end
 end
