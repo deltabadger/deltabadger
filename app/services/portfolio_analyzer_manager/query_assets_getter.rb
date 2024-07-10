be7f5b4e3af7e0b605fcf239235bc6a06eef2d25
Guillem <guillemavila@gmail.com>
2024-07-10 15:43:25 +0200

    logs

--- app/services/portfolio_analyzer_manager/query_assets_getter.rb
+++ app/services/portfolio_analyzer_manager/query_assets_getter.rb
@@ -5,12 +5,19 @@ module PortfolioAnalyzerManager
     def call(query, portfolio)
       return Result::Success.new([]) if query.blank?
 
+      start_time = Time.now
       symbols_info = PortfolioAnalyzerManager::SymbolsInfoGetter.call
+      end_time = Time.now
+      Rails.logger.info("SymbolsInfoGetter.call took #{end_time - start_time} seconds")
       return symbols_info if symbols_info.failure?
 
+      start_time = Time.now
       query_downcase = query.downcase
       portfolio_assets_api_ids = portfolio.assets.pluck(:api_id)
+      end_time = Time.now
+      Rails.logger.info("portfolio_assets_api_ids took #{end_time - start_time} seconds")
 
+      start_time = Time.now
       query_assets = symbols_info.data.each_with_object([]) do |symbol, assets|
         next if portfolio_assets_api_ids.include?(symbol['id'].to_s)
 
@@ -28,11 +35,27 @@ module PortfolioAnalyzerManager
           ),
           similarities: match_similarities
         }
+        # assets << {
+        #   asset: {
+        #     ticker: symbol['symbol']&.upcase,
+        #     name: symbol['name'],
+        #     portfolio_id: portfolio.id,
+        #     category: symbol['category'],
+        #     color: symbol['color'],
+        #     api_id: symbol['id'].to_s
+        #   },
+        #   similarities: match_similarities
+        # }
       end
+      end_time = Time.now
+      Rails.logger.info("query_assets took #{end_time - start_time} seconds")
 
+      start_time = Time.now
       sorted_assets = query_assets.sort_by do |a|
                         [-a[:similarities][0], -a[:similarities][1], -a[:similarities][2]]
                       end.map { |a| a[:asset] } # rubocop:disable Style/MultilineBlockChain
+      end_time = Time.now
+      Rails.logger.info("sorted_assets took #{end_time - start_time} seconds")
 
       Result::Success.new(sorted_assets)
     end
--- app/services/portfolio_analyzer_manager/symbols_info_getter.rb
+++ app/services/portfolio_analyzer_manager/symbols_info_getter.rb
@@ -5,6 +5,7 @@ module PortfolioAnalyzerManager
     def call(source = 'all')
       expires_in = Utilities::Time.seconds_to_midnight_utc.seconds + 5.minutes
       symbols_data = Rails.cache.fetch('symbols', expires_in: expires_in) do
+        Rails.logger.info('Fetching symbols from API')
         client = FinancialDataApiClient.new
         symbols_result = client.symbols(source)
         return symbols_result if symbols_result.failure?
