8056057abe5d49ff37c404fed0cab38e74061f58
Guillem <guillemavila@gmail.com>
2024-07-11 02:39:13 +0200

    add style and spinner placeholder

--- app/services/portfolio_analyzer_manager/query_assets_getter.rb
+++ app/services/portfolio_analyzer_manager/query_assets_getter.rb
@@ -24,28 +24,29 @@ module PortfolioAnalyzerManager
         match_similarities = get_match_similarities(symbol, query_downcase)
         next if match_similarities.first < 0.8
 
-        assets << {
-          asset: Asset.new(
-            ticker: symbol['symbol']&.upcase,
-            name: symbol['name'],
-            portfolio_id: portfolio.id,
-            category: symbol['category'],
-            color: symbol['color'],
-            api_id: symbol['id'].to_s
-          ),
-          similarities: match_similarities
-        }
         # assets << {
-        #   asset: {
+        #   asset: Asset.new(
         #     ticker: symbol['symbol']&.upcase,
         #     name: symbol['name'],
         #     portfolio_id: portfolio.id,
         #     category: symbol['category'],
         #     color: symbol['color'],
         #     api_id: symbol['id'].to_s
-        #   },
+        #   ),
         #   similarities: match_similarities
         # }
+        assets << {
+          asset: {
+            ticker: symbol['symbol']&.upcase,
+            name: symbol['name'],
+            portfolio_id: portfolio.id,
+            category: symbol['category'],
+            color: symbol['color'],
+            api_id: symbol['id'].to_s,
+            country: symbol['country']
+          },
+          similarities: match_similarities
+        }
       end
       end_time = Time.now
       Rails.logger.info("query_assets took #{end_time - start_time} seconds")
