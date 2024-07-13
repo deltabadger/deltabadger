360ae5879bcc7b52721598e366efea193b11f980
Guillem <guillemavila@gmail.com>
2024-07-14 01:51:14 +0200

    add missing_transactions_info

--- app/services/set_up_sidekiq.rb
+++ app/services/set_up_sidekiq.rb
@@ -9,6 +9,8 @@ class SetUpSidekiq
 
   def fill_sidekiq_queue
     Bot.working.each do |bot|
+      info = missing_transactions_info(bot)
+      puts info if info.present?
       @schedule_transaction.call(bot) if bot.trading?
       @schedule_withdrawal.call(bot) if bot.withdrawal?
     end
@@ -20,5 +22,17 @@ class SetUpSidekiq
 
   def working?(bot)
     bot.status == 'working'
+  end
+
+  def missing_transactions_info(bot)
+    return unless bot.trading? || bot.withdrawal?
+
+    interval = ParseInterval.new.call(bot).to_i
+    next_transaction_at = bot.trading? ? NextTradingBotTransactionAt.new.call(bot) : NextWithdrawalBotTransactionAt.new.call(bot)
+    time_missing_transactions = Time.current - next_transaction_at
+    missed_transactions = (time_missing_transactions.to_f / interval).floor
+    return if missed_transactions.zero?
+
+    "User: #{bot.user.id} bot: #{bot.id} missed transactions: #{missed_transactions}"
   end
 end
