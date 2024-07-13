6e541fc10659dfe5c5a99e164f18c3fd44995d89
Guillem <guillemavila@gmail.com>
2024-07-13 21:14:00 +0200

    filter by bot type

--- app/services/set_up_sidekiq.rb
+++ app/services/set_up_sidekiq.rb
@@ -1,13 +1,19 @@
 class SetUpSidekiq
   def initialize(
-    schedule_transaction: ScheduleTransaction.new
+    schedule_transaction: ScheduleTransaction.new,
+    schedule_withdrawal: ScheduleWithdrawal.new,
+    schedule_webhook: ScheduleWebhook.new
   )
     @schedule_transaction = schedule_transaction
+    @schedule_withdrawal = schedule_withdrawal
+    @schedule_webhook = schedule_webhook
   end
 
   def fill_sidekiq_queue
     Bot.working.each do |bot|
-      @schedule_transaction.call(bot)
+      @schedule_transaction.call(bot) if bot.trading?
+      @schedule_withdrawal.call(bot) if bot.withdrawal?
+      @schedule_webhook.call(bot) if bot.webhook?
     end
 
     true
