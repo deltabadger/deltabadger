d08d79220a618b88280cfdce7f09dbb1e0f95bed
Guillem <guillemavila@gmail.com>
2024-07-13 21:31:22 +0200

    do not schedule webhook

--- app/services/set_up_sidekiq.rb
+++ app/services/set_up_sidekiq.rb
@@ -1,19 +1,16 @@
 class SetUpSidekiq
   def initialize(
     schedule_transaction: ScheduleTransaction.new,
-    schedule_withdrawal: ScheduleWithdrawal.new,
-    schedule_webhook: ScheduleWebhook.new
+    schedule_withdrawal: ScheduleWithdrawal.new
   )
     @schedule_transaction = schedule_transaction
     @schedule_withdrawal = schedule_withdrawal
-    @schedule_webhook = schedule_webhook
   end
 
   def fill_sidekiq_queue
     Bot.working.each do |bot|
       @schedule_transaction.call(bot) if bot.trading?
       @schedule_withdrawal.call(bot) if bot.withdrawal?
-      @schedule_webhook.call(bot) if bot.webhook?
     end
 
     true
