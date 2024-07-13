2f12d7684f3792452bf8f608c1a561e001c31db7
Guillem <guillemavila@gmail.com>
2024-07-13 17:34:44 +0200

    logs

--- app/services/portfolio_analyzer_manager/symbols_info_getter.rb
+++ app/services/portfolio_analyzer_manager/symbols_info_getter.rb
@@ -5,21 +5,7 @@ module PortfolioAnalyzerManager
     def call(source = 'all')
       expires_in = Utilities::Time.seconds_to_midnight_utc.seconds + 5.minutes
 
-
-      start_time = Time.now
-      Rails.cache.fetch('symbols_unc', expires_in: expires_in) do
-        Rails.logger.info('Fetching symbols from API')
-        client = FinancialDataApiClient.new
-        symbols_result = client.symbols(source)
-        return symbols_result if symbols_result.failure?
-
-        symbols_result.data
-      end
-      end_time = Time.now
-      Rails.logger.info("SymbolsInfoGetter.call uncompressed fetch took #{end_time - start_time} seconds")
-
-
-      start_time = Time.now
+      # start_time = Time.now
       compressed_symbols_data = Rails.cache.fetch('symbols', expires_in: expires_in) do
         Rails.logger.info('Fetching symbols from API')
         client = FinancialDataApiClient.new
@@ -30,9 +16,8 @@ module PortfolioAnalyzerManager
         # Zlib::Deflate.deflate(Oj.dump(symbols_result.data))
       end
       symbols_data = JSON.parse(Zlib::Inflate.inflate(compressed_symbols_data))
-      end_time = Time.now
-      Rails.logger.info("SymbolsInfoGetter.call compressed fetch & parse took #{end_time - start_time} seconds")
-
+      # end_time = Time.now
+      # Rails.logger.info("SymbolsInfoGetter.call compressed fetch & parse took #{end_time - start_time} seconds")
 
       Result::Success.new(symbols_data)
     end
