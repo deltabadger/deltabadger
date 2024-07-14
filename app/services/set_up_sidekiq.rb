d6f42ac0e72bd099325d67a27673dfedb4b0e79c
Guillem <guillemavila@gmail.com>
2024-07-14 17:49:43 +0200

    format

--- app/services/set_up_sidekiq.rb
+++ app/services/set_up_sidekiq.rb
@@ -12,7 +12,7 @@ class SetUpSidekiq
       if bot.trading?
         params = continue_params(bot)
         if params.present? && (params[:price] - bot.settings['price'].to_f).positive?
-          puts "User: #{bot.user.id}, bot: #{bot.id}, email: #{bot.user.email}, missed amount: #{params}, bot settings: #{bot.settings}"
+          puts "User: #{bot.user.id}, bot: #{bot.id}, email: #{bot.user.email}, missed amount: #{params[:price].to_f}, bot settings: #{bot.settings}"
         end
         @schedule_transaction.call(bot) unless dry_run
 
@@ -39,6 +39,8 @@ class SetUpSidekiq
     interval = ParseInterval.new.call(bot).to_i
     time_missing_transactions = Time.current - next_withdrawal_at
     (time_missing_transactions.to_f / interval).floor
+  rescue StandardError
+    'unknown'
   end
 
   def continue_params(bot)
