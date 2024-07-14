84dfdb621bc65a0a0aa1849274471f0d62348881
Guillem <guillemavila@gmail.com>
2024-07-14 11:17:44 +0200

    use dry_run flag

--- app/services/set_up_sidekiq.rb
+++ app/services/set_up_sidekiq.rb
@@ -7,21 +7,15 @@ class SetUpSidekiq
     @schedule_withdrawal = schedule_withdrawal
   end
 
-  def fill_sidekiq_queue
+  def fill_sidekiq_queue(dry_run: false)
     Bot.working.each do |bot|
       if bot.trading?
-        restart_params = GetRestartParams.new.call(bot.id)
-        if restart_params[:restartType] == 'missed'
-          puts "User: #{bot.user.id} bot: #{bot.id} missed transactions amount #{restart_params[:missedAmount]}"
-          continue_params = {price: restart_params[:missedAmount], continue_schedule: false}
-        else
-          puts "User: #{bot.user.id} bot: #{bot.id} not missed transactions"
-          continue_params = nil
-        end
-        # @schedule_transaction.call(bot, continue_params: continue_params)
+        params = continue_params(bot)
+        puts "User: #{bot.user.id} bot: #{bot.id} missed transactions amount: #{params}"
+        @schedule_transaction.call(bot, continue_params: params) unless dry_run
       elsif bot.withdrawal?
         puts "User: #{bot.user.id} bot: #{bot.id} missed withdrawals: #{missed_withdrawals(bot)}"
-        # @schedule_withdrawal.call(bot)
+        @schedule_withdrawal.call(bot) unless dry_run
       end
     end
 
@@ -39,5 +33,16 @@ class SetUpSidekiq
     interval = ParseInterval.new.call(bot).to_i
     time_missing_transactions = Time.current - next_withdrawal_at
     (time_missing_transactions.to_f / interval).floor
+  end
+
+  def continue_params(bot)
+    restart_params = GetRestartParams.new.call(bot.id)
+    if restart_params[:restartType] == 'missed'
+      puts "User: #{bot.user.id} bot: #{bot.id} missed transactions amount #{restart_params[:missedAmount]}"
+      { price: restart_params[:missedAmount], continue_schedule: false }
+    else
+      puts "User: #{bot.user.id} bot: #{bot.id} not missed transactions"
+      nil
+    end
   end
 end
