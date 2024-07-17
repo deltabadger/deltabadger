23be827defd2dce7410b6d086429f81c8aa504f6
Jan Klosowski <jan@deltabadger.com>
2024-07-18 02:23:28 +0300

    new basket style

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,31 +10,67 @@ module ApplicationHelper
     turbo_stream.prepend 'flash', partial: 'layouts/flash'
   end
 
-  def ensure_contrast(bg_color, contrast_ratio)
-    # Convert the background color to RGB
-    return if bg_color.blank?
+  def hex_to_rgb(hex)
+    hex = hex.gsub('#', '')
+    r = hex[0..1].hex
+    g = hex[2..3].hex
+    b = hex[4..5].hex
+    [r, g, b]
+  end
 
-    r = bg_color[1..2].hex / 255.0
-    g = bg_color[3..4].hex / 255.0
-    b = bg_color[5..6].hex / 255.0
+  def rgb_to_hex(r, g, b)
+    '#' + [r, g, b].map { |x| x.to_s(16).rjust(2, '0') }.join
+  end
 
-    # Calculate the relative luminance
+  def calculate_luminance(r, g, b)
+    r /= 255.0
+    g /= 255.0
+    b /= 255.0
+
     r = r <= 0.03928 ? r / 12.92 : ((r + 0.055) / 1.055)**2.4
     g = g <= 0.03928 ? g / 12.92 : ((g + 0.055) / 1.055)**2.4
     b = b <= 0.03928 ? b / 12.92 : ((b + 0.055) / 1.055)**2.4
 
-    luminance = 0.2126 * r + 0.7152 * g + 0.0722 * b
+    0.2126 * r + 0.7152 * g + 0.0722 * b
+  end
 
-    # Calculate contrast ratio
-    contrast = ->(l1, l2) { (l1 + 0.05) / (l2 + 0.05) }
+  def lighten_color(r, g, b, percentage)
+    [(r + (255 - r) * percentage).round, (g + (255 - g) * percentage).round, (b + (255 - b) * percentage).round]
+  end
+
+  def darken_color(r, g, b, percentage)
+    [(r * (1 - percentage)).round, (g * (1 - percentage)).round, (b * (1 - percentage)).round]
+  end
+
+  def ensure_contrast(color)
+    return color if color.blank?
+
+    r, g, b = hex_to_rgb(color)
+    luminance = calculate_luminance(r, g, b)
+
+    if luminance > 0.7
+      r, g, b = darken_color(r, g, b, 0.2)
+    elsif luminance < 0.3 && luminance > 0.04
+      r, g, b = lighten_color(r, g, b, 0.1)
+    elsif luminance <= 0.04
+      r, g, b = lighten_color(r, g, b, 0.4)
+    end
+
+    rgb_to_hex(r, g, b)
+  end
+
+  def ensure_contrast_text(bg_color, contrast_ratio)
+    return if bg_color.blank?
+
+    r, g, b = hex_to_rgb(bg_color)
+    luminance = calculate_luminance(r, g, b)
 
     white_luminance = 1.0
     black_luminance = 0.0
 
-    white_contrast = contrast.call(white_luminance, luminance)
-    black_contrast = contrast.call(luminance, black_luminance)
+    white_contrast = (white_luminance + 0.05) / (luminance + 0.05)
+    black_contrast = (luminance + 0.05) / (black_luminance + 0.05)
 
-    # Ensure contrast ratio
     white_contrast >= contrast_ratio ? '#fff' : 'var(--text-dark)'
   end
 end
