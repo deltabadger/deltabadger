976e798f9ebba766d9e18c7b5ae1c86dba97a65d
Guillem <guillemavila@gmail.com>
2024-10-14 23:45:24 +0200

    fix stimulus controllers

--- app/javascript/controllers/form/checkbox_enables_controller.js
+++ app/javascript/controllers/form/checkbox_enables_controller.js
@@ -1,6 +1,6 @@
 import { Controller } from "@hotwired/stimulus"
 
-// Connects to data-controller="checkbox"
+// Connects to data-controller="form--checkbox-enables"
 export default class extends Controller {
   static targets = ["checkbox", "submit"]
 
