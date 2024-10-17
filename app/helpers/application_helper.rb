d77219a34e3a1b367a91f32d80736b6fa3e612a4
Guillem <guillemavila@gmail.com>
2024-10-17 15:40:57 +0200

    rename to main_html_classes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,5 @@
 module ApplicationHelper
-  def html_class
+  def main_html_classes
     classes = []
     classes << 'view--logged-in' if user_signed_in?
     classes << "view--#{controller_name}-#{action_name}"
