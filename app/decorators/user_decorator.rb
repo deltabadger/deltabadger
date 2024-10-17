56ecdc106451412a48a0f41594e1a280dab520d8
Guillem <guillemavila@gmail.com>
2024-10-17 15:40:30 +0200

    remove unused method

--- app/decorators/user_decorator.rb
+++ app/decorators/user_decorator.rb
@@ -4,12 +4,6 @@ class UserDecorator < ActiveRecordDecorator
     super(user)
   end
 
-  delegate :email, :name, :id, to: :__getobj__
-
-  def username
-    __getobj__.email.split('@').first # Use email as the username
-  end
-
   def plan_days_left
     (subscription.end_time.to_date - Date.today).to_i
   end
