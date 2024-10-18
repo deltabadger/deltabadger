891050e0c53e81c1c5769a2213fff65c6ae61132
Jan Klosowski <jan@deltabadger.com>
2024-10-19 01:16:56 +0300

    small fixes

--- app/services/portfolio_analyzer_manager/openai_insights_getter.rb
+++ app/services/portfolio_analyzer_manager/openai_insights_getter.rb
@@ -6,16 +6,16 @@ module PortfolioAnalyzerManager
       expires_in = Utilities::Time.seconds_to_midnight_utc.seconds + 5.minutes
       cache_key = 'openai_insights_' + portfolio.backtest_cache_key
       Rails.cache.fetch(cache_key, expires_in: expires_in) do
-        messages = training_prompts.map { |prompt| { role: "system", content: prompt } }
-        messages << { role: "user", content: prompt(portfolio) }
+        messages = training_prompts.map { |prompt| { role: 'system', content: prompt } }
+        messages << { role: 'user', content: prompt(portfolio) }
         response = client.chat(
           parameters: {
-            model: "gpt-4o",
+            model: 'gpt-4o',
             messages: messages,
-            temperature: 0.7,
+            temperature: 0.7
           }
         )
-        answer = response.dig("choices", 0, "message", "content")
+        answer = response.dig('choices', 0, 'message', 'content')
         return Result::Failure.new(response) if answer.blank?
 
         Result::Success.new(answer)
@@ -30,14 +30,12 @@ module PortfolioAnalyzerManager
 
     def training_prompts
       [
-        "You are a financial advisor explaining things to a client who is new to investing",
+        ''
       ]
     end
 
     def prompt(portfolio)
-      text = ''
-      text += 'This is my portfolio: '
-      text += 'Assets:'
+      text = 'Evaluate my portfolio like Naval. Group up to 5 pros and/or cons. Balance them based on your total evaluation of the portfolio. Use Poor/Neutral/Good/Exeptional scale. Reply in the following HTML format (all texts are examples): <div class="insight insight--summary"><span class="insight__name">Tech Giants Strikes Back</span>. An impressive selection of biggest…</div><div class="insight insight--pro"><span class="insight__name">Sharpe Ratio <b>2.22</b> &middot; Exceptional</span>. Excellent risk-adjusted returns.</div><div class="insight insight--con"><span class="insight__name">CVaR <b>-1.36%</b> &middot; Poor</span>. The conditional value at risk is… not small.</div>'
       portfolio.assets.each do |asset|
         text += " #{asset.ticker} #{(asset.effective_allocation * 100).round(2)}%"
       end
