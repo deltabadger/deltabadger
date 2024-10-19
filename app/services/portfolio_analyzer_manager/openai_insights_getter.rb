5db51368525ed5e3a9125ba757a7d3f9088e54be
Jan Klosowski <jan@deltabadger.com>
2024-10-19 03:36:08 +0300

    small fixes

--- app/services/portfolio_analyzer_manager/openai_insights_getter.rb
+++ app/services/portfolio_analyzer_manager/openai_insights_getter.rb
@@ -35,23 +35,25 @@ module PortfolioAnalyzerManager
     end
 
     def prompt(portfolio)
-      text = 'Evaluate my portfolio like Naval. Group up to 5 pros and/or cons. Balance them based on your total evaluation of the portfolio. Use Poor/Neutral/Good/Exeptional scale. Reply in the following HTML format (all texts are examples): <div class="insight insight--summary"><span class="insight__name">Tech Giants Strikes Back</span>. An impressive selection of biggest…</div><div class="insight insight--pro"><span class="insight__name">Sharpe Ratio <b>2.22</b> &middot; Exceptional</span>. Excellent risk-adjusted returns.</div><div class="insight insight--con"><span class="insight__name">CVaR <b>-1.36%</b> &middot; Poor</span>. The conditional value at risk is… not small.</div>'
+      text = 'Evaluate my portfolio like Naval. Show up to 5 pros and/or cons. Organize them: pros first. Put more attention to metrics like volatility and beta, then those mostly reflecting past like Sharpe. Find geopolitical or technical thesis behind. Notice relevant macro events in the recent years. Point out overlapping assets like ETFs with the same stocks. Use Poor/Neutral/Good/Exeptional scale. Reply in the following HTML format (all texts are examples). I inject your reply directly into my HTML template so do NOT return it as a code snippet: <div class="insight insight--summary"><span class="insight__name">Edgy title</span>. Short narrative about the portfolio in 5 sentences.</div><div class="insight insight--pro"><span class="insight__name">Sharpe Ratio <b>2.22</b> &middot; Exceptional</span>. Excellent risk-adjusted returns.</div><div class="insight insight--con"><span class="insight__name">CVaR <b>-1.36%</b> &middot; Poor</span>. The conditional value at risk is… not small.</div>'
+      text += ' '
+      text += 'Assets:'
       portfolio.assets.each do |asset|
-        text += " #{asset.ticker} #{(asset.effective_allocation * 100).round(2)}%"
+        text += " #{asset.ticker} #{(asset.effective_allocation * 100).round(1)}%"
       end
       text += '. '
       text += "Benchmark: #{portfolio.benchmark_name}. "
-      text += "Risk-free rate: #{(portfolio.risk_free_rate * 100).round(2)}%. "
+      text += "Risk-free rate: #{(portfolio.risk_free_rate * 100).round(1)}%. "
       text += "Metrics for time since #{portfolio.backtest_start_date} to #{1.day.ago.to_date}: "
-      text += "Portfolio performance +#{portfolio.backtest['metrics']['totalReturn'].round(2)}%, "
-      text += "Benchmark performance +#{portfolio.backtest['metrics']['benchmarkTotalReturn'].round(2)}%, "
-      text += "Expected Return #{portfolio.backtest['metrics']['expectedReturn'].round(2)}%, "
-      text += "CAGR #{portfolio.backtest['metrics']['cagr'].round(2)}%, "
-      text += "Volatility #{portfolio.backtest['metrics']['volatility'].round(2)}%, "
-      text += "Max. Drawdown #{portfolio.backtest['metrics']['maxDrawdown'].round(2)}%, "
+      text += "Past performance +#{(portfolio.backtest['metrics']['totalReturn'] * 100).round(0)}%, "
+      text += "Benchmark past performance +#{(portfolio.backtest['metrics']['benchmarkTotalReturn'] * 100).round(0)}%, "
+      text += "Expected Return #{(portfolio.backtest['metrics']['expectedReturn'] * 100).round(1)}%, "
+      text += "CAGR #{(portfolio.backtest['metrics']['cagr'] * 100).round(1)}%, "
+      text += "Volatility #{(portfolio.backtest['metrics']['volatility'] * 100).round(1)}%, "
+      text += "Max. Drawdown #{(portfolio.backtest['metrics']['maxDrawdown'] * 100).round(1)}%, "
       text += "Calmar Ratio #{portfolio.backtest['metrics']['calmarRatio'].round(2)}, "
-      text += "VaR #{portfolio.backtest['metrics']['valueAtRisk'].round(2)}%, "
-      text += "CVaR #{portfolio.backtest['metrics']['conditionalValueAtRisk'].round(2)}%, "
+      text += "VaR #{(portfolio.backtest['metrics']['valueAtRisk'] * 100).round(1)}%, "
+      text += "CVaR #{(portfolio.backtest['metrics']['conditionalValueAtRisk'] * 100).round(1)}%, "
       text += "Sharpe Ratio #{portfolio.backtest['metrics']['sharpeRatio'].round(2)}, "
       text += "Sortino Ratio #{portfolio.backtest['metrics']['sortinoRatio'].round(2)}, "
       text += "Treynor Ratio #{portfolio.backtest['metrics']['treynorRatio'].round(2)}, "
