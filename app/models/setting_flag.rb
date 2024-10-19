66165bb9e7354f96d0f4732cfb8f96ec418637d4
Guillem <guillemavila@gmail.com>
2024-10-20 00:20:13 +0200

    fix class method

--- app/models/setting_flag.rb
+++ app/models/setting_flag.rb
@@ -3,15 +3,15 @@ class SettingFlag < ApplicationRecord
   SHOW_WIRE_PAYMENT = 'show_wire_payment'.freeze
   SHOW_BITCOIN_PAYMENT = 'show_bitcoin_payment'.freeze
 
-  def show_zen_payment?
+  def self.show_zen_payment?
     @show_zen_payment ||= exists?(name: SHOW_ZEN_PAYMENT, value: true)
   end
 
-  def show_bitcoin_payment?
+  def self.show_bitcoin_payment?
     @show_bitcoin_payment ||= exists?(name: SHOW_BITCOIN_PAYMENT, value: true)
   end
 
-  def show_wire_payment?
+  def self.show_wire_payment?
     @show_wire_payment ||= exists?(name: SHOW_WIRE_PAYMENT, value: true)
   end
 end
