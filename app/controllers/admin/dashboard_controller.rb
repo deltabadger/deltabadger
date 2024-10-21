4cd4d61dd78dc0e6876d2b71a88b23dd65222005
Guillem <guillemavila@gmail.com>
2024-10-21 03:55:49 +0200

    remove SubscriptionsRepository

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,11 +1,10 @@
 class Admin::DashboardController < Admin::ApplicationController
   def index
-    subscriptions_repository = SubscriptionsRepository.new
     legendary_plan_stats = PaymentsManager::LegendaryPlanStatsCalculator.call.data
     render :index, locals: {
       number_of_all_users: User.count,
-      number_of_standard_plans: subscriptions_repository.number_of_active_subscriptions('standard'),
-      number_of_pro_plans: subscriptions_repository.number_of_active_subscriptions('pro'),
+      number_of_standard_plans: Subscription.number_of_active_subscriptions('standard'),
+      number_of_pro_plans: Subscription.number_of_active_subscriptions('pro'),
       number_of_legendary_plans_sold: legendary_plan_stats[:legendary_plans_sold_count],
       number_of_legendary_plans_available: legendary_plan_stats[:legendary_plans_for_sale_count],
       number_of_all_bots: Bot.count,
