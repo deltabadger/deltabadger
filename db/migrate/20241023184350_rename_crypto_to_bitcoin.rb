25b2e212846b16cd15ef3e4315d041aedd2c998f
Guillem <guillemavila@gmail.com>
2024-10-23 21:22:16 +0200

    reversible migration

--- db/migrate/20241023184350_rename_crypto_to_bitcoin.rb
+++ db/migrate/20241023184350_rename_crypto_to_bitcoin.rb
@@ -1,5 +1,5 @@
 class RenameCryptoToBitcoin < ActiveRecord::Migration[6.0]
-  def change
+  def up
     rename_column :payments, :crypto_total, :btc_total
     change_column :payments, :btc_total, :decimal, precision: 16, scale: 8
 
@@ -17,5 +17,25 @@ class RenameCryptoToBitcoin < ActiveRecord::Migration[6.0]
 
     rename_column :affiliates, :paid_crypto_commission, :paid_btc_commission
     change_column :affiliates, :paid_btc_commission, :decimal, precision: 16, scale: 8
+  end
+
+  def down
+    rename_column :payments, :btc_total, :crypto_total
+    change_column :payments, :crypto_total, :decimal, precision: 20, scale: 10
+
+    rename_column :payments, :btc_paid, :crypto_paid
+    change_column :payments, :crypto_paid, :decimal, precision: 20, scale: 10
+
+    rename_column :payments, :btc_commission, :crypto_commission
+    change_column :payments, :crypto_commission, :decimal, precision: 20, scale: 10
+
+    rename_column :affiliates, :unexported_btc_commission, :unexported_crypto_commission
+    change_column :affiliates, :unexported_crypto_commission, :decimal, precision: 20, scale: 10
+
+    rename_column :affiliates, :exported_btc_commission, :exported_crypto_commission
+    change_column :affiliates, :exported_crypto_commission, :decimal, precision: 20, scale: 10
+
+    rename_column :affiliates, :paid_btc_commission, :paid_crypto_commission
+    change_column :affiliates, :paid_crypto_commission, :decimal, precision: 20, scale: 10
   end
 end
