2327ec437e0fc7e672de5cc2457a47bebde0485f
Guillem <guillemavila@gmail.com>
2024-10-24 16:43:35 +0200

    standard to basic

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -3,7 +3,7 @@ class Admin::DashboardController < Admin::ApplicationController
     legendary_plan = SubscriptionPlan.legendary
     render :index, locals: {
       number_of_all_users: User.count,
-      number_of_standard_plans: SubscriptionPlan.standard.active_subscriptions_count,
+      number_of_basic_plans: SubscriptionPlan.basic.active_subscriptions_count,
       number_of_pro_plans: SubscriptionPlan.pro.active_subscriptions_count,
       number_of_legendary_plans_sold: legendary_plan.active_subscriptions_count,
       number_of_legendary_plans_available: legendary_plan.for_sale_count,
