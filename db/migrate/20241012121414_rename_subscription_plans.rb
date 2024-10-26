07d8b6bfeed081787990a2b14d6cae1330c6f194
Guillem <guillemavila@gmail.com>
2024-10-27 01:20:49 +0200

    rename test users emails

--- db/migrate/20241012121414_rename_subscription_plans.rb
+++ db/migrate/20241012121414_rename_subscription_plans.rb
@@ -20,6 +20,13 @@ class RenameSubscriptionPlans < ActiveRecord::Migration[6.0]
         SET name = 'legendary'
         WHERE name = 'legendary_badger'
     SQL
+
+    User.find_each do |user|
+      user.update_columns(email: 'free@test.com') if user.email == 'saver@test.com'
+      user.update_columns(email: 'basic@test.com') if user.email == 'investor@test.com'
+      user.update_columns(email: 'pro@test.com') if user.email == 'hodler@test.com'
+      user.update_columns(email: 'legendary@test.com') if user.email == 'legendary_badger@test.com'
+    end
   end
 
   def down
@@ -43,5 +50,13 @@ class RenameSubscriptionPlans < ActiveRecord::Migration[6.0]
         SET name = 'legendary_badger'
         WHERE name = 'legendary'
     SQL
+
+
+    User.find_each do |user|
+      user.update_columns(email: 'saver@test.com') if user.email == 'free@test.com'
+      user.update_columns(email: 'investor@test.com') if user.email == 'basic@test.com'
+      user.update_columns(email: 'hodler@test.com') if user.email == 'pro@test.com'
+      user.update_columns(email: 'legendary_badger@test.com') if user.email == 'legendary@test.com'
+    end
   end
 end
