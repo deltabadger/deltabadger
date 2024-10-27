03d899adfc1e146c20c25b95bcd9b1a0363e451e
Guillem <guillemavila@gmail.com>
2024-10-27 05:56:36 +0100

    better cache

--- app/helpers/locales_helper.rb
+++ app/helpers/locales_helper.rb
@@ -4,7 +4,7 @@ module LocalesHelper
   end
 
   def localized_payment_country_options
-    VatRate.all_in_display_order.map do |vat_rate|
+    @localized_payment_country_options ||= VatRate.all_in_display_order.map do |vat_rate|
       [
         vat_rate.country == VatRate::NOT_EU ? t('helpers.label.payment.other') : vat_rate.country,
         vat_rate.country
