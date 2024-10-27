7a5304da3f2b645c33096a81b8d83c16e1ce3611
Guillem <guillemavila@gmail.com>
2024-10-27 18:35:22 +0100

    better instance variables

--- app/controllers/legendaries_controller.rb
+++ app/controllers/legendaries_controller.rb
@@ -1,14 +1,10 @@
 class LegendariesController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_show_instance_variables
 
-  def show
-    @subscription = current_user.subscription
-    @address_pattern = Ethereum.address_pattern
-  end
+  def show; end
 
   def update
-    @subscription = current_user.subscription
-
     if legendary_params[:eth_address_confirmation] && @subscription.update(eth_address: legendary_params[:eth_address])
       ClaimNftMailer.form_submission_email(
         current_user.email,
@@ -22,6 +18,11 @@ class LegendariesController < ApplicationController
   end
 
   private
+
+  def set_show_instance_variables
+    @subscription = current_user.subscription
+    @address_pattern = Ethereum.address_pattern
+  end
 
   def legendary_params
     params.require(:subscription).permit(:eth_address, :eth_address_confirmation)
