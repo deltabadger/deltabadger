aa0dd957a836393284390137be2468d10576cd23
Guillem <guillemavila@gmail.com>
2024-10-29 05:14:47 +0100

    enforce NFT id uniqueness at database level

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_10_23_225401) do
+ActiveRecord::Schema.define(version: 2024_10_29_034943) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -234,6 +234,7 @@ ActiveRecord::Schema.define(version: 2024_10_23_225401) do
     t.datetime "first_month_ending_sent_at"
     t.integer "nft_id"
     t.string "eth_address"
+    t.index ["nft_id"], name: "index_subscriptions_on_nft_id", unique: true, where: "(nft_id IS NOT NULL)"
     t.index ["subscription_plan_variant_id"], name: "index_subscriptions_on_subscription_plan_variant_id"
     t.index ["user_id"], name: "index_subscriptions_on_user_id"
   end
