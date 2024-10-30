f72a124fad867d4a7192eefa7714cfbc1aed464c
Jan Klosowski <jan@deltabadger.com>
2024-10-30 11:46:48 +0200

    new styles

--- app/helpers/numbers_helper.rb
+++ app/helpers/numbers_helper.rb
@@ -1,9 +1,10 @@
 module NumbersHelper
-  def format_price(price, currency)
+  def format_price(price, currency, decimal_places = 2)
+    formatted_price = format("%0.0#{decimal_places}f", price)
     if currency == 'EUR'
-      "#{format('%0.02f', price)}€"
+      "<span class=price-ticker>€</span><span class=price-amount>#{formatted_price}</span>".html_safe
     else
-      "$#{format('%0.02f', price)}"
+      "<span class=price-ticker>$</span><span class=price-amount>#{formatted_price}</span>".html_safe
     end
   end
 
