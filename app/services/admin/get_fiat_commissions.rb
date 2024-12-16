9258609b5ffbdcaee306bcc172140dbfc93538de
Guillem <guillemavila@gmail.com>
2024-12-16 23:41:51 +0100

    use payments commissions

--- app/services/admin/get_fiat_commissions.rb
+++ app/services/admin/get_fiat_commissions.rb
@@ -1,21 +1,18 @@
 module Admin
   class GetFiatCommissions < BaseService
     def call
-      referred_fiat_payments = get_referred_fiat_payments
-      return Result::Failure.new("Couldn\\'t fetch the payments' data") unless referred_fiat_payments.success?
+      referred_fiat_payments = Payment.paid.where(payment_type: %w[stripe zen wire])
+                                      .where('commission > ?', 0)
+                                      .where.not(external_statuses: 'Commission granted')
 
-      subscription_plan_variants = SubscriptionPlanVariant.all.map { |s| [s['id'], s] }.to_h
-      referred_fiat_payments.data.each do |payment|
-        subscription_plan_variant = subscription_plan_variants[(payment['subscription_plan_variant_id'])]
+      btc_price_result = get_btc_price
+      return btc_price_result if btc_price_result.failure?
+
+      referred_fiat_payments.each do |payment|
+        commission_in_btc = (payment.commission / btc_price_result.data[payment.currency]).ceil(8)
         affiliate = Affiliate.find(User.find(payment['user_id'])['referrer_id'])
-        affiliate_commission_percent = affiliate.total_bonus_percent - affiliate.discount_percent
-        commission_in_btc_result = get_commission_in_btc(payment.currency,
-                                                         affiliate_commission_percent,
-                                                         subscription_plan_variant)
-        return commission_in_btc_result if commission_in_btc_result.failure?
-
-        update_commission(affiliate, commission_in_btc_result.data)
-        payment.update(external_statuses: 'Commission granted')
+        update_commission(affiliate, commission_in_btc)
+        payment.update!(external_statuses: 'Commission granted')
       end
       Result::Success.new("Fiat payments\\' commissions granted")
     rescue StandardError => e
@@ -24,36 +21,20 @@ module Admin
 
     private
 
-    # get paid fiat payments without the granted commission
-    def get_referred_fiat_payments
-      fiat_payments_list = Payment.paid.where(payment_type: %w[stripe zen wire])
-                                  .where.not(external_statuses: 'Commission granted')
-      fiat_payments_with_referrers = fiat_payments_list.to_a.filter do |payment|
-        !User.find(payment['user_id'])['referrer_id'].nil?
+    def get_btc_price
+      btc_price = {}
+      %w[EUR USD].each do |currency|
+        btc_price_result = Admin::BitcoinPriceGetter.call(quote: currency)
+        return btc_price_result if btc_price_result.failure?
+
+        btc_price[currency] = btc_price_result.data
       end
-      Result::Success.new(fiat_payments_with_referrers)
-    rescue StandardError
-      Result::Failure.new("Couldn\\'t fetch the payments' data")
-    end
-
-    def get_btc_cost(subscription_plan_variant, currency)
-      undiscounted_cost = currency == 'EUR' ? subscription_plan_variant.cost_eur : subscription_plan_variant.cost_usd
-      btc_price_result = Admin::BitcoinPriceGetter.call(quote: currency)
-      return btc_price_result if btc_price_result.failure?
-
-      Result::Success.new(undiscounted_cost / btc_price_result.data)
-    end
-
-    def get_commission_in_btc(currency, commission_percent, subscription_plan_variant)
-      btc_cost_result = get_btc_cost(subscription_plan_variant, currency)
-      return btc_cost_result if btc_cost_result.failure?
-
-      Result::Success.new((commission_percent * btc_cost_result.data).ceil(8))
+      Result::Success.new(btc_price)
     end
 
     def update_commission(affiliate, commission)
       previous_btc_commission = affiliate.unexported_btc_commission
-      affiliate.update(unexported_btc_commission: previous_btc_commission + commission)
+      affiliate.update!(unexported_btc_commission: previous_btc_commission + commission)
     end
   end
 end
