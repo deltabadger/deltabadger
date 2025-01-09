0759b1537eb7dd62fc42c9af9fe71846643ca75b
Guillem <guillemavila@gmail.com>
2025-01-09 19:59:07 +0100

    better errors

--- app/models/affiliate.rb
+++ app/models/affiliate.rb
@@ -87,8 +87,7 @@ class Affiliate < ApplicationRecord
   private
 
   def valid_btc_address
-    return if btc_address.nil? || ::Bitcoin.valid_address?(btc_address)
-
-    errors.add(:btc_address, :invalid)
+    valid_address = btc_address.nil? || ::Bitcoin.valid_address?(btc_address)
+    errors.add(:btc_address, :invalid) unless valid_address
   end
 end
