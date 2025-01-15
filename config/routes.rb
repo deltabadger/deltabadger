e9ef4e621eb283c0fe28aea34434327eb5550dca
Jan Klosowski <jan@deltabadger.com>
2025-01-15 15:09:46 +0200

    new file

--- config/routes.rb
+++ config/routes.rb
@@ -75,6 +75,7 @@ Rails.application.routes.draw do
     devise_scope :user do
       get  'signup', to: 'users/registrations#new', as: 'new_user_registration'
       post 'signup', to: 'users/registrations#create', as: 'user_registration'
+      post 'verify_two_factor', to: 'users/sessions#verify_two_factor', as: 'verify_two_factor_user_session'
     end
 
     resource :affiliate, path: 'referral-program', only: [:new, :create, :show] do
