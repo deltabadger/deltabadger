ada0e51ba842c411c83b95478e255b7d6ff9b88b
Guillem <guillemavila@gmail.com>
2025-01-26 18:02:42 +0100

    Update update_email_job.rb

--- app/jobs/sendgrid/update_email_job.rb
+++ app/jobs/sendgrid/update_email_job.rb
@@ -2,20 +2,32 @@ require 'utilities/hash'
 
 class Sendgrid::UpdateEmailJob < SendgridJob
   def perform(old_email, new_email)
-    result = client.get_contacts_by_emails(emails: [old_email])
-    contact_id = Utilities::Hash.dig_or_raise(result.data, 'result', old_email, 'contact', 'id')
-    contact_lists_ids = Utilities::Hash.dig_or_raise(result.data, 'result', old_email, 'contact', 'list_ids')
-    contact_name = result.data.dig('result', old_email, 'contact', 'first_name')
+    old_contact_details = Rails.cache.fetch("sendgrid_contact_details_#{old_email}", expires_in: 30.days) do
+      result = client.get_contacts_by_emails(emails: [old_email])
+      contact_name = Utilities::Hash.dig_or_raise(result.data, 'result', old_email, 'contact', 'first_name')
+      contact_id = Utilities::Hash.dig_or_raise(result.data, 'result', old_email, 'contact', 'id')
+      contact_lists_ids = Utilities::Hash.dig_or_raise(result.data, 'result', old_email, 'contact', 'list_ids')
+      {
+        contact_name: contact_name,
+        contact_id: contact_id,
+        contact_lists_ids: contact_lists_ids
+      }.compact
+    end
 
-    result = client.delete_contacts(ids: [contact_id])
+    result = client.delete_contacts(ids: [old_contact_details[:contact_id]])
     raise StandardError, result.errors if result.failure?
 
     contact = {
       email: new_email,
-      first_name: contact_name
+      first_name: old_contact_details[:contact_name]
     }.compact
 
-    result = client.add_or_update_contacts(list_ids: [contact_lists_ids], contacts: [contact])
+    result = client.add_or_update_contacts(
+      list_ids: old_contact_details[:contact_lists_ids],
+      contacts: [contact]
+    )
     raise StandardError, result.errors if result.failure?
+
+    Rails.cache.delete("sendgrid_contact_details_#{old_email}")
   end
 end
