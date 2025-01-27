46a625bf9902d70abc4baae18e262074b04fa5d4
Guillem <guillemavila@gmail.com>
2025-01-27 20:47:23 +0100

    remove TransactionsRepository

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -5,11 +5,13 @@ class Transaction < ApplicationRecord
 
   after_create :set_daily_transaction_aggregate
 
+  scope :for_bot, ->(bot) { where(bot_id: bot.id).order(id: :desc) }
+  scope :today_for_bot, ->(bot) { for_bot(bot).where('created_at >= ?', Date.today.beginning_of_day) }
+  scope :for_bot_by_status, ->(bot, status: :success) { where(bot_id: bot.id).where(status: status).order(created_at: :desc) }
+
   validates :bot, presence: true
 
-  def get_errors
-    JSON.parse(error_messages)
-  end
+  BTC = %w[XXBT XBT BTC].freeze
 
   def price
     return 0.0 unless rate.present?
@@ -17,17 +19,37 @@ class Transaction < ApplicationRecord
     amount * rate
   end
 
+  # def count_by_status_and_exchange(status, exchange)
+  #   joins(:bot).where(bots: { exchange_id: exchange.id }).where(status: status).count
+  # end
+
+  # def total_btc_sold
+  #   total_btc_by_type('sell')
+  # end
+
+  # def total_btc_bought_day_ago
+  #   joins(:bot)
+  #     .where("bots.settings->>'type' = 'buy' AND bots.settings->>'base' IN (?) AND transactions.created_at < ? ",
+  #            BTC, 1.days.ago)
+  #     .sum(:amount).ceil(8)
+  # end
+
   private
+
+  # def total_btc_by_type(type)
+  #   joins(:bot)
+  #     .where("bots.settings->>'type' = ? AND bots.settings->>'base' IN (?)", type, BTC)
+  #     .sum(:amount).ceil(8)
+  # end
 
   # rubocop:disable Metrics/CyclomaticComplexity, Metrics/PerceivedComplexity
   def set_daily_transaction_aggregate
     return unless success?
 
-    transactions_repository = TransactionsRepository.new
     daily_transaction_aggregate = DailyTransactionAggregate.today_for_bot(bot).first
     return DailyTransactionAggregate.create(attributes.except('id')) unless daily_transaction_aggregate
 
-    bot_transactions = transactions_repository.today_for_bot(bot)
+    bot_transactions = today_for_bot(bot)
     bot_transactions_with_rate = bot_transactions.reject { |t| t.rate.nil? }
     bot_transactions_with_amount = bot_transactions.reject { |t| t.amount.nil? }
     return if bot_transactions_with_rate.count.zero? || bot_transactions_with_amount.count.zero?
