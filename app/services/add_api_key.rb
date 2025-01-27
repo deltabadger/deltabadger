e234bc72c27dfa24effb00c7b78e2584cf5958ba
Guillem <guillemavila@gmail.com>
2025-01-27 18:45:36 +0100

    remove ApiKeysRepository

--- app/services/add_api_key.rb
+++ app/services/add_api_key.rb
@@ -1,16 +1,13 @@
 class AddApiKey < BaseService
   API_KEYS_VALIDATION_QUEUE = 'api_keys_validation'.freeze
   def initialize(
-    api_keys_repository: ApiKeysRepository.new,
     validator_worker: ApiKeyValidatorWorker
   )
-
-    @api_keys_repository = api_keys_repository
     @validator_worker = validator_worker
   end
 
   def call(params)
-    saved_api_key = @api_keys_repository.save(ApiKey.new(params.merge(status: 'pending')))
+    saved_api_key = ApiKey.save(ApiKey.new(params.merge(status: 'pending')))
     @validator_worker.sidekiq_options(queue: API_KEYS_VALIDATION_QUEUE)
     @validator_worker.perform_at(
       Time.now,
