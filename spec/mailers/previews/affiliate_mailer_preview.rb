54ffc078214d2b881fa08c25b1020a31fc0bb6a4
Jan Klosowski <jan@deltabadger.com>
2025-02-04 16:04:57 +0200

    New emails

---
+++ spec/mailers/previews/affiliate_mailer_preview.rb
@@ -0,0 +1,28 @@
+# Preview all emails at http://localhost:3000/rails/mailers/affiliate_mailer
+class AffiliateMailerPreview < ActionMailer::Preview
+  def new_btc_address_confirmation
+    user = User.new(email: 'test@example.com', name: 'Mathias')
+    AffiliateMailer.with(
+      user: user,
+      new_btc_address: '1A1zP1eP5QGefi2DMPTfTL5SLmv7DivfNa',
+      token: 'abc123'
+    ).new_btc_address_confirmation
+  end
+
+  def referrals_payout_notification
+    user = User.new(email: 'test@example.com', name: 'Mathias')
+    AffiliateMailer.with(
+      user: user,
+      amount: 100
+    ).referrals_payout_notification
+  end
+
+  def registration_reminder
+    referrer = Affiliate.new
+    referrer.user = User.new(email: 'test@example.com', name: 'Mathias')
+    AffiliateMailer.with(
+      referrer: referrer,
+      amount: 50
+    ).registration_reminder
+  end
+end
