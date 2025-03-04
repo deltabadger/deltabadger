86076758eee65145ccca065187c2e86e8179da8e
Guillem <guillemavila@gmail.com>
2025-03-04 05:17:14 +0100

    docs

--- app/jobs/application_job.rb
+++ app/jobs/application_job.rb
@@ -1,5 +1,10 @@
 class ApplicationJob < ActiveJob::Base
   include Bullet::ActiveJob if Rails.env.development?
+
+  # For retries we dont use ActiveJob retry_on exponential backoff and builtin executions count
+  # because this would ignore retries being placed in the retries section in the Sidekiq dashboard,
+  # and potentially ignore Sentry alerts. Instead we use middleware-injected retry_count from Sidekiq.
+
   attr_accessor :retry_count
 
   def deserialize(job_data)
