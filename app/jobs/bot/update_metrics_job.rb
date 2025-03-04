808b79f863cdaa2bfd73a00d3634e763abfd22b9
Guillem <guillemavila@gmail.com>
2025-03-04 16:50:40 +0100

    pass ActiveRecord instances to jobs

--- app/jobs/bot/update_metrics_job.rb
+++ app/jobs/bot/update_metrics_job.rb
@@ -4,15 +4,14 @@ class Bot::UpdateMetricsJob < BotJob
   # FIXME: ideally calling this job should kill any running Bot::UpdateMetricsJob
   # for the same bot, but by now we are only cancelling other enqueued jobs.
 
-  def perform(bot_id)
-    cancel_other_jobs(bot_id)
-    bot = Bot.find(bot_id)
+  def perform(bot)
+    cancel_other_jobs(bot)
     bot.metrics(recalculate: true)
   end
 
   private
 
-  def cancel_other_jobs(bot_id)
+  def cancel_other_jobs(bot)
     sidekiq_places = [
       Sidekiq::RetrySet.new,
       Sidekiq::ScheduledSet.new,
@@ -22,7 +21,7 @@ class Bot::UpdateMetricsJob < BotJob
       place.each do |job|
         job.delete if job.queue == queue_name &&
                       job.display_class == 'Bot::UpdateMetricsJob' &&
-                      job.display_args == [bot_id]
+                      job.display_args == [{ '_aj_globalid' => bot.to_global_id.to_s }]
       end
     end
   end
