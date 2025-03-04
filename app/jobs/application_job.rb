827ef70e7308f5891f28b7493112c16d9ac401da
Guillem <guillemavila@gmail.com>
2025-03-04 04:59:28 +0100

    notifications fixes

--- app/jobs/application_job.rb
+++ app/jobs/application_job.rb
@@ -1,3 +1,9 @@
 class ApplicationJob < ActiveJob::Base
   include Bullet::ActiveJob if Rails.env.development?
+  attr_accessor :retry_count
+
+  def deserialize(job_data)
+    self.retry_count = job_data['retry_count'] || 0
+    super
+  end
 end
