38fee265a37a6d095609a1f7036c1ff12fc82614
Guillem <guillemavila@gmail.com>
2025-03-15 00:53:29 +0100

    fixes

--- app/workers/make_withdrawal_worker.rb
+++ app/workers/make_withdrawal_worker.rb
@@ -3,7 +3,11 @@ class MakeWithdrawalWorker
 
   def perform(bot_id)
     MakeWithdrawal.call(bot_id)
+    bot = Bot.find(bot_id)
+    Bot::BroadcastStatusBarUpdateJob.perform_later(bot, condition: 'next_action_job_at.present?')
   rescue StandardError => e # prevent job from retrying
     Raven.capture_exception(e)
+    bot = Bot.find(bot_id)
+    Bot::BroadcastStatusBarUpdateJob.perform_later(bot, condition: 'next_action_job_at.present?')
   end
 end
