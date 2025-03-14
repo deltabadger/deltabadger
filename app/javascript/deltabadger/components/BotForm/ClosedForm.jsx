0b2cac44fd6b8456a4159fb08268baedef269240
Jan Klosowski <jan@deltabadger.com>
2025-03-14 16:02:23 +0200

    new bot button improvements

--- app/javascript/deltabadger/components/BotForm/ClosedForm.jsx
+++ app/javascript/deltabadger/components/BotForm/ClosedForm.jsx
@@ -4,7 +4,7 @@ import I18n from 'i18n-js'
 export const ClosedForm = ({ handleSubmit }) => (
   <div className="db-bots__item d-flex db-add-more-bots">
     <button onClick={() => handleSubmit()} className="button button--primary">
-      <span className="d-none">{I18n.t('bots.add_new_bot')}</span>
+      <span className="d-none">{I18n.t('bot.new')}</span>
       <i className="material-icons">add</i>
     </button>
   </div>
