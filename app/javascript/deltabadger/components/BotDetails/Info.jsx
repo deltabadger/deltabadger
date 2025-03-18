7759425704183df55919648be503cc0c9e8a31f2
Jan Klosowski <jan@deltabadger.com>
2025-03-18 08:56:12 +0200

    fix react tabs

--- app/javascript/deltabadger/components/BotDetails/Info.jsx
+++ app/javascript/deltabadger/components/BotDetails/Info.jsx
@@ -4,16 +4,16 @@ import { RawHTML } from '../RawHtml'
 
 export const Info = ({ active }) => (
   <div className={`legacy-tab ${active ? 'active' : ''}`} id="info" role="tabpanel" aria-labelledby="info-tab">
-    <RawHTML className="db-showif--pick-exchange">
+    <RawHTML className="legacy-tab__section db-showif--pick-exchange">
       {I18n.t('bots.details.info.what_is_dca_html')}
     </RawHTML>
     <RawHTML className="db-bot-info--dca">
       {I18n.t('bots.details.info.smart_intervals_html')}
     </RawHTML>
-    <RawHTML className="db-showif--setup db-bot-info--dca second-info-title">
+    <RawHTML className="legacy-tab__section db-showif--setup db-bot-info--dca second-info-title">
       {I18n.t('bots.details.info.daily_weekly_monthly_html')}
     </RawHTML>
-    <RawHTML className="db-showif--setup db-bot-info--withdrawal">
+    <RawHTML className="legacy-tab__section db-showif--setup db-bot-info--withdrawal">
       {I18n.t('bots.details.info.withdrawal_html')}
     </RawHTML>
     <div className="db-bot-info--webhook">
