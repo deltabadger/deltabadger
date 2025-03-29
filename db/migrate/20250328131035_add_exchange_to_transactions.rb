3627d22c502d88aecf862fcdbd500248a049df46
Guillem <guillemavila@gmail.com>
2025-03-29 02:56:19 +0100

    exchange in Transaction fixes

--- db/migrate/20250328131035_add_exchange_to_transactions.rb
+++ db/migrate/20250328131035_add_exchange_to_transactions.rb
@@ -5,6 +5,8 @@ class AddExchangeToTransactions < ActiveRecord::Migration[6.0]
     Transaction.find_each do |transaction|
       transaction.update_column(:exchange_id, transaction.bot.exchange_id)
     end
+
+    change_column_null :transactions, :exchange_id, false
   end
 
   def down
