09ec19930827b5eeb7819146f2d1bc70efae8cf3
Jan Klosowski <jan@deltabadger.com>
2025-04-18 14:52:17 +0300

    generalize show-hide Stimulus

--- app/javascript/controllers/show_hide_controller.js
+++ app/javascript/controllers/show_hide_controller.js
@@ -1,16 +1,28 @@
 import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="show-hide"
+// Toggles specified classes on target elements when the toggle action is triggered.
+//
+// Example usage:
+// <button data-action="click->show-hide#toggle">Toggle Classes</button>
+// <div data-show-hide-target="togglable" data-show-hide-toggle-class-value='["class-one", "class-two"]'>
+//   Content with classes to toggle
+// </div>
+//
 export default class extends Controller {
-  static targets = ["hideable", "toggleButton"]
+  static targets = ["togglable"]
+  static values = { toggleClass: Array }
 
   toggle() {
-    // Toggle visibility of elements with the hideable target
-    this.hideableTargets.forEach(element => element.classList.toggle("hidden"));
-
-    // Toggle the active class on the toggleButton if it exists
-    if (this.hasToggleButtonTarget) {
-      this.toggleButtonTarget.classList.toggle("active");
+    if (!this.hasToggleClassValue) {
+      console.error("Missing data-show-hide-toggle-class-value attribute on controller element.");
+      return;
     }
+
+    this.togglableTargets.forEach(element => {
+      this.toggleClassValue.forEach(klass => {
+        element.classList.toggle(klass);
+      });
+    });
   }
 }
