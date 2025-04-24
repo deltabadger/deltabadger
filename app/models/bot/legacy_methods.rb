a1c5231d2c77f1968728f00b7ab1657976ab794e
Guillem <guillemavila@gmail.com>
2025-04-24 19:19:59 +0200

    add pnl to legacy bots

--- app/models/bot/legacy_methods.rb
+++ app/models/bot/legacy_methods.rb
@@ -158,4 +158,14 @@ module Bot::LegacyMethods
   def selected_subaccount
     settings.fetch('selected_subaccount', '')
   end
+
+  def pnl
+    return if transactions.empty? || bot_type == 'withdrawal'
+
+    stats = Presenters::Api::Stats.call(
+      bot: self,
+      transactions: daily_transaction_aggregates.order(created_at: :desc)
+    )
+    (stats[:currentValue].to_f - stats[:totalInvested].to_f) / stats[:totalInvested].to_f
+  end
 end
