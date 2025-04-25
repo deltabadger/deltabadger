e82c836288878144388de95608d04f87dbf4ca8e
Guillem <guillemavila@gmail.com>
2025-04-25 18:46:50 +0200

    better broadcast status bar

--- app/workers/make_withdrawal_worker.rb
+++ app/workers/make_withdrawal_worker.rb
@@ -4,10 +4,10 @@ class MakeWithdrawalWorker
   def perform(bot_id)
     MakeWithdrawal.call(bot_id)
     bot = Bot.find(bot_id)
-    Bot::BroadcastStatusBarUpdateJob.perform_later(bot, condition: 'next_action_job_at.present?')
+    bot.broadcast_status_bar_update
   rescue StandardError => e # prevent job from retrying
     Raven.capture_exception(e)
     bot = Bot.find(bot_id)
-    Bot::BroadcastStatusBarUpdateJob.perform_later(bot, condition: 'next_action_job_at.present?')
+    bot.broadcast_status_bar_update
   end
 end
