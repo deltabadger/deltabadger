c5606dd3c1310c904e656e87a9123fe6b3ee146f
Guillem <guillemavila@gmail.com>
2025-04-30 20:32:20 +0200

    add timezone

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2025_04_16_185148) do
+ActiveRecord::Schema.define(version: 2025_04_30_164002) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -342,6 +342,7 @@ ActiveRecord::Schema.define(version: 2025_04_16_185148) do
     t.boolean "news_banner_dismissed", default: false
     t.boolean "sendgrid_unsubscribed", default: false
     t.boolean "has_community_access", default: false, null: false
+    t.string "time_zone", default: "UTC", null: false
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
