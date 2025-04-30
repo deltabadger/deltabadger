aa747b9fd7ae68cc6d77468e8cf73e1e054a9a09
Guillem <guillemavila@gmail.com>
2025-04-30 20:33:01 +0200

    Update 20250328131035_add_exchange_to_transactions.rb

--- db/migrate/20250328131035_add_exchange_to_transactions.rb
+++ db/migrate/20250328131035_add_exchange_to_transactions.rb
@@ -2,10 +2,10 @@ class AddExchangeToTransactions < ActiveRecord::Migration[6.0]
   def up
     add_reference :transactions, :exchange, foreign_key: true
 
-    Transaction.find_each do |transaction|
-      Rails.logger.info "Updating transaction #{transaction.id}"
-      puts "Updating transaction #{transaction.id}"
-      transaction.update_column(:exchange_id, transaction.bot.exchange_id)
+    Bot.pluck(:id, :exchange_id).sort.each do |id, exchange_id|
+      Rails.logger.info "Updating transactions for bot #{id} with exchange #{exchange_id}"
+      puts "Updating transactions for bot #{id} with exchange #{exchange_id}"
+      Transaction.where(bot_id: id).update_all(exchange_id: exchange_id)
     end
 
     change_column_null :transactions, :exchange_id, false
