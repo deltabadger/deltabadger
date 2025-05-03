5e7a3ba0c5b9cf7e60fc5bc83d4ce28c39668ce3
Guillem <guillemavila@gmail.com>
2025-05-03 23:15:34 +0200

    stage hack

--- db/migrate/20250503204242_add_error_messages_json_to_transactions.rb
+++ db/migrate/20250503204242_add_error_messages_json_to_transactions.rb
@@ -1,5 +1,6 @@
 class AddErrorMessagesJsonToTransactions < ActiveRecord::Migration[6.0]
   def change
     add_column :transactions, :error_messages_json, :jsonb, default: [], null: false
+    change_column :transactions, :error_messages, :string, default: "[]", null: true
   end
 end
