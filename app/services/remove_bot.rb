d6749eace41d51d1ab073ef1fa6d3403a10215fe
Guillem <guillemavila@gmail.com>
2025-05-05 13:44:59 +0200

    Update remove_bot.rb

--- app/services/remove_bot.rb
+++ app/services/remove_bot.rb
@@ -3,8 +3,8 @@ class RemoveBot < BaseService
     @unschedule_transactions = unschedule_transactions
   end
 
-  def call(bot_id:)
-    bot = Bot.find(bot_id)
+  def call(bot_id:, user:)
+    bot = user.bots.find(bot_id)
 
     if bot.working?
       Result::Failure.new('Bot is currently working. Stop bot before removing')
