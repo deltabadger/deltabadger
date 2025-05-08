2add2358165fb48893bbdb3d35263df8f747ee7c
Guillem <guillemavila@gmail.com>
2025-05-09 01:39:43 +0200

    Update update_metrics_job.rb

--- app/jobs/bot/update_metrics_job.rb
+++ app/jobs/bot/update_metrics_job.rb
@@ -7,7 +7,7 @@ class Bot::UpdateMetricsJob < ApplicationJob
   def perform(bot)
     cancel_other_jobs(bot)
     bot.metrics(force: true)
-    bot.broadcast_metrics_update
+    Bot::BroadcastMetricsUpdateJob.perform_later(bot)
   end
 
   private
