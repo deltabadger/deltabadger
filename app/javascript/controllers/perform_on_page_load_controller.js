668797c9b2867bbb573393d573d012bf52ab0536
Guillem <guillemavila@gmail.com>
2025-05-09 04:20:58 +0200

    drastic cleanup

--- app/javascript/controllers/perform_on_page_load_controller.js
+++ app/javascript/controllers/perform_on_page_load_controller.js
@@ -6,36 +6,6 @@ export default class extends Controller {
   static values = { channel: String, method: String, methodArgs: Object };
 
   connect() {
-    this.boundTriggerJob = this.#triggerJob.bind(this);
-    this.hasTriggered = false;
-
-    window.addEventListener("turbo:load", this.boundTriggerJob, { once: true });
-    window.addEventListener("load", this.boundTriggerJob, { once: true });
-
-    this.fallbackTimeout = setTimeout(() => {
-      if (!this.hasTriggered) {
-        console.log("Fallback: triggering job after 5 seconds");
-        this.boundTriggerJob();
-      }
-    }, 5000);
-  }
-
-  disconnect() {
-    if (this.boundTriggerJob) {
-      window.removeEventListener("turbo:load", this.boundTriggerJob);
-      window.removeEventListener("load", this.boundTriggerJob);
-    }
-    if (this.fallbackTimeout) {
-      clearTimeout(this.fallbackTimeout);
-    }
-  }
-
-  #triggerJob() {
-    if (this.hasTriggered) return;
-
-    this.hasTriggered = true;
-    clearTimeout(this.fallbackTimeout);
-
     const consumer = createConsumer();
     const channel = this.channelValue;
     const method = this.methodValue;
