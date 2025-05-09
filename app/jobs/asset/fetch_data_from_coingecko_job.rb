4a1f832072cb35037d429ae004840deeb0fd98b5
Guillem <guillemavila@gmail.com>
2025-05-09 03:17:59 +0200

    Update fetch_data_from_coingecko_job.rb

--- app/jobs/asset/fetch_data_from_coingecko_job.rb
+++ app/jobs/asset/fetch_data_from_coingecko_job.rb
@@ -2,9 +2,10 @@ class Asset::FetchDataFromCoingeckoJob < ApplicationJob
   queue_as :default
 
   def perform(asset)
+    image_url_was = asset.image_url
     result = asset.sync_data_with_coingecko
     raise StandardError, result.errors.to_sentence unless result.success?
 
-    Asset::InferColorFromImageJob.perform_later(asset)
+    Asset::InferColorFromImageJob.perform_later(asset) if image_url_was != asset.image_url
   end
 end
