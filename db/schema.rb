91ad8b9f56278e47971a48418ab4da7c94477869
Guillem <guillemavila@gmail.com>
2025-05-10 13:11:06 +0200

    use ends_at for subscription

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2025_05_10_012306) do
+ActiveRecord::Schema.define(version: 2025_05_10_110509) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -278,7 +278,7 @@ ActiveRecord::Schema.define(version: 2025_05_10_012306) do
   create_table "subscriptions", force: :cascade do |t|
     t.bigint "subscription_plan_variant_id"
     t.bigint "user_id"
-    t.datetime "end_time"
+    t.datetime "ends_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "nft_id"
