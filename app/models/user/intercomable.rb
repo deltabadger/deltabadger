600eb7ab231dd9de36f9b48e6394e1d6d917dcc8
Guillem <guillemavila@gmail.com>
2025-05-11 03:25:24 +0200

    Update intercomable.rb

--- app/models/user/intercomable.rb
+++ app/models/user/intercomable.rb
@@ -6,11 +6,13 @@ module User::Intercomable
   end
 
   def update_intercom_subscription
+    # TODO: implement using intercom gem
     # intercom_custom_data.user[:subscription] = subscription&.name&.capitalize
     # intercom_custom_data.user[:subscription_ends_at] = subscription&.ends_at
   end
 
   def update_intercom_email_verified
+    # TODO: implement using intercom gem
     # intercom_custom_data.user[:email_verified] = confirmed_at.present?
   end
 end
