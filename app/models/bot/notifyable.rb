439f4b0346b3324c54d74d5d06273615ba4ebca4
Guillem <guillemavila@gmail.com>
2025-05-14 00:03:53 +0200

    fixes

--- app/models/bot/notifyable.rb
+++ app/models/bot/notifyable.rb
@@ -29,12 +29,12 @@ module Bot::Notifyable
     ).end_of_funds.deliver_later
   end
 
-  def notify_bot_stopped_by_quote_amount_limit
+  def notify_stopped_by_quote_amount_limit
     BotAlertsMailer.with(
-      user_name: user.name,
+      user: user,
       label: label,
       amount: quote_amount_limit,
       quote: quote_asset.symbol
-    ).bot_stopped_by_quote_amount_limit.deliver_later
+    ).stopped_by_quote_amount_limit.deliver_later
   end
 end
