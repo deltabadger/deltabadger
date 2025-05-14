b2601139797509446ec55f4ddf225288bd725069
Guillem <guillemavila@gmail.com>
2025-05-14 15:50:59 +0200

    fix autowidth

--- app/javascript/controllers/autowidth_input_controller.js
+++ app/javascript/controllers/autowidth_input_controller.js
@@ -3,6 +3,7 @@ import { Controller } from "@hotwired/stimulus"
 // Automatically adjusts the width of an input element based on its content length using a mirror element for precise calculation.
 // Connects to data-controller="autowidth-input"
 export default class extends Controller {
+  static values = { minWidth: Number }
 
   connect() {
     this.resize = this.resize.bind(this)
@@ -25,8 +26,8 @@ export default class extends Controller {
     const measuredWidth = this.mirrorElement.offsetWidth;
 
     // Add a small buffer (e.g., 2px) to prevent text clipping or overflow
-    const buffer = 2;
-    const minWidth = this.element.style.width;
+    const buffer = 10;
+    const minWidth = this.minWidthValue || 0;
     const newWidth = Math.max(measuredWidth + buffer, minWidth);
 
     // Set the input element's width
