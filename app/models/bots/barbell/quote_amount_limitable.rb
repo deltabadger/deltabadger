0ed6e212a0be9f1079fe6c574d8bd933990bd3b6
Guillem <guillemavila@gmail.com>
2025-05-15 18:20:26 +0200

    better validations

--- app/models/bots/barbell/quote_amount_limitable.rb
+++ app/models/bots/barbell/quote_amount_limitable.rb
@@ -5,17 +5,22 @@ module Bots::Barbell::QuoteAmountLimitable
     store_accessor :settings, :quote_amount_limited, :quote_amount_limit
     store_accessor :transient_data, :quote_amount_limit_enabled_at
 
+    after_initialize :set_quote_amount_limited, :set_quote_amount_limit
+
     before_save :set_quote_amount_limit_enabled_at, if: :will_save_change_to_settings?
 
-    validates :quote_amount_limited, inclusion: { in: [true, false] }, if: -> { quote_amount_limited.present? }
-    validates :quote_amount_limit, presence: true, if: -> { quote_amount_limited? }
-    validates :quote_amount_limit, numericality: { greater_than: 0 }, if: -> { quote_amount_limited? }
+    validates :quote_amount_limited, inclusion: { in: [true, false] }
+    validates :quote_amount_limit, numericality: { greater_than_or_equal_to: 0 }
+    validates :quote_amount_limit, numericality: { greater_than: 0 }, if: :quote_amount_limited?
     validate :validate_quote_amount_limit_not_reached, if: :quote_amount_limited?, on: :start
   end
 
-  def quote_amount_limit_enabled_at
-    value = super
-    value.present? ? Time.zone.parse(value) : nil
+  def set_quote_amount_limited
+    self.quote_amount_limited ||= false
+  end
+
+  def set_quote_amount_limit
+    self.quote_amount_limit ||= 0
   end
 
   def set_quote_amount_limit_enabled_at
@@ -24,22 +29,28 @@ module Bots::Barbell::QuoteAmountLimitable
     self.quote_amount_limit_enabled_at = quote_amount_limited? ? Time.current : nil
   end
 
+  def quote_amount_limit_enabled_at
+    value = super
+    value.present? ? Time.zone.parse(value) : nil
+  end
+
   def quote_amount_limited?
-    quote_amount_limited.present? && quote_amount_limited
+    quote_amount_limited == true
   end
 
   def quote_amount_available_before_limit_reached
-    return Float::INFINITY unless quote_amount_limit.present?
+    return Float::INFINITY unless quote_amount_limited?
 
     quote_amount_spent = transactions.success.where('created_at > ?', quote_amount_limit_enabled_at).sum(:quote_amount)
     [quote_amount_limit - quote_amount_spent, 0].max
   end
 
   def quote_amount_limit_reached?
-    return false unless quote_amount.present? && quote_amount_limit.present?
+    return false unless quote_amount_limited?
+    return true if quote_amount_limit.zero?
 
     quote_amount_limited? && quote_amount_available_before_limit_reached < [
-      [quote_amount, quote_amount_limit].min * 0.01, # give a 1% buffer to avoid rounding & fees errors
+      [quote_amount || Float::INFINITY, quote_amount_limit].min * 0.01, # 1% buffer to avoid rounding & fees errors
       1.0 / (10**decimals[:quote])                   # minimum amount to be shown to the user
     ].max
   end
