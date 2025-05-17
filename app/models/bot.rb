6575fa0d6d8b2df42f3381d3f6fafd8695c2e93e
Guillem <guillemavila@gmail.com>
2025-05-17 19:26:16 +0200

    Update bot.rb

--- app/models/bot.rb
+++ app/models/bot.rb
@@ -4,9 +4,9 @@ class Bot < ApplicationRecord
   has_many :transactions, dependent: :destroy
   has_many :daily_transaction_aggregates
 
-  enum status: %i[created scheduled stopped deleted executing retrying]
+  enum status: %i[created scheduled stopped deleted executing retrying waiting]
 
-  scope :working, -> { where(status: %i[scheduled executing retrying]) }
+  scope :working, -> { where(status: %i[scheduled executing retrying waiting]) }
   scope :legacy, -> { where(type: %w[Bots::Basic Bots::Withdrawal Bots::Webhook]) }
   scope :not_legacy, -> { where.not(type: %w[Bots::Basic Bots::Withdrawal Bots::Webhook]) }
 
@@ -30,7 +30,7 @@ class Bot < ApplicationRecord
   end
 
   def working?
-    scheduled? || executing? || retrying?
+    scheduled? || executing? || retrying? || waiting?
   end
 
   def last_transaction
@@ -76,6 +76,29 @@ class Bot < ApplicationRecord
       target: dom_id(self, :status_button),
       partial: legacy? ? 'bots/status/status_button_legacy' : 'bots/status/status_button',
       locals: { bot: self }
+    )
+  end
+
+  def broadcast_new_order(order)
+    # TODO: When transactions point to real asset ids, we can use the asset ids directly instead of symbols
+    ticker = order.exchange.tickers.find_by(base_asset: order.base_asset, quote_asset: order.quote_asset)
+    decimals = {
+      order.base_asset.symbol => ticker.base_decimals,
+      order.quote_asset.symbol => ticker.quote_decimals
+    }
+
+    if transactions.limit(2).count == 1
+      broadcast_remove_to(
+        ["user_#{user_id}", :bot_updates],
+        target: 'orders_list_placeholder'
+      )
+    end
+
+    broadcast_prepend_to(
+      ["user_#{user_id}", :bot_updates],
+      target: 'orders_list',
+      partial: 'bots/orders/order',
+      locals: { order: order, decimals: decimals, exchange_name: order.exchange.name, current_user: user }
     )
   end
 
