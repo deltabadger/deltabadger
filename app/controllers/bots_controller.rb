3a32b215d79f221ef4328bff589809005b560a0b
Guillem <guillemavila@gmail.com>
2025-05-30 01:48:07 +0200

    improved controllers

--- app/controllers/bots_controller.rb
+++ app/controllers/bots_controller.rb
@@ -1,8 +1,8 @@
 class BotsController < ApplicationController
-  include Pagy::Backend
-
   before_action :authenticate_user!
-  before_action :set_bot, except: %i[index new]
+
+  include Pagy::Backend
+  include Botable
 
   def index
     return render 'bots/react_dashboard' if params[:create] # TODO: remove this once the legacy dashboard is removed
@@ -82,49 +82,6 @@ class BotsController < ApplicationController
     end
   end
 
-  def confirm_destroy; end
-
-  def destroy
-    if @bot.destroy
-      flash[:notice] = t('errors.bots.destroy_success', bot_label: @bot.label)
-      render turbo_stream: turbo_stream_redirect(bots_path)
-    else
-      flash.now[:alert] = @bot.errors.messages.values.flatten.to_sentence
-      render turbo_stream: turbo_stream_prepend_flash, status: :unprocessable_entity
-    end
-  end
-
-  def new_api_key
-    @api_key = @bot.api_key
-  end
-
-  def create_api_key
-    @api_key = @bot.api_key
-    @api_key.key = api_key_params[:key]
-    @api_key.secret = api_key_params[:secret]
-    @api_key.validate_key_permissions
-    if @api_key.correct? && @api_key.save
-      flash[:notice] = t('errors.bots.api_key_success')
-      render turbo_stream: turbo_stream_page_refresh
-    else
-      render :new_api_key, status: :unprocessable_entity
-    end
-  end
-
-  def start
-    return if @bot.start(start_fresh: Utilities::String.to_boolean(params[:start_fresh]))
-
-    flash.now[:alert] = @bot.errors.messages.values.flatten.to_sentence
-    render turbo_stream: turbo_stream_prepend_flash, status: :unprocessable_entity
-  end
-
-  def stop
-    return if @bot.stop
-
-    flash.now[:alert] = @bot.errors.messages.values.flatten.to_sentence
-    render turbo_stream: turbo_stream_prepend_flash, status: :unprocessable_entity
-  end
-
   def asset_search
     asset_type = params[:asset_field] == 'quote_asset_id' ? :quote_asset : :base_asset
     available_assets = @bot.available_assets_for_current_settings(asset_type: asset_type)
@@ -142,17 +99,7 @@ class BotsController < ApplicationController
     @asset_field = params[:asset_field]
   end
 
-  def confirm_restart; end
-
-  def confirm_restart_legacy; end
-
   private
-
-  def set_bot
-    @bot = current_user.bots.find(params[:id])
-  rescue ActiveRecord::RecordNotFound
-    redirect_to bots_path, alert: t('bot.not_found')
-  end
 
   def basic_bot_params
     params.require(:bots_basic).permit(:label)
@@ -164,10 +111,6 @@ class BotsController < ApplicationController
 
   def webhook_bot_params
     params.require(:bots_webhook).permit(:label)
-  end
-
-  def api_key_params
-    params.require(:api_key).permit(:key, :secret)
   end
 
   def update_params_legacy
---
+++ app/controllers/concerns/botable.rb
@@ -0,0 +1,15 @@
+module Botable
+  extend ActiveSupport::Concern
+
+  included do
+    before_action :set_bot, only: %i[new create show edit update destroy]
+  end
+
+  private
+
+  def set_bot
+    @bot = current_user.bots.find(params[:id] || params[:bot_id])
+  rescue ActiveRecord::RecordNotFound
+    redirect_to bots_path, alert: t('bot.not_found')
+  end
+end
