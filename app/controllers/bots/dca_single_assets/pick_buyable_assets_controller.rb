e35a35e9b47dc9a753c88a11b31393cdaf07d725
Guillem <guillemavila@gmail.com>
2025-05-30 19:17:50 +0200

    add views

---
+++ app/controllers/bots/dca_single_assets/pick_buyable_assets_controller.rb
@@ -0,0 +1,33 @@
+class Bots::DcaSingleAssets::PickBuyableAssetsController < ApplicationController
+  before_action :authenticate_user!
+
+  include Bots::Searchable
+
+  def new
+    session[:bot_config] ||= {}
+    @bot = current_user.bots.dca_single_asset.new(session[:bot_config])
+    @bot.base_asset_id = nil
+    session[:bot_config]['label'] ||= @bot.label
+    @assets = asset_search_results(@bot, search_params[:query], :base_asset)
+  end
+
+  def create
+    if bot_params[:base_asset_id].present?
+      bot = current_user.bots.dca_single_asset.new(session[:bot_config])
+      session[:bot_config].deep_merge!({ settings: bot.parsed_settings(bot_params) }.deep_stringify_keys)
+      redirect_to new_bots_dca_single_assets_pick_exchange_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def search_params
+    params.permit(:query)
+  end
+
+  def bot_params
+    params.require(:bots_dca_single_asset).permit(:base_asset_id)
+  end
+end
