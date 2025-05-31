500305daa4c58985fc513b98304707fc27d2f1e1
Guillem <guillemavila@gmail.com>
2025-05-31 19:01:21 +0200

    add indicator trigger view

--- app/helpers/bot_helper.rb
+++ app/helpers/bot_helper.rb
@@ -24,7 +24,7 @@ module BotHelper
     return [] unless defined?(bot.class::PRICE_LIMIT_VALUE_CONDITIONS)
 
     bot.class::PRICE_LIMIT_VALUE_CONDITIONS.map do |condition|
-      [t("bot.settings.extra_price_limit.price_condition.#{condition}"), condition]
+      [t("bot.settings.extra_price_limit.value_condition.#{condition}"), condition]
     end
   end
 
@@ -32,6 +32,34 @@ module BotHelper
     bot.tickers.pluck(:base_asset_id, :id).map do |base_asset_id, id|
       [Asset.find(base_asset_id).symbol, id]
     end.sort_by(&:first)
+  end
+
+  def indicator_limit_timing_condition_select_options(bot)
+    return [] unless defined?(bot.class::INDICATOR_LIMIT_TIMING_CONDITIONS)
+
+    bot.class::INDICATOR_LIMIT_TIMING_CONDITIONS.map do |condition|
+      [t("bot.settings.extra_indicator_limit.timing_condition.#{condition}"), condition]
+    end
+  end
+
+  def indicator_limit_value_condition_select_options(bot)
+    return [] unless defined?(bot.class::INDICATOR_LIMIT_VALUE_CONDITIONS)
+
+    bot.class::INDICATOR_LIMIT_VALUE_CONDITIONS.map do |condition|
+      [t("bot.settings.extra_indicator_limit.value_condition.#{condition}"), condition]
+    end
+  end
+
+  def ticker_select_options(bot)
+    bot.tickers.pluck(:id, :base_asset_id, :quote_asset_id).map do |id, base_asset_id, quote_asset_id|
+      ["#{Asset.find(base_asset_id).symbol}#{Asset.find(quote_asset_id).symbol}", id]
+    end.sort_by(&:first)
+  end
+
+  def indicator_select_options
+    Bot::IndicatorLimitable::INDICATOR_LIMIT_INDICATORS.map do |indicator|
+      [indicator.upcase, indicator]
+    end
   end
 
   def vs_currencies_select_options
