685befddfa5b37ebe598cc395c683cf7a1a1844b
Guillem <guillemavila@gmail.com>
2025-05-31 03:35:58 +0200

    remove FetchPricesJob

--- app/jobs/bot/price_limit_check_job.rb
+++ app/jobs/bot/price_limit_check_job.rb
@@ -8,8 +8,7 @@ class Bot::PriceLimitCheckJob < ApplicationJob
       bot.update!(started_at: Time.current)
       Bot::ActionJob.perform_later(bot)
     else
-      # Add 10 seconds to the next check to let Exchange::FetchAllPricesJob cron job feed the cache
-      next_check_at = Time.current + Utilities::Time.seconds_to_end_of_minute + 10.seconds
+      next_check_at = Time.current + Utilities::Time.seconds_to_end_of_minute
       Bot::PriceLimitCheckJob.set(wait_until: next_check_at).perform_later(bot)
     end
   end
