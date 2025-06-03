c8b33fcc49cf4fa0a2064bc92c50b4446282f75c
Guillem <guillemavila@gmail.com>
2025-06-03 06:12:09 +0200

    fix BroadcastAfterScheduledActionJob on legacy

--- app/workers/make_transaction_worker.rb
+++ app/workers/make_transaction_worker.rb
@@ -3,11 +3,7 @@ class MakeTransactionWorker
 
   def perform(bot_id, continue_params = nil)
     MakeTransaction.call(bot_id, continue_params: continue_params)
-    bot = Bot.find(bot_id)
-    Bot::BroadcastAfterScheduledActionJob.perform_later(bot)
   rescue StandardError => e # prevent job from retrying
     Raven.capture_exception(e)
-    bot = Bot.find(bot_id)
-    Bot::BroadcastAfterScheduledActionJob.perform_later(bot)
   end
 end
