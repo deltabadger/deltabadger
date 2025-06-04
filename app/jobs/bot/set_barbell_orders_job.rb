88c1eeac1891b789b09eb09f43000695bc3ae7f3
Guillem <guillemavila@gmail.com>
2025-06-05 01:11:17 +0200

    Delete set_barbell_orders_job.rb

--- app/jobs/bot/set_barbell_orders_job.rb
+++
@@ -1,7 +0,0 @@
-class Bot::SetBarbellOrdersJob < BotJob
-  # temporary fix for Bot::SetBarbellOrdersJob -> Bot::ActionJob
-  # delete this file once Bot::SetBarbellOrdersJob doesn't exist
-  def perform(bot)
-    Bot::ActionJob.perform_later(bot)
-  end
-end
