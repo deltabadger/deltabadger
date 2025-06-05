96dd16596fef1f4603c4c3faa91d8c7c1df7d821
Guillem <guillemavila@gmail.com>
2025-06-05 19:50:50 +0200

    add standard candle intervals

---
+++ app/models/exchange/candle_builder.rb
@@ -0,0 +1,38 @@
+module Exchange::CandleBuilder
+  extend ActiveSupport::Concern
+
+  def build_candles_from_candles(candles:, timeframe:)
+    # Warning: candles must have a smaller timeframe than the one we're building, otherwise it returns the same candles
+
+    grouped_candles(candles, timeframe).map do |date, group|
+      next if date < candles.first[0]
+
+      [
+        date,                        # Date (start of the timeframe period)
+        group.first[1],              # Open (first candle's open)
+        group.map { |c| c[2] }.max,  # High (max high in group)
+        group.map { |c| c[3] }.min,  # Low (min low in group)
+        group.last[4],               # Close (last candle's close)
+        group.sum { |c| c[5] }       # Volume (sum of volumes)
+      ]
+    end.compact
+  end
+
+  private
+
+  def grouped_candles(candles, timeframe)
+    timeframe_seconds = timeframe.to_i
+
+    candles.group_by do |candle|
+      if timeframe_seconds == 1.week.to_i
+        candle[0].beginning_of_week
+      elsif timeframe_seconds == 1.month.to_i
+        candle[0].beginning_of_month
+      else
+        timestamp = candle[0].to_i
+        period_start = (timestamp / timeframe_seconds) * timeframe_seconds
+        Time.at(period_start).utc
+      end
+    end
+  end
+end
