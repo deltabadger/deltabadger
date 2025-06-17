bcfb5cf397e0820f9c93520d0817015136e5d89d
Guillem <guillemavila@gmail.com>
2025-06-17 22:11:17 +0200

    Update exports_controller.rb

--- app/controllers/bots/exports_controller.rb
+++ app/controllers/bots/exports_controller.rb
@@ -7,9 +7,10 @@ class Bots::ExportsController < ApplicationController
   before_action :set_bot
 
   def create
-    # TODO: If the process takes too long, delegate it to a sidekiq background job, then store the file in S3
-    # or similar and update the app/views/bots/orders/_export.html.erb partial to show a green download button.
-    # This will also allow us to hide the loading spinner once the file is ready.
+    # TODO: Ideally we would delegate it to a sidekiq background job, then store the file in S3 or similar,
+    #       then update the app/views/bots/orders/_export.html.erb partial to show a green download button.
+    #       This will also allow us to hide the loading spinner once the file is ready, and avoid users
+    #       generating multiple files at once.
     @bot.transactions.submitted.where(external_status: 'open').each do |transaction|
       Bot::FetchAndUpdateOrderJob.perform_now(transaction, update_missed_quote_amount: true)
     end
