0d063c290da99c84022c39d0044d6256c52f9053
Guillem <guillemavila@gmail.com>
2025-07-29 00:37:16 +0200

    add zen api

--- app/models/card.rb
+++ app/models/card.rb
@@ -2,4 +2,5 @@ class Card < ApplicationRecord
   belongs_to :user
 
   validates :token, presence: true, uniqueness: { scope: :user_id }
+  validates :ip, format: { with: /\A\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\z/ }
 end
