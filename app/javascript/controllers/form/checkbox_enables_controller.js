4cef5440107101a4a8c834e06e56ec0b191c4768
Guillem <guillemavila@gmail.com>
2025-08-02 12:54:44 +0200

    better checkbox enables

--- app/javascript/controllers/form/checkbox_enables_controller.js
+++ app/javascript/controllers/form/checkbox_enables_controller.js
@@ -3,13 +3,19 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="form--checkbox-enables"
 export default class extends Controller {
   static targets = ["checkbox", "submit"]
+  static values = { mode: { type: String, default: 'some' } } // 'some' or 'all'
 
   connect() {
-    console.log("checkbox-enables connected")
+    // console.log("checkbox-enables connected")
   }
 
   toggleSubmitButton() {
-    const anyChecked = this.checkboxTargets.some(checkbox => checkbox.checked)
-    this.submitTarget.disabled = !anyChecked
+    let shouldEnable = false;
+    if (this.modeValue === 'all') {
+      shouldEnable = this.checkboxTargets.every(checkbox => checkbox.checked);
+    } else {
+      shouldEnable = this.checkboxTargets.some(checkbox => checkbox.checked);
+    }
+    this.submitTarget.disabled = !shouldEnable;
   }
 }
