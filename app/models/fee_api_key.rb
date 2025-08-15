b582080780fef4b1dab4d3bb5dccec37bc755197
Guillem <guillemavila@gmail.com>
2025-08-15 15:58:18 +0200

    small fixes

--- app/models/fee_api_key.rb
+++ app/models/fee_api_key.rb
@@ -1,9 +1,9 @@
 class FeeApiKey < ApplicationRecord
   belongs_to :exchange
 
-  attr_encrypted :key, key: ENV.fetch('DATABASE_API_KEY_ENCRYPTION_KEY')
-  attr_encrypted :secret, key: ENV.fetch('DATABASE_API_SECRET_ENCRYPTION_KEY')
-  attr_encrypted :passphrase, key: ENV.fetch('DATABASE_API_PASSPHRASE_ENCRYPTION_KEY')
+  attr_encrypted :key, key: ENV.fetch('API_KEY_ENCRYPTION_KEY')
+  attr_encrypted :secret, key: ENV.fetch('API_SECRET_ENCRYPTION_KEY')
+  attr_encrypted :passphrase, key: ENV.fetch('API_PASSPHRASE_ENCRYPTION_KEY')
 
   delegate :name, to: :exchange, prefix: true
 end
