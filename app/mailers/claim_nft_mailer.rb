d90776416da88a1090d1dc32009a078ff106396c
Guillem <guillemavila@gmail.com>
2025-08-19 01:34:47 +0200

    use params in legendaries mailer

--- app/mailers/claim_nft_mailer.rb
+++ app/mailers/claim_nft_mailer.rb
@@ -1,10 +1,10 @@
 class ClaimNftMailer < ApplicationMailer
   default from: 'no-reply@deltabadger.com'
 
-  def form_submission_email(user_email, nft_id, eth_address)
-    @user_email = user_email
-    @nft_id = nft_id
-    @eth_address = eth_address
+  def form_submission_email
+    @user_email = params[:user].email
+    @nft_id = params[:subscription].nft_id
+    @eth_address = params[:subscription].eth_address
 
     mail(to: ['guillem@deltabadger.com', 'jan@deltabadger.com'], subject: 'New NFT Claim Request')
   end
