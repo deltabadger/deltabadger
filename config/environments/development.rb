0d779fab6fed10ae0ad146bcbc3f4173b10059ca
Guillem <guillemavila@gmail.com>
2025-08-28 02:47:47 +0200

    Update mailer default URL options to use APP_ROOT_URL
    Changed action_mailer.default_url_options in both development and production environments to use ENV['APP_ROOT_URL'] for consistency. Also added protocol to development configuration. This improves flexibility and centralizes URL configuration.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -85,7 +85,7 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :letter_opener
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = { host: ENV.fetch('APP_ROOT_URL'), protocol: 'http' }
 
   routes.default_url_options = { host: ENV.fetch('APP_ROOT_URL'), protocol: 'http' }
 
