2716bf4abcd7f956ee8f622fa426c3d813485af2
Guillem <guillemavila@gmail.com>
2026-01-02 16:30:48 +0100

    remove caffeinate
    remove caffeinate
    
    mailers

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2025_12_30_014648) do
+ActiveRecord::Schema.define(version: 2026_01_02_151655) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -101,47 +101,6 @@ ActiveRecord::Schema.define(version: 2025_12_30_014648) do
     t.string "stop_message_key"
     t.index ["exchange_id"], name: "index_bots_on_exchange_id"
     t.index ["user_id"], name: "index_bots_on_user_id"
-  end
-
-  create_table "caffeinate_campaign_subscriptions", force: :cascade do |t|
-    t.bigint "caffeinate_campaign_id", null: false
-    t.string "subscriber_type", null: false
-    t.integer "subscriber_id", null: false
-    t.string "user_type"
-    t.integer "user_id"
-    t.string "token", null: false
-    t.datetime "ended_at"
-    t.string "ended_reason"
-    t.datetime "resubscribed_at"
-    t.datetime "unsubscribed_at"
-    t.string "unsubscribe_reason"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["caffeinate_campaign_id", "subscriber_id", "subscriber_type", "user_id", "user_type", "ended_at", "resubscribed_at", "unsubscribed_at"], name: "index_caffeinate_campaign_subscriptions"
-    t.index ["caffeinate_campaign_id"], name: "caffeineate_campaign_subscriptions_on_campaign"
-    t.index ["token"], name: "index_caffeinate_campaign_subscriptions_on_token", unique: true
-  end
-
-  create_table "caffeinate_campaigns", force: :cascade do |t|
-    t.string "name", null: false
-    t.string "slug", null: false
-    t.boolean "active", default: true, null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["slug"], name: "index_caffeinate_campaigns_on_slug", unique: true
-  end
-
-  create_table "caffeinate_mailings", force: :cascade do |t|
-    t.bigint "caffeinate_campaign_subscription_id", null: false
-    t.datetime "send_at", null: false
-    t.datetime "sent_at"
-    t.datetime "skipped_at"
-    t.string "mailer_class", null: false
-    t.string "mailer_action", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["caffeinate_campaign_subscription_id", "send_at", "sent_at", "skipped_at"], name: "index_caffeinate_mailings"
-    t.index ["caffeinate_campaign_subscription_id"], name: "index_caffeinate_mailings_on_campaign_subscription"
   end
 
   create_table "conversion_rates", force: :cascade do |t|
@@ -312,8 +271,6 @@ ActiveRecord::Schema.define(version: 2025_12_30_014648) do
   add_foreign_key "api_keys", "users"
   add_foreign_key "bots", "exchanges"
   add_foreign_key "bots", "users"
-  add_foreign_key "caffeinate_campaign_subscriptions", "caffeinate_campaigns"
-  add_foreign_key "caffeinate_mailings", "caffeinate_campaign_subscriptions"
   add_foreign_key "daily_transaction_aggregates", "bots"
   add_foreign_key "exchange_assets", "assets"
   add_foreign_key "exchange_assets", "exchanges"
