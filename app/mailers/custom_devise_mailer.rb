4ecd51f82434a3d6ae8477f57ffa76b91b07a984
Guillem <guillemavila@gmail.com>
2026-01-02 18:43:07 +0100

    remove financial data api

--- app/mailers/custom_devise_mailer.rb
+++ app/mailers/custom_devise_mailer.rb
@@ -1,14 +1,12 @@
 class CustomDeviseMailer < Devise::Mailer
   layout 'mailers/transactional'
 
-  before_action :set_show_dca_profit
-
   helper LocalesHelper
 
   default template_path: 'devise/mailer'
 
   def email_already_taken(email)
-    @resource = User.find_by(email: email)
+    @resource = User.find_by(email:)
 
     mail(to: email, subject: t('devise.mailer.email_already_taken.subject'))
   end
@@ -18,11 +16,5 @@ class CustomDeviseMailer < Devise::Mailer
     @token = token
 
     mail(to: @resource.unconfirmed_email)
-  end
-
-  private
-
-  def set_show_dca_profit
-    @show_dca_profit = false
   end
 end
