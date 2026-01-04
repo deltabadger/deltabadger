a1f6c5d16615b10ab9b427b456805d670bca060b
Jan Klosowski <jan@deltabadger.com>
2026-01-04 16:45:32 +0200

    translations

--- app/models/app_config.rb
+++ app/models/app_config.rb
@@ -2,6 +2,11 @@ class AppConfig < ApplicationRecord
   validates :key, presence: true, uniqueness: true
 
   COINGECKO_API_KEY = 'coingecko_api_key'.freeze
+  SETUP_SYNC_STATUS = 'setup_sync_status'.freeze
+
+  SYNC_STATUS_PENDING = 'pending'.freeze
+  SYNC_STATUS_IN_PROGRESS = 'in_progress'.freeze
+  SYNC_STATUS_COMPLETED = 'completed'.freeze
 
   def self.get(key)
     find_by(key: key)&.value
@@ -26,5 +31,29 @@ class AppConfig < ApplicationRecord
     User.exists?(admin: true)
   rescue ActiveRecord::StatementInvalid
     false
+  end
+
+  def self.setup_sync_status
+    get(SETUP_SYNC_STATUS)
+  end
+
+  def self.setup_sync_status=(value)
+    set(SETUP_SYNC_STATUS, value)
+  end
+
+  def self.setup_sync_pending?
+    setup_sync_status == SYNC_STATUS_PENDING
+  end
+
+  def self.setup_sync_in_progress?
+    setup_sync_status == SYNC_STATUS_IN_PROGRESS
+  end
+
+  def self.setup_sync_completed?
+    setup_sync_status == SYNC_STATUS_COMPLETED
+  end
+
+  def self.setup_sync_needed?
+    setup_sync_pending? || setup_sync_in_progress?
   end
 end
