940194cf1704f723a3931d6c9a04349a8223d9b4
Jan Klosowski <jan@deltabadger.com>
2026-01-12 11:35:16 +0200

    Stop tracking SQLite database files

---
+++ config/importmap.rb
@@ -0,0 +1,5 @@
+# Pin npm packages by running ./bin/importmap
+# This file is primarily needed for mission_control-jobs dashboard
+# The main app uses jsbundling-rails (esbuild)
+
+pin "application"
