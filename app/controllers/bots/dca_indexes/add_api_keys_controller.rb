226e88eb0c1706f5db23e4ce12c0b3e16643b17e
Jan Klosowski <jan@deltabadger.com>
2026-02-01 01:50:34 +0200

    small fixes

--- app/controllers/bots/dca_indexes/add_api_keys_controller.rb
+++ app/controllers/bots/dca_indexes/add_api_keys_controller.rb
@@ -30,10 +30,10 @@ class Bots::DcaIndexes::AddApiKeysController < ApplicationController
       render turbo_stream: turbo_stream_redirect(new_bots_dca_indexes_pick_spendable_asset_path)
     elsif @api_key.incorrect?
       flash.now[:alert] = t('errors.incorrect_api_key_permissions')
-      render turbo_stream: turbo_stream_prepend_flash, status: :unprocessable_entity
+      render :new, status: :unprocessable_entity
     else
       flash.now[:alert] = t('errors.api_key_permission_validation_failed')
-      render turbo_stream: turbo_stream_prepend_flash, status: :unprocessable_entity
+      render :new, status: :unprocessable_entity
     end
   end
 
--- app/controllers/bots/dca_single_assets/add_api_keys_controller.rb
+++ app/controllers/bots/dca_single_assets/add_api_keys_controller.rb
@@ -29,10 +29,10 @@ class Bots::DcaSingleAssets::AddApiKeysController < ApplicationController
       render turbo_stream: turbo_stream_redirect(new_bots_dca_single_assets_pick_spendable_asset_path)
     elsif @api_key.incorrect?
       flash.now[:alert] = t('errors.incorrect_api_key_permissions')
-      render turbo_stream: turbo_stream_prepend_flash, status: :unprocessable_entity
+      render :new, status: :unprocessable_entity
     else
       flash.now[:alert] = t('errors.api_key_permission_validation_failed')
-      render turbo_stream: turbo_stream_prepend_flash, status: :unprocessable_entity
+      render :new, status: :unprocessable_entity
     end
   end
 
