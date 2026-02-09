49c9aa8c4c930aed2f15a64fbdc026e242258de8
Jan Klosowski <jan@deltabadger.com>
2026-02-09 19:59:31 +0200

    dependency update

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,4 @@
 module ApplicationHelper
-  include Pagy::Frontend
-
   EXCHANGE_SVGS = Hash.new do |hash, name_id|
     path = Rails.root.join("app/views/svg/_exchange-#{name_id}.html.erb")
     hash[name_id] = File.read(path).html_safe.freeze
