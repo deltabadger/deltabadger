3f5919c78339dab34d1aa99d1d876a8141f9cdd8
Jan Klosowski <jan@deltabadger.com>
2026-02-10 23:14:43 +0200

    optimizing market data logic

--- app/models/clients/market_data.rb
+++ app/models/clients/market_data.rb
@@ -43,7 +43,7 @@ class Clients::MarketData < Client
       config.request :json
       config.response :json
       config.response :raise_error
-      config.response :logger, Rails.logger, headers: false, bodies: true, log_level: :debug
+      config.response :logger, Rails.logger, headers: false, bodies: false, log_level: :debug
       config.adapter :net_http_persistent do |http|
         http.idle_timeout = 100
       end
