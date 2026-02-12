2ac414f87811bf68c634dd8c6d16c60876260430
Jan Klosowski <jan@deltabadger.com>
2026-02-12 22:21:19 +0200

    silence harmless error

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,13 +1,18 @@
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
-    rescue_from RuntimeError, with: :handle_runtime_error
-
-    private
-
-    def handle_runtime_error(error)
-      raise error unless error.message.start_with?('Unable to find subscription')
-
-      logger.debug "[ActionCable] Ignoring stale unsubscribe: #{error.message}"
-    end
   end
 end
+
+# Silence "Unable to find subscription" errors on stale unsubscribes.
+# ActionCable's execute_command rescues the error but always logs it
+# as an error regardless of rescue_from handlers, so we patch remove
+# to return early instead of raising.
+ActionCable::Connection::Subscriptions.prepend(Module.new do
+  def remove(data)
+    if subscription = @subscriptions[data["identifier"]]
+      remove_subscription(subscription)
+    else
+      logger.debug "[ActionCable] Ignoring stale unsubscribe: #{data['identifier']}"
+    end
+  end
+end)
