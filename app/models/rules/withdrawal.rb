9fc4b3002172564e652be65da3012347525420fa
Jan Klosowski <jan@deltabadger.com>
2026-02-18 13:55:08 +0200

    rule fixes

--- app/models/rules/withdrawal.rb
+++ app/models/rules/withdrawal.rb
@@ -7,12 +7,12 @@ class Rules::Withdrawal < Rule
   store_accessor :settings, :max_fee_percentage
 
   validates :address, presence: true
-  validates :exchange, presence: true
-  validates :asset, presence: true
   validates :max_fee_percentage, presence: true,
                                  numericality: { greater_than: 0, less_than_or_equal_to: 100 }
 
   def minimum_withdrawal_amount
+    return nil if max_fee_percentage.blank?
+
     fee = BigDecimal(exchange.withdrawal_fee.presence || '0')
     pct = BigDecimal(max_fee_percentage.to_s)
     return nil if fee.zero? || pct.zero?
