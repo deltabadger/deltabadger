ccd2af89912f83bb8db3abc0027ba0d0122ca876
Jan Klosowski <jan@deltabadger.com>
2026-03-22 22:32:01 +0200

    remove esbuild dependency

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,5 +1,5 @@
 # Pin npm packages by running ./bin/importmap
 # This file is primarily needed for mission_control-jobs dashboard
-# The main app uses jsbundling-rails (esbuild)
+# The main app uses jsbundling-rails (bun)
 
 pin "application"
